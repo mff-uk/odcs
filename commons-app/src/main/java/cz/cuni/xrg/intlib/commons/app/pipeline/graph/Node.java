5ea34322ed8ff711aa6bcca662733e07444e0f7f
Jan Vojt <jan@vojt.net>
2013-04-16 17:41:49 +0200

    Removed neighbors from nodes -> represented by edges.

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/pipeline/graph/Node.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/pipeline/graph/Node.java
@@ -1,9 +1,6 @@
 package cz.cuni.xrg.intlib.commons.app.pipeline.graph;
 
 import cz.cuni.xrg.intlib.commons.app.dpu.DPUInstance;
-import cz.cuni.xrg.intlib.commons.app.pipeline.Pipeline;
-
-import java.util.List;
 
 /**
  * Node represents DPU on the pipeline and holds information
@@ -19,8 +16,6 @@ public class Node {
 
     private DPUInstance dpuInstance;
 
-    private List<Node> neighbours;
-
     private Position position;
 
     public Node(DPUInstance dpuInstance) {
@@ -31,20 +26,12 @@ public class Node {
         return dpuInstance;
     }
 
-    public List<Node> getNeighbours() {
-        return neighbours;
-    }
-
     public Position getPosition() {
         return position;
     }
 
     public void setDpuInstance(DPUInstance dpuInstance) {
         this.dpuInstance = dpuInstance;
-    }
-
-    public void setNeighbours(List<Node> neighbours) {
-        this.neighbours = neighbours;
     }
 
     public void setPosition(Position newPosition) {
