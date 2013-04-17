4986bde10d0572863bc485bdcd25ac874effa066
Petr Škoda <p.skoda@centrum.cz>
2013-04-17 22:40:36 +0200

    PipelineList, add 'run' button

--- frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/views/PipelineList.java
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/views/PipelineList.java
@@ -7,6 +7,8 @@ import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
@@ -16,6 +18,7 @@ import com.vaadin.ui.Button.ClickEvent;
 import cz.cuni.xrg.intlib.auxiliaries.App;
 import cz.cuni.xrg.intlib.auxiliaries.ContainerFactory;
 import cz.cuni.xrg.intlib.commons.app.pipeline.Pipeline;
+import cz.cuni.xrg.intlib.commons.app.pipeline.PipelineExecution;
 import cz.cuni.xrg.intlib.frontend.gui.ViewNames;
 
 public class PipelineList extends CustomComponent implements View {
@@ -27,45 +30,99 @@ public class PipelineList extends CustomComponent implements View {
 	private Table tablePipelines;
 
 	private Button btnCreatePipeline;
-	
-	class actionColumnGenerator implements com.vaadin.ui.Table.ColumnGenerator {
 
-                @Override
-		public Object generateCell(final Table source, final Object itemId, Object columnId) {
-			HorizontalLayout layout = new HorizontalLayout();
-			
-			Button updateButton = new Button();
-			updateButton.setCaption("edit");
-			updateButton.addClickListener(new com.vaadin.ui.Button.ClickListener() {
-                                @Override
-				public void buttonClick(ClickEvent event) {
-						// navigate to PipelineEdit/New
-						App.getApp().getNavigator().navigateTo( 
-								ViewNames.PipelineEdit.getUrl() + "/" + itemId.toString() );
-					}
-				});
-			layout.addComponent(updateButton);
-			// get item 			
-			final BeanItem<Pipeline> item = (BeanItem<Pipeline>) source.getItem(itemId);
-			
-			Button daleteButton = new Button();
-			daleteButton.setCaption("delete");
-			daleteButton.addClickListener(new com.vaadin.ui.Button.ClickListener() {
-                                @Override
-				public void buttonClick(ClickEvent event) {
-						// navigate to PipelineEdit/New
-						App.getApp().getPipelines().delete( item.getBean() );
-						// now we have to remove pipeline from table
-						source.removeItem(itemId);
-					}
-				});
-			layout.addComponent(daleteButton);			
-			
-			return layout;
-		}
-		
+	/**
+	 * Class used to run pipelines.
+	 * @author Petyr
+	 *
+	 */
+	public class RunPipeline implements Runnable {
+
+		/**
+		 * Pipeline to run.
+		 */
+	    private Pipeline pipeline;
+
+	    public RunPipeline(Pipeline pipeline) {
+	        this.pipeline = pipeline;
+	    }
+
+	    public void run() {
+	    	PipelineExecution pipelineExec = new PipelineExecution(pipeline);
+			pipelineExec.run();
+	    }
 	}
 	
+	/**
+	 * Generate column in table with buttons.
+	 * @author Petyr
+	 *
+	 */
+	class actionColumnGenerator implements com.vaadin.ui.Table.ColumnGenerator {
+
+		@Override
+		public Object generateCell(final Table source, final Object itemId,
+				Object columnId) {
+			HorizontalLayout layout = new HorizontalLayout();
+
+			Button updateButton = new Button();
+			updateButton.setCaption("edit");
+			updateButton
+					.addClickListener(new com.vaadin.ui.Button.ClickListener() {
+						@Override
+						public void buttonClick(ClickEvent event) {
+							// navigate to PipelineEdit/New
+							App.getApp()
+									.getNavigator()
+									.navigateTo(
+											ViewNames.PipelineEdit.getUrl()
+													+ "/" + itemId.toString());
+						}
+					});
+			layout.addComponent(updateButton);
+			// get item
+			final BeanItem<Pipeline> item = (BeanItem<Pipeline>) source.getItem(itemId);
+
+			Button deleteButton = new Button();
+			deleteButton.setCaption("delete");
+			deleteButton
+					.addClickListener(new com.vaadin.ui.Button.ClickListener() {
+						@Override
+						public void buttonClick(ClickEvent event) {
+							// navigate to PipelineEdit/New
+							App.getApp().getPipelines().delete(item.getBean());
+							// now we have to remove pipeline from table
+							source.removeItem(itemId);
+						}
+					});
+			layout.addComponent(deleteButton);
+
+			Button runButton = new Button();
+			runButton.setCaption("delete");
+			runButton
+					.addClickListener(new com.vaadin.ui.Button.ClickListener() {
+						@Override
+						public void buttonClick(ClickEvent event) {
+							// navigate to PipelineEdit/New
+							Pipeline pipeline = item.getBean();
+
+							// start pipeline in othre thread
+							RunPipeline pipelineRun = new RunPipeline(pipeline);
+					        Thread t = new Thread(pipelineRun);
+					        t.start();
+														
+							// show message about action
+							Notification.show("pipeline execution started ..",
+									Type.HUMANIZED_MESSAGE);
+						}
+					});
+			layout.addComponent(runButton);
+
+			return layout;
+		}
+
+	}
+
 	public PipelineList() {
 
 	}
@@ -74,11 +131,11 @@ public class PipelineList extends CustomComponent implements View {
 		// common part: create layout
 		mainLayout = new VerticalLayout();
 		mainLayout.setImmediate(true);
-		
+
 		// top-level component properties
 		setWidth("600px");
 		setHeight("800px");
-		
+
 		// label
 		label = new Label();
 		label.setImmediate(false);
@@ -87,40 +144,45 @@ public class PipelineList extends CustomComponent implements View {
 		label.setValue("<h1>Pipelines</h>");
 		label.setContentMode(ContentMode.HTML);
 		mainLayout.addComponent(label);
-		
+
 		tablePipelines = new Table();
 		tablePipelines.setWidth("640px");
 		tablePipelines.setHeight("480px");
 		// assign data source
-		Container container = ContainerFactory.CreatePipelines(App.getApp().getPipelines().getAllPipelines());
+		Container container = ContainerFactory.CreatePipelines(App.getApp()
+				.getPipelines().getAllPipelines());
 		tablePipelines.setContainerDataSource(container);
-		
+
 		// set columns
-		tablePipelines.setVisibleColumns(new String[] {"id", "name", "description"});
+		tablePipelines.setVisibleColumns(new String[] { "id", "name",
+				"description" });
 		mainLayout.addComponent(tablePipelines);
 		// add column
-		tablePipelines.addGeneratedColumn("", new actionColumnGenerator() );
-		
+		tablePipelines.addGeneratedColumn("", new actionColumnGenerator());
+
 		btnCreatePipeline = new Button();
 		btnCreatePipeline.setCaption("create pipeline");
 		btnCreatePipeline.setHeight("25px");
 		btnCreatePipeline.setWidth("150px");
-		btnCreatePipeline.addClickListener(new com.vaadin.ui.Button.ClickListener() {
-                        @Override
-			public void buttonClick(ClickEvent event) {
-				// navigate to PipelineEdit/New
-				App.getApp().getNavigator().navigateTo( ViewNames.PipelineEdit_New.getUrl() );
-			}
-		});
+		btnCreatePipeline
+				.addClickListener(new com.vaadin.ui.Button.ClickListener() {
+					@Override
+					public void buttonClick(ClickEvent event) {
+						// navigate to PipelineEdit/New
+						App.getApp()
+								.getNavigator()
+								.navigateTo(ViewNames.PipelineEdit_New.getUrl());
+					}
+				});
 		mainLayout.addComponent(btnCreatePipeline);
-		
+
 		return mainLayout;
 	}
 
-        @Override
+	@Override
 	public void enter(ViewChangeEvent event) {
 		buildMainLayout();
-		setCompositionRoot(mainLayout);		
+		setCompositionRoot(mainLayout);
 	}
 
 }
