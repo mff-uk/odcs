f02788dc3d306063f776f3750f601e1552fe15f8
Jiri Tomes <Tooomy@seznam.cz>
2013-04-22 23:42:33 +0200

    Repair test cases for run Pipeline + refactoring.

--- frontend/src/test/java/cz/cuni/xrg/intlib/commons/app/pipeline/PipelineRunSecondTest.java
+++ frontend/src/test/java/cz/cuni/xrg/intlib/commons/app/pipeline/PipelineRunSecondTest.java
@@ -2,19 +2,15 @@ package cz.cuni.xrg.intlib.commons.app.pipeline;
 
 import static org.junit.Assert.*;
 
-import java.util.LinkedList;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cz.cuni.xrg.intlib.commons.Type;
 import cz.cuni.xrg.intlib.commons.app.dpu.DPU;
-import cz.cuni.xrg.intlib.commons.app.dpu.DpuFacade;
 import cz.cuni.xrg.intlib.commons.app.module.ModuleFacade;
 import cz.cuni.xrg.intlib.commons.app.pipeline.graph.PipelineGraph;
 import cz.cuni.xrg.intlib.commons.configuration.Configuration;
-import cz.cuni.xrg.intlib.repository.LocalRepo;
+import cz.cuni.xrg.intlib.repository.RDFFormatType;
+
 
 /**
  * Test scenario for pipeline runs.
@@ -22,7 +18,7 @@ import cz.cuni.xrg.intlib.repository.LocalRepo;
  * 
  * TODO resolve vaadin dependency on config dialogue and move to backend
  */
-public class PipelineRunTestSecondUseCase {
+public class PipelineRunSecondTest {
 	
 	/**
 	 * Facade for loading modules given as jar files
@@ -80,18 +76,19 @@ public class PipelineRunTestSecondUseCase {
 		
 		// set configurations
 		Configuration exConfig = new  Configuration();
-// TODO: set your path here to the source file
-		exConfig.setValue("Path", "e:/tmp/test/ted4.ttl");
-		exConfig.setValue("FileSuffix", "ttl");
+// TODO: set your File extractor
+		exConfig.setValue("Path", "http://ld.opendata.cz/tedDumps/ted4.ttl");
+		exConfig.setValue("FileSuffix", ".ttl");
 		exConfig.setValue("OnlyThisSuffix", true);
 		
 		graph.getNodeById(eId).getDpuInstance().setInstanceConfig(exConfig);
 
 		Configuration ldConfig = new  Configuration();
-// TODO: set your path here	 to the output directory
-		ldConfig.setValue("DirectoryPath", "e:/tmp/test/");
+                
+// TODO: set your File Loader
+		ldConfig.setValue("DirectoryPath", "C:\\intlib\\");
 		ldConfig.setValue("FileName", "out");
-		ldConfig.setValue("RDFFileFormat", "AUTO");
+		ldConfig.setValue("RDFFileFormat", RDFFormatType.AUTO);
 		
 		graph.getNodeById(lId).getDpuInstance().setInstanceConfig(ldConfig);		
 		
