f645e8b36df7e0860990d6b4bf8c56836b2254f9
Jiri Tomes <Tooomy@seznam.cz>
2013-05-01 20:48:19 +0200

    Remove unused class RDFGraph, IN/OUT/Knowledge graphs in DPUInstance set as comments.

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/dpu/DPUInstance.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/dpu/DPUInstance.java
@@ -1,6 +1,5 @@
 package cz.cuni.xrg.intlib.commons.app.dpu;
 
-import cz.cuni.xrg.intlib.commons.RDFGraph;
 import cz.cuni.xrg.intlib.commons.configuration.Configuration;
 
 /**
@@ -23,17 +22,17 @@ public class DPUInstance {
     /**
      * All edges go to the component on the canvas.
      */
-    private RDFGraph inputGraph;
+    //private RDFGraph inputGraph;
     
     /**
      * All edges go from the component on the canvas.
      */
-    private RDFGraph outputGraph;
+    //private RDFGraph outputGraph;
     
     /**
      * Connection to knowledge base
      */
-    private RDFGraph knowledgeGraph;
+    //private RDFGraph knowledgeGraph;
     
     /**
      * Configuration setting for this component.
@@ -50,11 +49,12 @@ public class DPUInstance {
 		this.description = dpu.getDescription();
     }
 
+  /*
     public DPUInstance(RDFGraph inputGraph, RDFGraph outputGraph, InstanceConfiguration instanceConfig) {
         this.inputGraph = inputGraph;
         this.outputGraph = outputGraph;
         this.instanceConfig = instanceConfig;
-    }
+    }*/
 
 	/**
 	 * @return the id
@@ -80,44 +80,47 @@ public class DPUInstance {
 	/**
 	 * @return the inputGraph
 	 */
-	public RDFGraph getInputGraph() {
+	
+        /*
+        public RDFGraph getInputGraph() {
 		return inputGraph;
-	}
+	}*/
 
 	/**
 	 * @param inputGraph the inputGraph to set
 	 */
-	public void setInputGraph(RDFGraph inputGraph) {
+	/*
+        public void setInputGraph(RDFGraph inputGraph) {
 		this.inputGraph = inputGraph;
-	}
+	}*/
 
 	/**
 	 * @return the outputGraph
 	 */
-	public RDFGraph getOutputGraph() {
+	/*public RDFGraph getOutputGraph() {
 		return outputGraph;
-	}
+	}*/
 
 	/**
 	 * @param outputGraph the outputGraph to set
 	 */
-	public void setOutputGraph(RDFGraph outputGraph) {
+	/*public void setOutputGraph(RDFGraph outputGraph) {
 		this.outputGraph = outputGraph;
-	}
+	}*/
 
 	/**
 	 * @return the knowledgeGraph
 	 */
-	public RDFGraph getKnowledgeGraph() {
+	/*public RDFGraph getKnowledgeGraph() {
 		return knowledgeGraph;
-	}
+	}*/
 
 	/**
 	 * @param knowledgeGraph the knowledgeGraph to set
 	 */
-	public void setKnowledgeGraph(RDFGraph knowledgeGraph) {
+	/*public void setKnowledgeGraph(RDFGraph knowledgeGraph) {
 		this.knowledgeGraph = knowledgeGraph;
-	}
+	}*/
 
 	/**
 	 * @return the instanceConfig
