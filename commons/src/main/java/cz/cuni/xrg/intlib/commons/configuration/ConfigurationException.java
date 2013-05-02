58143bb93833c15e5bfcc9f344b56eb1379198bf
Jiri Tomes <Tooomy@seznam.cz>
2013-05-02 17:05:03 +0200

    NotSupporteRDFFormatException as extending of ConfigurationException.

--- commons/src/main/java/cz/cuni/xrg/intlib/commons/configuration/ConfigurationException.java
+++ commons/src/main/java/cz/cuni/xrg/intlib/commons/configuration/ConfigurationException.java
@@ -7,5 +7,29 @@ package cz.cuni.xrg.intlib.commons.configuration;
  *
  */
 public class ConfigurationException extends RuntimeException {
+private final String message = "Configuration is wrong set.";
+
+    public ConfigurationException() {
+    }
+
+    public ConfigurationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+    
+    
+
 
 }
