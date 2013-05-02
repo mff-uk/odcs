5c3cf256dc477aa80342269985d4e41c3bae9006
Jiri Tomes <Tooomy@seznam.cz>
2013-05-02 11:16:34 +0200

    Move run method from PipelineExectution to PipelineWorker class. Remove run in Pipeline class.

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/pipeline/Pipeline.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/pipeline/Pipeline.java
@@ -1,39 +1,13 @@
 package cz.cuni.xrg.intlib.commons.app.pipeline;
 
-import cz.cuni.xrg.intlib.commons.Type;
-import cz.cuni.xrg.intlib.commons.app.dpu.DPU;
-import cz.cuni.xrg.intlib.commons.app.dpu.DPUInstance;
-import cz.cuni.xrg.intlib.commons.app.module.ModuleFacade;
 import javax.persistence.*;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ApplicationEventPublisherAware;
-//import cz.cuni.xrg.intlib.commons.app.pipeline.event.PipelineCompletedEvent;
-//import cz.cuni.xrg.intlib.commons.app.pipeline.event.PipelineStartedEvent;
-import cz.cuni.xrg.intlib.commons.app.pipeline.graph.DependencyGraph;
-import cz.cuni.xrg.intlib.commons.app.pipeline.graph.GraphIterator;
-import cz.cuni.xrg.intlib.commons.app.pipeline.graph.Node;
 import cz.cuni.xrg.intlib.commons.app.pipeline.graph.PipelineGraph;
 import cz.cuni.xrg.intlib.commons.app.user.Resource;
-import cz.cuni.xrg.intlib.commons.configuration.Configuration;
 import cz.cuni.xrg.intlib.commons.extractor.Extract;
-import cz.cuni.xrg.intlib.commons.extractor.ExtractCompletedEvent;
-import cz.cuni.xrg.intlib.commons.extractor.ExtractContext;
-import cz.cuni.xrg.intlib.commons.extractor.ExtractException;
-import cz.cuni.xrg.intlib.commons.extractor.ExtractFailedEvent;
 import cz.cuni.xrg.intlib.commons.loader.Load;
-import cz.cuni.xrg.intlib.commons.loader.LoadCompletedEvent;
-import cz.cuni.xrg.intlib.commons.loader.LoadContext;
-import cz.cuni.xrg.intlib.commons.loader.LoadException;
-import cz.cuni.xrg.intlib.commons.loader.LoadFailedEvent;
 import cz.cuni.xrg.intlib.commons.transformer.Transform;
-import cz.cuni.xrg.intlib.commons.transformer.TransformCompletedEvent;
-import cz.cuni.xrg.intlib.commons.transformer.TransformContext;
-import cz.cuni.xrg.intlib.commons.transformer.TransformException;
-import cz.cuni.xrg.intlib.commons.transformer.TransformFailedEvent;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import org.springframework.context.support.GenericApplicationContext;
+
+
 
 /**
  * Represents a fixed workflow composed of one or several {@link Extract}s,
@@ -71,7 +45,7 @@ import org.springframework.context.support.GenericApplicationContext;
  */
 @Entity
 @Table(name = "pipeline_model")
-public class Pipeline implements Resource, ApplicationEventPublisherAware {
+public class Pipeline implements Resource {
 
     /**
      * Unique ID for each pipeline
@@ -90,23 +64,17 @@ public class Pipeline implements Resource, ApplicationEventPublisherAware {
     private String description;
     @Transient
     private PipelineGraph graph;
-    /**
-     * Publisher instance responsible for publishing pipeline execution events.
-     */
-    @Transient
-    protected ApplicationEventPublisher eventPublisher;
 
     /**
      * Default constructor for JPA
      */
     public Pipeline() {
-        this.eventPublisher = new GenericApplicationContext();
     }
 
     public Pipeline(String name, String description) {
         this.name = name;
         this.description = description;
-        this.eventPublisher = new GenericApplicationContext();
+
     }
 
     public String getName() {
@@ -148,110 +116,91 @@ public class Pipeline implements Resource, ApplicationEventPublisherAware {
     }
 
     /**
-     * Returns the event publisher instance.
-     *
-     * @return
-     */
-    public ApplicationEventPublisher getEventPublisher() {
-        return eventPublisher;
-    }
-
-    /**
-     * Setter for event publisher instance.
-     *
-     * @param eventPublisher
-     */
-    @Override
-    public void setApplicationEventPublisher(ApplicationEventPublisher eventPublisher) {
-        this.eventPublisher = eventPublisher;
-    }
-
-    /**
      * Runs the pipeline.
      */
-    public void run() {
+    /*
+     public void run() {
 
-        long pipelineStart = System.currentTimeMillis();
-        String runId = UUID.randomUUID().toString();
-        final Map<String, Object> customData = new HashMap<String, Object>();
+     long pipelineStart = System.currentTimeMillis();
+     String runId = UUID.randomUUID().toString();
+     final Map<String, Object> customData = new HashMap<>();
 
-        DependencyGraph dependencyGraph = new DependencyGraph(graph);
-        GraphIterator iterator = new GraphIterator(dependencyGraph);
+     DependencyGraph dependencyGraph = new DependencyGraph(graph);
+     GraphIterator iterator = new GraphIterator(dependencyGraph);
 
-        //eventPublisher.publishEvent(new PipelineStartedEvent(this, runId, this));
+     //eventPublisher.publishEvent(new PipelineStartedEvent(this, runId, this));
 
-        while (iterator.hasNext()) {
+     while (iterator.hasNext()) {
 
-            Node node = iterator.next();
-            DPUInstance dpuInstance = node.getDpuInstance();
-            DPU dpu = dpuInstance.getDpu();
+     Node node = iterator.next();
+     DPUInstance dpuInstance = node.getDpuInstance();
+     DPU dpu = dpuInstance.getDpu();
 
-            Type dpuType = dpu.getType();
-            String dpuJarPath = dpu.getJarPath();
+     Type dpuType = dpu.getType();
+     String dpuJarPath = dpu.getJarPath();
 
-            Configuration configuration = dpuInstance.getInstanceConfig();
-            ModuleFacade moduleFacade = new ModuleFacade();
+     Configuration configuration = dpuInstance.getInstanceConfig();
+     ModuleFacade moduleFacade = new ModuleFacade();
 
-            switch (dpuType) {
+     switch (dpuType) {
 
-                case EXTRACTOR: {
+     case EXTRACTOR: {
 
-                    Extract extractor = moduleFacade.getInstanceExtract(dpuJarPath);
-                    extractor.setSettings(configuration);
+     Extract extractor = moduleFacade.getInstanceExtract(dpuJarPath);
+     extractor.setSettings(configuration);
 
-                    ExtractContext context = new ExtractContext(runId, customData);
+     ExtractContext context = new ExtractContext(runId, customData);
 
-                    try {
-                        long start = System.currentTimeMillis();
+     try {
+     long start = System.currentTimeMillis();
 
-                        extractor.extract(context);
-                        context.setDuration(System.currentTimeMillis() - start);
-                        eventPublisher.publishEvent(new ExtractCompletedEvent(extractor, context, this));
+     extractor.extract(context);
+     context.setDuration(System.currentTimeMillis() - start);
+     eventPublisher.publishEvent(new ExtractCompletedEvent(extractor, context, this));
 
-                    } catch (ExtractException ex) {
-                        eventPublisher.publishEvent(new ExtractFailedEvent(ex, extractor, context, this));
-                    }
-                }
+     } catch (ExtractException ex) {
+     eventPublisher.publishEvent(new ExtractFailedEvent(ex, extractor, context, this));
+     }
+     }
 
-                case TRANSFORMER: {
-                    Transform transformer = moduleFacade.getInstanceTransform(dpuJarPath);
-                    transformer.setSettings(configuration);
+     case TRANSFORMER: {
+     Transform transformer = moduleFacade.getInstanceTransform(dpuJarPath);
+     transformer.setSettings(configuration);
 
-                    TransformContext context = new TransformContext(runId, customData);
+     TransformContext context = new TransformContext(runId, customData);
 
-                    try {
-                        long start = System.currentTimeMillis();
+     try {
+     long start = System.currentTimeMillis();
 
-                        transformer.transform(context);
-                        context.setDuration(System.currentTimeMillis() - start);
-                        eventPublisher.publishEvent(new TransformCompletedEvent(transformer, context, this));
+     transformer.transform(context);
+     context.setDuration(System.currentTimeMillis() - start);
+     eventPublisher.publishEvent(new TransformCompletedEvent(transformer, context, this));
 
-                    } catch (TransformException ex) {
-                        eventPublisher.publishEvent(new TransformFailedEvent(ex, transformer, context, this));
-                    }
-                }
+     } catch (TransformException ex) {
+     eventPublisher.publishEvent(new TransformFailedEvent(ex, transformer, context, this));
+     }
+     }
 
-                case LOADER: {
-                    Load loader = moduleFacade.getInstanceLoader(dpuJarPath);
-                    loader.setSettings(configuration);
+     case LOADER: {
+     Load loader = moduleFacade.getInstanceLoader(dpuJarPath);
+     loader.setSettings(configuration);
 
-                    LoadContext context = new LoadContext(runId, customData);
-                    try {
-                        long start = System.currentTimeMillis();
+     LoadContext context = new LoadContext(runId, customData);
+     try {
+     long start = System.currentTimeMillis();
 
-                        loader.load(context);
-                        context.setDuration(System.currentTimeMillis() - start);
-                        eventPublisher.publishEvent(new LoadCompletedEvent(loader, context, this));
-                    } catch (LoadException ex) {
-                        eventPublisher.publishEvent(new LoadFailedEvent(ex, loader, context, this));
-                    }
-                }
-            }
-        }
+     loader.load(context);
+     context.setDuration(System.currentTimeMillis() - start);
+     eventPublisher.publishEvent(new LoadCompletedEvent(loader, context, this));
+     } catch (LoadException ex) {
+     eventPublisher.publishEvent(new LoadFailedEvent(ex, loader, context, this));
+     }
+     }
+     }
+     }
 
-        //eventPublisher.publishEvent(new PipelineCompletedEvent((System.currentTimeMillis() - pipelineStart), this, runId, this));
-    }
-
+     //eventPublisher.publishEvent(new PipelineCompletedEvent((System.currentTimeMillis() - pipelineStart), this, runId, this));
+     }*/
     @Override
     public String getResourceId() {
         return Pipeline.class.toString();
