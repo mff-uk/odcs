f37f412bdd3fa16ed8ffba01e919b6d45fbe535f
bogo777 <bogo777@gmail.com>
2013-05-08 15:00:32 +0200

    debugging view basic version(gui only)

--- frontend/src/main/java/cz/cuni/xrg/intlib/auxiliaries/ContainerFactory.java
+++ frontend/src/main/java/cz/cuni/xrg/intlib/auxiliaries/ContainerFactory.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.vaadin.data.Container;
 import com.vaadin.data.util.BeanContainer;
+import cz.cuni.xrg.intlib.commons.ExecutionMessage;
+import cz.cuni.xrg.intlib.commons.RDFTriple;
 
 import cz.cuni.xrg.intlib.commons.app.dpu.DPU;
 import cz.cuni.xrg.intlib.commons.app.pipeline.Pipeline;
@@ -16,12 +18,12 @@ import cz.cuni.xrg.intlib.commons.app.pipeline.Pipeline;
 public class ContainerFactory {
 
 	/**
-	 * Prevent from creating instance. 
+	 * Prevent from creating instance.
 	 */
 	private ContainerFactory() {
-		
+
 	}
-	
+
 	/**
 	 * Create container for Pipelines and fill it with given data.
 	 * @param data data for container
@@ -31,13 +33,13 @@ public class ContainerFactory {
 		BeanContainer<Integer, Pipeline> container = new BeanContainer<Integer, Pipeline>( Pipeline.class );
 		// set container id
 		container.setBeanIdProperty("id");
-		
+
 		for (Pipeline item : data) {
 			container.addBean(item);
 		}
 		return container;
 	}
-	
+
 	/**
 	 * Create container for DPUs and fill it with given data.
 	 * @param data data for container
@@ -47,11 +49,29 @@ public class ContainerFactory {
 		BeanContainer<Integer, DPU> container = new BeanContainer<Integer, DPU>( DPU.class );
 		// set container id
 		container.setBeanIdProperty("id");
-		
+
 		for (DPU item : data) {
 			container.addBean(item);
-		}		
+		}
 		return container;
-	}	
-	
+	}
+
+	public static Container CreateExecutionMessages(List<ExecutionMessage> data) {
+		BeanContainer<Integer, ExecutionMessage> container = new BeanContainer<Integer, ExecutionMessage>( ExecutionMessage.class);
+		container.setBeanIdProperty("id");
+		container.addAll(data);
+
+		return container;
+	}
+
+	public static Container CreateRDFData(List<RDFTriple> data) {
+		BeanContainer<Integer, RDFTriple> container = new BeanContainer<Integer, RDFTriple> (RDFTriple.class);
+		container.setBeanIdProperty("id");
+		container.addAll(data);
+
+		return container;
+	}
+
+
+
 }
