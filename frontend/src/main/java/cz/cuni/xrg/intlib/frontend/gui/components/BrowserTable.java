51438386341b2ef4595e15fcd4ca816fce925a13
bogo777 <bogo777@gmail.com>
2013-05-08 15:01:19 +0200

    debugging view basic version(gui only), new classes

---
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/components/BrowserTable.java
@@ -0,0 +1,35 @@
+package cz.cuni.xrg.intlib.frontend.gui.components;
+
+import com.vaadin.data.Container;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Table;
+import cz.cuni.xrg.intlib.auxiliaries.App;
+import cz.cuni.xrg.intlib.auxiliaries.ContainerFactory;
+import cz.cuni.xrg.intlib.commons.RDFTriple;
+import java.util.List;
+
+/**
+ *
+ * @author Bogo
+ */
+public class BrowserTable extends CustomComponent {
+
+	private Table dataTable;
+
+	public BrowserTable(List<RDFTriple> data) {
+
+		loadBrowserTable(data);
+		setCompositionRoot(dataTable);
+
+	}
+
+	private void loadBrowserTable(List<RDFTriple> data) {
+		dataTable = new Table();
+		Container container = ContainerFactory.CreateRDFData(data);
+		dataTable.setContainerDataSource(container);
+
+		dataTable.setVisibleColumns(new String[]{ "subject", "predicate", "object" });
+	}
+
+
+}
