653991b167cdb7da5b955baa904e7437fd6d7a3c
Petr Škoda <p.skoda@centrum.cz>
2013-05-10 16:51:13 +0200

    RecordType add conversion method from Type (commons.messaging)

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/dpu/execution/RecordType.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/dpu/execution/RecordType.java
@@ -1,5 +1,7 @@
 package cz.cuni.xrg.intlib.commons.app.dpu.execution;
 
+import cz.cuni.xrg.intlib.commons.message.Type;
+
 /**
  * Types for execution.record.
  * 
@@ -11,5 +13,24 @@ public enum RecordType {
 	,Log
 	,Info
 	,Warning
-	,Error
+	,Error;
+	
+	
+	/**
+	 * Convert message.Type to RecordType.
+	 */
+	public static RecordType fromMessageType(Type type) {
+		switch(type) {
+		case Debug:
+			return RecordType.Debug;
+		case Info:
+			return RecordType.Info;
+		case Warning:
+			return RecordType.Warning;
+		case Error:
+			return RecordType.Error;
+		default:
+			return RecordType.Info;
+		}		
+	}
 }
