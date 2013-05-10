1b2dbd402bfda3c12aed091c609e67af36f50c62
Petr Škoda <p.skoda@centrum.cz>
2013-05-10 12:27:05 +0200

    AppConfiguration update

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/AppConfiguration.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/AppConfiguration.java
@@ -2,7 +2,7 @@ package cz.cuni.xrg.intlib.commons.app;
 
 /**
  * Contains application configuration.
- *
+ * 
  * @author Petyr
  */
 public class AppConfiguration {
@@ -11,11 +11,29 @@ public class AppConfiguration {
 	 * Path to the dpu directory folder.
 	 */
 	private String dpuDirectory = "C:/Users/Bogo/Documents/NetBeansProjects/intlib";
-
+	
+	/**
+	 * Backend instance IP address.
+	 */
+	private String backendAddress = "127.0.0.1";
+	
+	/**
+	 * Port for communicating with backend.
+	 */
+	private Integer backendPort = 5010;
+	
 	public String getDpuDirectory() {
 		return this.dpuDirectory;
 	}
-
+	
+	public String getBackendAddress() {
+		return this.backendAddress;
+	}
+	
+	public Integer getBackendPort() {
+		return this.backendPort;
+	}
+	
 	public void setDpuDirectory(String dpuDirectory) {
 		this.dpuDirectory = dpuDirectory;
 	}
