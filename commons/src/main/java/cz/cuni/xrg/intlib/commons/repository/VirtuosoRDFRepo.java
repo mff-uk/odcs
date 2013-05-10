0152ac0bc549d3a2732320c0250172a8f2c08b18
Jiri Tomes <Tooomy@seznam.cz>
2013-05-10 20:07:38 +0200

    Add VirtuosoRDFRepo class (implements interface RDFDataRepository).

---
+++ commons/src/main/java/cz/cuni/xrg/intlib/commons/repository/VirtuosoRDFRepo.java
@@ -0,0 +1,59 @@
+package cz.cuni.xrg.intlib.commons.repository;
+
+import cz.cuni.xrg.intlib.commons.data.RDFDataRepository;
+import org.openrdf.repository.RepositoryException;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Jiri Tomes
+ */
+public class VirtuosoRDFRepo extends LocalRDFRepo implements RDFDataRepository {
+
+    private static VirtuosoRDFRepo virtuosoRepo = null;
+    /**
+     * Logging information about execution of method using openRDF.
+     */
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(VirtuosoRDFRepo.class);
+
+    /**
+     * Construct a VirtuosoRepository with a specified parameters.
+     * 
+     * 
+     */
+    public static VirtuosoRDFRepo createVirtuosoRDFRepo(String URL_Host_List, String user, String password, String defaultGraph) {
+        if (virtuosoRepo == null) {
+            virtuosoRepo = new VirtuosoRDFRepo(URL_Host_List, user, password, defaultGraph);
+        }
+        return virtuosoRepo;
+    }
+
+    /**
+     * Construct a VirtuosoRepository with a specified parameters.
+     *
+     * @param URL_Host_List the Virtuoso JDBC URL connection string or hostlist
+     * for poolled connection.
+     *
+     * @param user the database user on whose behalf the connection is being
+     * made.
+     *
+     * @param password the user's password.
+     *
+     * @param defaultGraph a default Graph name, used for Sesame calls, when
+     * contexts list is empty, exclude exportStatements, hasStatement,
+     * getStatements methods.
+     */
+    public VirtuosoRDFRepo(String URL_Host_List, String user, String password, String defaultGraph) {
+
+        //repository = new VirtuosoRepository(URL_Host_List, user, password,defaultGraph);
+
+        try {
+            repository.initialize();
+            logger.info("Virtuoso repository incicialized");
+
+        } catch (RepositoryException ex) {
+            logger.debug(ex.getMessage());
+
+        }
+    }
+}
