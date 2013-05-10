be4cf6735fbd13cfaa2d368599815ed2b0320638
Jiri Tomes <Tooomy@seznam.cz>
2013-05-10 22:03:08 +0200

    Move test classes and files from intlib-commons to backend.

--- backend/src/test/java/cz/cuni/xrg/intlib/backend/repository/LocalRDFRepoTest.java
+++ backend/src/test/java/cz/cuni/xrg/intlib/backend/repository/LocalRDFRepoTest.java
@@ -1,4 +1,4 @@
-package cz.cuni.xrg.intlib.commons.repository;
+package cz.cuni.xrg.intlib.backend.repository;
 
 import cz.cuni.xrg.intlib.commons.data.rdf.CannotOverwriteFileException;
 import java.io.File;
