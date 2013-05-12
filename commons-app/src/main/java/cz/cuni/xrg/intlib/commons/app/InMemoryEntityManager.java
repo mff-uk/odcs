3a8205de99fae4593b4363856235e51db5fb022e
Jan Vojt <jan@vojt.net>
2013-05-12 23:47:05 +0200

    Removed javax.persistence dependency (conflict w/ Hibernate).

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/InMemoryEntityManager.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/InMemoryEntityManager.java
@@ -5,19 +5,12 @@ import cz.cuni.xrg.intlib.commons.app.pipeline.Pipeline;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.FlushModeType;
-import javax.persistence.LockModeType;
-import javax.persistence.Query;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.metamodel.Metamodel;
 
 public class InMemoryEntityManager implements EntityManager {
 
@@ -201,6 +194,104 @@ public class InMemoryEntityManager implements EntityManager {
         // TODO Auto-generated method stub
     }
 
+	@Override
+	public <T> T find(Class<T> entityClass, Object primaryKey,
+			Map<String, Object> properties) {
+		return find(entityClass, primaryKey);
+	}
+
+	@Override
+	public <T> T find(Class<T> entityClass, Object primaryKey,
+			LockModeType lockMode) {
+		return find(entityClass, primaryKey);
+	}
+
+	@Override
+	public <T> T find(Class<T> entityClass, Object primaryKey,
+			LockModeType lockMode,
+			Map<String, Object> properties) {
+		return find(entityClass, primaryKey);
+	}
+
+	@Override
+	public void lock(Object entity, LockModeType lockMode,
+			Map<String, Object> properties) {
+	}
+
+	@Override
+	public void refresh(Object entity,
+			Map<String, Object> properties) {
+		refresh(entity);
+	}
+
+	@Override
+	public void refresh(Object entity, LockModeType lockMode) {
+		refresh(entity);
+	}
+
+	@Override
+	public void refresh(Object entity, LockModeType lockMode,
+			Map<String, Object> properties) {
+		refresh(entity);
+	}
+
+	@Override
+	public void detach(Object entity) {
+		repo.remove(entity);
+	}
+
+	@Override
+	public LockModeType getLockMode(Object entity) {
+		return LockModeType.NONE;
+	}
+
+	@Override
+	public void setProperty(String propertyName, Object value) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Map<String, Object> getProperties() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public <T> TypedQuery<T> createQuery(CriteriaQuery<T> criteriaQuery) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public <T> TypedQuery<T> createQuery(String qlString,
+			Class<T> resultClass) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public <T> TypedQuery<T> createNamedQuery(String name,
+			Class<T> resultClass) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> cls) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public EntityManagerFactory getEntityManagerFactory() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public CriteriaBuilder getCriteriaBuilder() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Metamodel getMetamodel() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
     /**
      * Stub to immitate transaction in tests.
      *
@@ -339,5 +430,104 @@ public class InMemoryEntityManager implements EntityManager {
         public Query setFlushMode(FlushModeType flushMode) {
             return this;
         }
+
+		@Override
+		public int getMaxResults() {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public int getFirstResult() {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Map<String, Object> getHints() {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public <T> Query setParameter(Parameter<T> param, T value) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Query setParameter(Parameter<Calendar> param, Calendar value,
+				TemporalType temporalType) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Query setParameter(Parameter<Date> param, Date value,
+				TemporalType temporalType) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Set<Parameter<?>> getParameters() {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Parameter<?> getParameter(String name) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public <T> Parameter<T> getParameter(String name,
+				Class<T> type) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Parameter<?> getParameter(int position) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public <T> Parameter<T> getParameter(int position,
+				Class<T> type) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public boolean isBound(Parameter<?> param) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public <T> T getParameterValue(Parameter<T> param) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Object getParameterValue(String name) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Object getParameterValue(int position) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public FlushModeType getFlushMode() {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public Query setLockMode(LockModeType lockMode) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public LockModeType getLockMode() {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public <T> T unwrap(Class<T> cls) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
     }
 }
