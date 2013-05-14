a57989103bdf6ea5cc8aaa510e82b683a3266d8f
Jiri Tomes <Tooomy@seznam.cz>
2013-05-15 00:42:48 +0200

    Modify VirtuosoRDFRepo and its tests.

--- backend/src/main/java/cz/cuni/xrg/intlib/backend/data/rdf/VirtuosoRDFRepo.java
+++ backend/src/main/java/cz/cuni/xrg/intlib/backend/data/rdf/VirtuosoRDFRepo.java
@@ -1,10 +1,32 @@
 package cz.cuni.xrg.intlib.backend.data.rdf;
 
+import static cz.cuni.xrg.intlib.backend.data.rdf.LocalRDFRepo.logger;
+import cz.cuni.xrg.intlib.commons.data.rdf.CannotOverwriteFileException;
 import cz.cuni.xrg.intlib.commons.data.rdf.RDFDataRepository;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
+import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.Rio;
 import org.slf4j.LoggerFactory;
 import virtuoso.sesame2.driver.VirtuosoRepository;
 
@@ -130,6 +152,420 @@ public class VirtuosoRDFRepo extends LocalRDFRepo implements RDFDataRepository {
         return newCopy;
     }
 
+    @Override
+    public void addTripleToRepository(String namespace, String subjectName, String predicateName, String objectName) {
+
+        Statement statement = createNewStatement(namespace, subjectName, predicateName, objectName);
+        addStatement(statement, graph);
+    }
+
+    private void addStatement(Statement statement, Resource... graphs) {
+
+        try {
+
+            RepositoryConnection conection = repository.getConnection();
+            if (graphs != null) {
+                conection.add(statement, graphs);
+            } else {
+                conection.add(statement);
+            }
+
+            conection.commit();
+            conection.close();
+
+        } catch (RepositoryException ex) {
+            logger.debug(ex.getMessage());
+
+
+        }
+
+    }
+
+    @Override
+    public void extractRDFfromXMLFileToRepository(String path, String suffix, String baseURI, boolean useSuffix) {
+
+        final String aceptedSuffix = suffix.toUpperCase();
+
+        FilenameFilter acceptedFileFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                if (name.toUpperCase().endsWith(aceptedSuffix)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+
+        File dirFile = new File(path);
+
+        if (path.toLowerCase().startsWith("http")) {
+
+            InputStreamReader inputStream;
+            try {
+
+                URL urlPath = new URL(path);
+
+                inputStream = new InputStreamReader(urlPath.openStream(), encode);
+                RDFFormat format = RDFFormat.forFileName(path, RDFFormat.RDFXML);
+
+                RepositoryConnection connection = repository.getConnection();
+                if (graph != null) {
+                    connection.add(inputStream, baseURI, format, graph);
+                } else {
+                    connection.add(inputStream, baseURI, format);
+                }
+
+
+                inputStream.close();
+
+            } catch (IOException | RepositoryException | RDFParseException ex) {
+                logger.debug(ex.getMessage());
+            }
+
+        }
+
+        if (dirFile.isDirectory()) {
+            File[] files;
+            if (useSuffix) {
+                files = dirFile.listFiles(acceptedFileFilter);
+            } else {
+                files = dirFile.listFiles();
+            }
+
+            if (files == null) {
+                return;
+            }
+
+            for (int i = 0; i < files.length; i++) {
+                File nextFile = files[i];
+                addFileToRepository(nextFile, baseURI, graph);
+            }
+
+        }
+        if (dirFile.isFile()) {
+            addFileToRepository(dirFile, baseURI, graph);
+        }
+
+    }
+
+    private void addFileToRepository(File dataFile, String baseURI, Resource... graphs) {
+        try {
+            RDFFormat fileFormat = RDFFormat.forFileName(dataFile.getAbsolutePath(), RDFFormat.RDFXML);
+            RepositoryConnection connection = repository.getConnection();
+
+            if (graphs != null) {
+                connection.add(dataFile, baseURI, fileFormat, graphs);
+            } else {
+                connection.add(dataFile, baseURI, fileFormat);
+            }
+
+            connection.commit();
+            connection.close();
+
+        } catch (RepositoryException | IOException | RDFParseException ex) {
+            logger.debug("Error by adding file to repository");
+        }
+    }
+
+    /**
+     * Load all triples in repository to defined file in defined RDF format.
+     *
+     * @param directoryPath
+     * @param fileName
+     * @param format
+     * @param canFileOverWrite
+     * @throws CannotOverwriteFileException
+     */
+    @Override
+    public void loadRDFfromRepositoryToXMLFile(String directoryPath, String fileName, org.openrdf.rio.RDFFormat format,
+            boolean canFileOverWrite, boolean isNameUnique) throws CannotOverwriteFileException {
+
+        final String slash = File.separator;
+
+        try {
+            if (!directoryPath.endsWith(slash)) {
+                directoryPath += slash;
+            }
+
+            File directory = new File(directoryPath);
+
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            File dataFile = new File(directoryPath + fileName);
+
+            if (!dataFile.exists()) {
+                createNewFile(dataFile);
+
+            } else {
+                if (isNameUnique) {
+
+                    String uniqueFileName = UniqueNameGenerator.getNextName(fileName);
+
+                    dataFile = new File(directoryPath + uniqueFileName);
+                    createNewFile(dataFile);
+
+                } else if (canFileOverWrite) {
+                    createNewFile(dataFile);
+                } else {
+                    logger.debug("File existed and cannot be overwritten");
+                    throw new CannotOverwriteFileException();
+                }
+
+            }
+
+
+            OutputStreamWriter os;
+            try {
+                os = new OutputStreamWriter(new FileOutputStream(dataFile.getAbsoluteFile()), encode);
+
+                RDFWriter writer = Rio.createWriter(format, os);
+
+                RepositoryConnection connection = repository.getConnection();
+                if (graph != null) {
+                    connection.export(writer, graph);
+                } else {
+                    connection.export(writer);
+                }
+
+
+                connection.commit();
+                connection.close();
+                os.close();
+
+            } catch (FileNotFoundException ex) {
+                logger.debug(ex.getMessage());
+            } catch (IOException ex) {
+                logger.debug(ex.getMessage());
+            }
+
+        } catch (RDFHandlerException | RepositoryException ex) {
+            logger.debug(ex.getMessage());
+        }
+    }
+
+    /**
+     * Load RDF data from repository to SPARQL endpointURL to the collection of
+     * URI graphs with endpoint authentisation (name,password).
+     *
+     * @param endpointURL
+     * @param defaultGraphURI
+     * @param userName
+     * @param password
+     */
+    @Override
+    public void loadtoSPARQLEndpoint(URL endpointURL, List<String> endpointGraphsURI, String userName, String password) {
+        try {
+
+            final int graphSize = endpointGraphsURI.size();
+            List<String> dataParts = this.getInsertPartsTriplesQuery(STATEMENTS_COUNT, this);
+            final int partsCount = dataParts.size();
+
+            boolean usePassword = (!userName.isEmpty() | !password.isEmpty());
+
+            if (usePassword) {
+
+                final String myName = userName;
+                final String myPassword = password;
+
+                Authenticator autentisator = new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(myName, myPassword.toCharArray());
+                    }
+                };
+
+                Authenticator.setDefault(autentisator);
+
+            }
+
+            RepositoryConnection connection = repository.getConnection();
+
+            for (int i = 0; i < graphSize; i++) {
+
+                for (int j = 0; j < partsCount; j++) {
+
+                    final String endpointGraph = endpointGraphsURI.get(i).replace(" ", "+");
+                    final String query = dataParts.get(j);
+                    final String myquery = URLEncoder.encode(query, encode);
+
+                    URL call = new URL(endpointURL.toString() + "?default-graph-uri=" + endpointGraph + "&query=" + myquery);
+
+                    HttpURLConnection httpConnection = (HttpURLConnection) call.openConnection();
+                    httpConnection.setRequestProperty("Content-type", "text/xml");
+
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+                        reader.close();
+
+                        final String processing = String.valueOf(j + 1) + "/" + String.valueOf(partsCount);
+
+                        logger.debug("Data " + processing + " part loaded successful");
+                    } catch (IOException ex) {
+                        logger.debug(
+                                "Cannot open http connection stream at '"
+                                + call.toString()
+                                + "' - data not loaded");
+                        logger.debug(ex.getMessage());
+                    } finally {
+                        httpConnection.disconnect();
+                    }
+
+                }
+            }
+
+            connection.close();
+
+        } catch (RepositoryException | IOException ex) {
+            logger.debug(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void extractfromSPARQLEndpoint(URL endpointURL, List<String> endpointGraphsURI, String query, String hostName, String password) {
+        try {
+
+            final RDFFormat format = RDFFormat.N3;
+            final int graphSize = endpointGraphsURI.size();
+            final String myquery = query.replace(" ", "+");
+            final String encoder = URLEncoder.encode(format.getDefaultMIMEType(), encode);
+
+            RepositoryConnection connection = repository.getConnection();
+
+            for (int i = 0; i < graphSize; i++) {
+
+                final String endpointGraph = endpointGraphsURI.get(i).replace(" ", "+");
+
+                URL call = new URL(endpointURL.toString() + "?default-graph-uri=" + endpointGraph + "&query=" + myquery + "&format=" + encoder);
+
+                HttpURLConnection httpConnection = (HttpURLConnection) call.openConnection();
+                httpConnection.addRequestProperty("Accept", format.getDefaultMIMEType());
+
+                boolean usePassword = (!hostName.isEmpty() | !password.isEmpty());
+
+                if (usePassword) {
+
+                    final String myName = hostName;
+                    final String myPassword = password;
+
+                    Authenticator autentisator = new Authenticator() {
+                        @Override
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(myName, myPassword.toCharArray());
+                        }
+                    };
+
+                    Authenticator.setDefault(autentisator);
+
+                }
+
+                try (InputStreamReader inputStreamReader = new InputStreamReader(httpConnection.getInputStream(), encode)) {
+                    if (graph != null) {
+                        connection.add(inputStreamReader, endpointGraph, format, graph);
+                    } else {
+                        connection.add(inputStreamReader, endpointGraph, format);
+                    }
+                }
+            }
+
+            connection.close();
+
+
+        } catch (IOException ex) {
+            logger.debug("Can not open http connection stream");
+            logger.debug(ex.getMessage());
+        } catch (Exception ex) {
+            logger.debug(ex.getMessage());
+        }
+    }
+
+    @Override
+    public long getTripleCountInRepository() {
+        long size = 0;
+
+        if (repository != null) {
+            try {
+                RepositoryConnection connection = repository.getConnection();
+                if (graph != null) {
+                    size = connection.size(graph);
+                } else {
+                    size = connection.size();
+                }
+                connection.close();
+
+            } catch (RepositoryException ex) {
+                logger.debug(ex.getMessage());
+            }
+
+        }
+        return size;
+    }
+
+    @Override
+    public void cleanAllRepositoryData() {
+        try {
+            RepositoryConnection connection = repository.getConnection();
+            if (graph != null) {
+                connection.clear(graph);
+            } else {
+                connection.clear();
+            }
+
+            connection.commit();
+            connection.close();
+
+        } catch (RepositoryException ex) {
+            logger.debug(ex.getMessage());
+        }
+    }
+
+    @Override
+    public boolean isTripleInRepository(String namespace, String subjectName,
+            String predicateName, String objectName) {
+        boolean hasTriple = false;
+
+        if (repository != null) {
+            try {
+                Statement statement = createNewStatement(namespace, subjectName, predicateName, objectName);
+                RepositoryConnection connection = repository.getConnection();
+                if (graph != null) {
+                    hasTriple = connection.hasStatement(statement, true, graph);
+                } else {
+                    hasTriple = connection.hasStatement(statement, true);
+                }
+
+                connection.close();
+
+            } catch (RepositoryException ex) {
+                logger.debug(ex.getMessage());
+            }
+        }
+        return hasTriple;
+    }
+
+    @Override
+    public List<Statement> getRepositoryStatements() {
+        List<Statement> statemens = new ArrayList<>();
+
+        if (repository != null) {
+            try {
+                RepositoryConnection connection = repository.getConnection();
+                if (graph != null) {
+                    statemens = connection.getStatements(null, null, null, true, graph).asList();
+                } else {
+                    statemens = connection.getStatements(null, null, null, true).asList();
+                }
+
+            } catch (RepositoryException ex) {
+                logger.debug(ex.getMessage());
+            }
+        }
+
+        return statemens;
+    }
+
     /**
      * Creates read only copy of instance Virtuoso repository.
      */
@@ -141,6 +577,4 @@ public class VirtuosoRDFRepo extends LocalRDFRepo implements RDFDataRepository {
         //return newCopy;
         setReadOnly(true);
     }
-
-    
 }
--- backend/src/test/java/cz/cuni/xrg/intlib/backend/repository/VirtuosoTest.java
+++ backend/src/test/java/cz/cuni/xrg/intlib/backend/repository/VirtuosoTest.java
@@ -1,9 +1,11 @@
 package cz.cuni.xrg.intlib.backend.repository;
 
 import cz.cuni.xrg.intlib.backend.data.rdf.VirtuosoRDFRepo;
+import cz.cuni.xrg.intlib.commons.data.rdf.CannotOverwriteFileException;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.openrdf.model.Resource;
+import org.openrdf.rio.RDFFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,22 +13,26 @@ import org.slf4j.LoggerFactory;
  *
  * @author Jiri Tomes
  */
-public class VirtuosoTest {
+public class VirtuosoTest extends LocalRDFRepoTest {
 
     public static final String HOSTNAME = "localhost";
     public static final String PORT = "1111";
     public static final String USERNAME = "dba";
     public static final String PASSWORD = "dba";
     public static final String DEFAUTLGRAPH = "http://default";
-    private static VirtuosoRDFRepo virtuosoRepo;
-    private static final Logger logger = LoggerFactory.getLogger(VirtuosoTest.class);
-
+    
     @BeforeClass
-    public static void setUp() {
-        virtuosoRepo = VirtuosoRDFRepo.createVirtuosoRDFRepo(HOSTNAME, PORT, USERNAME, PASSWORD, DEFAUTLGRAPH);
+    public static void setUpVirtuoso() {
+        rdfRepo = VirtuosoRDFRepo.createVirtuosoRDFRepo(HOSTNAME, PORT, USERNAME, PASSWORD, DEFAUTLGRAPH);
+        logger = LoggerFactory.getLogger(VirtuosoTest.class);
+    }
+    
+    @AfterClass
+    public static void cleaning() {
+        rdfRepo.cleanAllRepositoryData();
     }
 
-    @Test
+    /*@Test
     public void isRepositoryCreated() {
         assertNotNull(virtuosoRepo);
     }
@@ -63,6 +69,262 @@ public class VirtuosoTest {
         testNewTriple(namespace, subjectName, predicateName, objectName);
     }
 
+    @Test
+    public void isRepositoryEmpty() {
+        virtuosoRepo.cleanAllRepositoryData();
+        assertEquals(0, virtuosoRepo.getTripleCountInRepository());
+    }
+    
+    @Test
+    public void loadRDFtoXMLFile() {
+
+        String fileName = "RDF_output.rdf";
+        RDFFormat format = RDFFormat.RDFXML;
+        boolean canBeOverWriten = true;
+        boolean isNameUnique = false;
+
+        try {
+            virtuosoRepo.loadRDFfromRepositoryToXMLFile(
+                    outDir.toString(), fileName, format, canBeOverWriten, isNameUnique);
+
+        } catch (CannotOverwriteFileException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void loadRDFtoN3File() {
+
+        String fileName = "N3_output.n3";
+        RDFFormat format = RDFFormat.N3;
+        boolean canBeOverWriten = true;
+        boolean isNameUnique = false;
+
+        try {
+            localRepo.loadRDFfromRepositoryToXMLFile(
+                    outDir.toString(), fileName, format, canBeOverWriten, isNameUnique);
+
+        } catch (CannotOverwriteFileException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void loadRDFtoTRIGFile() {
+
+        String fileName = "TRIG_output.trig";
+        RDFFormat format = RDFFormat.TRIG;
+        boolean canBeOverWriten = true;
+        boolean isNameUnique = false;
+
+        try {
+            localRepo.loadRDFfromRepositoryToXMLFile(
+                    outDir.toString(), fileName, format, canBeOverWriten, isNameUnique);
+
+        } catch (CannotOverwriteFileException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void loadRDFtoTURTLEFile() {
+
+        String fileName = "TURTLE_output.ttl";
+        RDFFormat format = RDFFormat.TURTLE;
+        boolean canBeOverWriten = true;
+        boolean isNameUnique = false;
+
+        try {
+            localRepo.loadRDFfromRepositoryToXMLFile(
+                    outDir.toString(), fileName, format, canBeOverWriten, isNameUnique);
+
+        } catch (CannotOverwriteFileException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void loadOverWriteFail() {
+
+        String fileName = "CanNotOverWrite_output.rdf";
+        RDFFormat format = RDFFormat.RDFXML;
+
+        try {
+            localRepo.loadRDFfromRepositoryToXMLFile(outDir.toString(), fileName, format);
+            localRepo.loadRDFfromRepositoryToXMLFile(outDir.toString(), fileName, format);
+            fail();
+
+        } catch (CannotOverwriteFileException ex) {
+            // test passed
+        }
+    }
+
+    @Test
+    public void extractRDFFilesToRepository() {
+
+        String suffix = ".rdf";
+        String baseURI = "";
+        boolean useSuffix = true;
+
+        long size = localRepo.getTripleCountInRepository();
+
+        localRepo.extractRDFfromXMLFileToRepository(
+                testFileDir, suffix, baseURI, useSuffix);
+
+        long newSize = localRepo.getTripleCountInRepository();
+
+        assertTrue(newSize > size);
+    }
+
+    @Test
+    public void extractN3FilesToRepository() {
+
+        String suffix = ".n3";
+        String baseURI = "";
+        boolean useSuffix = true;
+
+        long size = localRepo.getTripleCountInRepository();
+
+        localRepo.extractRDFfromXMLFileToRepository(
+                testFileDir, suffix, baseURI, useSuffix);
+
+        long newSize = localRepo.getTripleCountInRepository();
+
+        assertTrue(newSize > size);
+    }
+
+    @Test
+    public void loadAllToXMLfile() {
+
+        String fileName = "AllData_output.rdf";
+        RDFFormat format = RDFFormat.RDFXML;
+        boolean canBeOverWriten = true;
+        boolean isNameUnique = false;
+
+        try {
+            localRepo.loadRDFfromRepositoryToXMLFile(
+                    outDir.toString(), fileName, format, canBeOverWriten, isNameUnique);
+
+        } catch (CannotOverwriteFileException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void transformUsingSPARQLUpdate() {
+
+        String namespace = "http://sport/hockey/";
+        String subjectName = "Jagr";
+        String predicateName = "playes_in";
+        String objectName = "Dalas_Stars";
+
+        String updateQuery = "DELETE { ?who ?what 'Dalas_Stars' }"
+                + "INSERT { ?who ?what 'Boston_Bruins' }"
+                + "WHERE { ?who ?what 'Dalas_Stars' }";
+
+        localRepo.addTripleToRepository(
+                namespace, subjectName, predicateName, objectName);
+
+        boolean beforeUpdate = localRepo.isTripleInRepository(
+                namespace, subjectName, predicateName, objectName);
+        assertTrue(beforeUpdate);
+
+        localRepo.transformUsingSPARQL(updateQuery);
+
+        boolean afterUpdate = localRepo.isTripleInRepository(
+                namespace, subjectName, predicateName, objectName);
+        assertFalse(afterUpdate);
+    }
+
+    private void TEDextractFile1ToRepository() {
+
+        String suffix = "ted4.ttl";
+        String baseURI = "";
+        boolean useSuffix = true;
+
+        long size = localRepo.getTripleCountInRepository();
+
+        localRepo.extractRDFfromXMLFileToRepository(
+                testFileDir, suffix, baseURI, useSuffix);
+
+        long newSize = localRepo.getTripleCountInRepository();
+
+        assertTrue(newSize > size);
+    }
+
+    private void TEDextractFile2ToRepository() {
+
+        String suffix = "ted4b.ttl";
+        String baseURI = "";
+        boolean useSuffix = true;
+
+        long size = localRepo.getTripleCountInRepository();
+
+        localRepo.extractRDFfromXMLFileToRepository(
+                testFileDir, suffix, baseURI, useSuffix);
+
+        long newSize = localRepo.getTripleCountInRepository();
+
+        boolean triplesAdded = newSize > size;
+
+        assertTrue(triplesAdded);
+    }
+
+    private void TEDTransformSPARQL() {
+
+        String updateQuery = "PREFIX dc: <http://purl.org/dc/elements/1.1/>"
+                + "INSERT DATA"
+                + "{ "
+                + "<http://example/book1> dc:title \"A new book\" ."
+                + "}";
+
+        localRepo.transformUsingSPARQL(updateQuery);
+
+    }
+
+    private void TEDloadtoTTLFile() {
+
+        String fileName = "output-ted-test.ttl";
+        RDFFormat format = RDFFormat.TURTLE;
+        boolean canBeOverWriten = true;
+        boolean isNameUnique = false;
+
+        try {
+            localRepo.loadRDFfromRepositoryToXMLFile(
+                    outDir.toString(), fileName, format, canBeOverWriten, isNameUnique);
+
+        } catch (CannotOverwriteFileException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void TEDPipelineTest() {
+        TEDextractFile1ToRepository();
+        TEDextractFile2ToRepository();
+        TEDTransformSPARQL();
+        TEDloadtoTTLFile();
+
+        long addedData = localRepo.getTripleCountInRepository();
+
+        assertTrue(addedData > 0);
+    }
+
+    @Test
+    public void SecondUpdateQueryTest() {
+
+        String updateQuery = "prefix s: <http://schema.org/>"
+                + "DELETE {?s s:streetAddress ?o}"
+                + "INSERT {?s s:streetAddress ?x}"
+                + "WHERE {"
+                + "{SELECT ?s ?o ?x"
+                + "WHERE {{?s s:streetAddress ?o}}} FILTER (BOUND(?x))}";
+
+        localRepo.transformUsingSPARQL(updateQuery);
+    }
+
+    
+
     private void testNewTriple(String namespace,
             String subjectName,
             String predicateName,
@@ -70,18 +332,18 @@ public class VirtuosoTest {
 
         Resource graph = virtuosoRepo.getGraph();
 
-        long size = virtuosoRepo.getTripleCountInRepository(graph);
+        long size = virtuosoRepo.getTripleCountInRepository();
         boolean isInRepository = virtuosoRepo.isTripleInRepository(
-                namespace, subjectName, predicateName, objectName, graph);
+                namespace, subjectName, predicateName, objectName);
 
         virtuosoRepo.addTripleToRepository(
-                namespace, subjectName, predicateName, objectName, graph);
-        long expectedSize = virtuosoRepo.getTripleCountInRepository(graph);
+                namespace, subjectName, predicateName, objectName);
+        long expectedSize = virtuosoRepo.getTripleCountInRepository();
 
         if (isInRepository) {
             assertEquals(expectedSize, size);
         } else {
             assertEquals(expectedSize, size + 1L);
         }
-    }
+    }*/
 }
