2f867419b2ba90e61c4767dc23b88dec063ed9f9
Petr Škoda <p.skoda@centrum.cz>
2013-05-18 16:20:03 +0200

    DataUnitType update

--- commons/src/main/java/cz/cuni/xrg/intlib/commons/data/DataUnitType.java
+++ commons/src/main/java/cz/cuni/xrg/intlib/commons/data/DataUnitType.java
@@ -11,15 +11,37 @@ public enum DataUnitType {
 	 * General RDF type, the application select repository type.
 	 * see @{link RDFDataRepository}
 	 */
-	RDF,
+	RDF(1),
 	/**
 	 * RDF data unit type.
 	 * see @{link RDFDataRepository}
 	 */
-	RDF_Local,
+	RDF_Local(1),
 	/**
 	 * RDF data unit type.
 	 * see @{link RDFDataRepository}
 	 */
-	RDF_Virtuoso
+	RDF_Virtuoso(1);
+	
+	/**
+	 * Determine DataUnit group, the types in the same 
+	 * group can be recast.
+	 */
+	private int group;
+	
+	private DataUnitType(int group) {
+		this.group = group;
+	}
+	
+	/**
+	 * Check if the source DataUnit sub-type with given can be cast to the 
+	 * target DataUnit sub-type.
+	 * @param source Type of source.
+	 * @param target Type of target.
+	 * @return
+	 */
+	public static boolean canCast(DataUnitType source, DataUnitType target) {
+		return source == target || source.group == target.group;
+	}
+	
 }
