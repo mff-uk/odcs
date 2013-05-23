fb116f4756eec917ee4175e222111be85e0bb40b
Petr Škoda <p.skoda@centrum.cz>
2013-05-23 16:44:24 +0200

    Quick hack ..

---
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/components/IntlibPagedTable.java
@@ -0,0 +1,13 @@
+package cz.cuni.xrg.intlib.frontend.gui.components;
+
+import com.jensjansson.pagedtable.PagedTable;
+
+/**
+ * Super all saving class.
+ * 
+ * @author Petyr
+ *
+ */
+public class IntlibPagedTable extends PagedTable {
+
+}
