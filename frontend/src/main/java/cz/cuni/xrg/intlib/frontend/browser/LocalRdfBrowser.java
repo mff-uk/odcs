2d7a571f1c5f3eed3289392a9f3255a311a0083d
Jiri Tomes <Tooomy@seznam.cz>
2013-05-30 16:34:11 +0200

    Formated classes in frontend.

--- frontend/src/main/java/cz/cuni/xrg/intlib/frontend/browser/LocalRdfBrowser.java
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/browser/LocalRdfBrowser.java
@@ -16,84 +16,88 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Implementation of browser for {@link cz.cuni.xrg.intlib.backend.data.rdf.LocalRDF}.
+ * Implementation of browser for
+ * {@link cz.cuni.xrg.intlib.backend.data.rdf.LocalRDF}.
  *
  * @author Petyr
  *
  */
 class LocalRdfBrowser extends DataUnitBrowser {
 
-	private List<RDFTriple> data = null;
-	private IntlibPagedTable dataTable;
+    private List<RDFTriple> data = null;
+    private IntlibPagedTable dataTable;
 
-	@Override
-	public void loadDataUnit(File directory, String dumpDirName) {
-		Logger logger = LoggerFactory.getLogger(LocalRdfBrowser.class);
+    @Override
+    public void loadDataUnit(File directory, String dumpDirName) {
+        Logger logger = LoggerFactory.getLogger(LocalRdfBrowser.class);
 
-		// FileName is from backend LocalRdf.dumpName = "dump_dat.ttl"; .. store somewhere else ?
-		logger.debug("Create LocalRDFRepo in directory={} dumpDirname={}", directory.toString(), dumpDirName);
+        // FileName is from backend LocalRdf.dumpName = "dump_dat.ttl"; .. store somewhere else ?
+        logger.debug("Create LocalRDFRepo in directory={} dumpDirname={}", directory.toString(), dumpDirName);
 
         LocalRDFRepo repository = new LocalRDFRepo(directory.getAbsolutePath(), dumpDirName);
-		File dumpFile = new File(directory, "dump_dat.ttl");
-		repository.load(dumpFile);
-		data = repository.getRDFTriplesInRepository();
+        File dumpFile = new File(directory, "dump_dat.ttl");
+        try {
+            repository.load(dumpFile);
+        } catch (Exception e) {
+            
+        }
+        data = repository.getRDFTriplesInRepository();
 
-                logger.debug("Number of triples: {}", data.size());
+        logger.debug("Number of triples: {}", data.size());
 
-                repository.shutDown();
+        repository.shutDown();
 
-		//data = buildStubRDFData();
-
-
+        //data = buildStubRDFData();
 
 
-	}
 
-	@Override
-	public void enter() {
-		VerticalLayout mainLayout = new VerticalLayout();
-		loadBrowserTable(data);
-		dataTable.setWidth("100%");
-		dataTable.setHeight("100%");
-		mainLayout.addComponent(dataTable);
-		mainLayout.addComponent(dataTable.createControls());
-		setCompositionRoot(mainLayout);
-	}
 
-	private void loadBrowserTable(List<RDFTriple> data) {
-		dataTable = new IntlibPagedTable();
-		Container container = ContainerFactory.CreateRDFData(data);
-		dataTable.setContainerDataSource(container);
+    }
 
-		dataTable.setVisibleColumns(new String[]{ "subject", "predicate", "object" });
-	}
+    @Override
+    public void enter() {
+        VerticalLayout mainLayout = new VerticalLayout();
+        loadBrowserTable(data);
+        dataTable.setWidth("100%");
+        dataTable.setHeight("100%");
+        mainLayout.addComponent(dataTable);
+        mainLayout.addComponent(dataTable.createControls());
+        setCompositionRoot(mainLayout);
+    }
 
-	private List<RDFTriple> buildStubRDFData() {
+    private void loadBrowserTable(List<RDFTriple> data) {
+        dataTable = new IntlibPagedTable();
+        Container container = ContainerFactory.CreateRDFData(data);
+        dataTable.setContainerDataSource(container);
 
-		List<RDFTriple> rdfTripleList = new ArrayList<>();
-/*
-		rdfTripleList.add(new RDFTriple(1, "rdf:Description", "rdf:about", "http://www.recshop.fake/cd/Empire Burlesque"));
-		rdfTripleList.add(new RDFTriple(2, "rdf:Description", "cd:artist", "Bob Dylan"));
-		rdfTripleList.add(new RDFTriple(3, "rdf:Description", "cd:country", "USA"));
-		rdfTripleList.add(new RDFTriple(4, "rdf:Description", "cd:company", "Columbia"));
-		rdfTripleList.add(new RDFTriple(5, "rdf:Description", "cd:price", "10.90"));
-		rdfTripleList.add(new RDFTriple(6, "rdf:Description", "cd:year", "1985"));
-*/
-		return rdfTripleList;
+        dataTable.setVisibleColumns(new String[]{"subject", "predicate", "object"});
+    }
 
-	}
+    private List<RDFTriple> buildStubRDFData() {
 
-	@Override
-	public void loadDataUnit(File directory) throws Exception {
-		// FileName is from backend LocalRdf.dumpName = "dump_dat.ttl"; .. store somewhere else ?
-		LoggerFactory.getLogger(LocalRdfBrowser.class).debug("Create LocalRDFRepo in directory {}", directory.toString());
-		LocalRDFRepo repository = new LocalRDFRepo(directory.toString(), "dump_dat.ttl");
-		// TODO Petyr, Jirka : load repository from folder ..
-		// get triples
-		data = repository.getRDFTriplesInRepository();
+        List<RDFTriple> rdfTripleList = new ArrayList<>();
+        /*
+         rdfTripleList.add(new RDFTriple(1, "rdf:Description", "rdf:about", "http://www.recshop.fake/cd/Empire Burlesque"));
+         rdfTripleList.add(new RDFTriple(2, "rdf:Description", "cd:artist", "Bob Dylan"));
+         rdfTripleList.add(new RDFTriple(3, "rdf:Description", "cd:country", "USA"));
+         rdfTripleList.add(new RDFTriple(4, "rdf:Description", "cd:company", "Columbia"));
+         rdfTripleList.add(new RDFTriple(5, "rdf:Description", "cd:price", "10.90"));
+         rdfTripleList.add(new RDFTriple(6, "rdf:Description", "cd:year", "1985"));
+         */
+        return rdfTripleList;
 
-                repository.shutDown();
-		//data = buildStubRDFData();
-	}
+    }
 
+    @Override
+    public void loadDataUnit(File directory) throws Exception {
+        // FileName is from backend LocalRdf.dumpName = "dump_dat.ttl"; .. store somewhere else ?
+        LoggerFactory.getLogger(LocalRdfBrowser.class).debug("Create LocalRDFRepo in directory {}", directory.toString());
+        LocalRDFRepo repository = new LocalRDFRepo(directory.toString(), "dump_dat.ttl");
+        // TODO Petyr, Jirka : load repository from folder ..
+        // get triples
+        data = repository.getRDFTriplesInRepository();
+
+        repository.shutDown();
+        //data = buildStubRDFData();
+    }
 }
--- frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/components/QueryView.java
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/components/QueryView.java
@@ -4,7 +4,6 @@ import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import cz.cuni.xrg.intlib.commons.DpuType;
-import cz.cuni.xrg.intlib.commons.app.pipeline.PipelineExecution;
 import cz.cuni.xrg.intlib.commons.app.rdf.LocalRDFRepo;
 import java.io.File;
 import java.util.HashMap;
@@ -21,114 +20,120 @@ import org.slf4j.LoggerFactory;
  */
 public class QueryView extends CustomComponent {
 
-	private DebuggingView parent;
-	private TextArea queryText;
-	private IntlibPagedTable resultTable;
-	private NativeSelect graphSelect;
+    private DebuggingView parent;
+    private TextArea queryText;
+    private IntlibPagedTable resultTable;
+    private NativeSelect graphSelect;
 
-	public QueryView(DebuggingView parent) {
-		this.parent = parent;
-		VerticalLayout mainLayout = new VerticalLayout();
+    public QueryView(DebuggingView parent) {
+        this.parent = parent;
+        VerticalLayout mainLayout = new VerticalLayout();
 
-		HorizontalLayout topLine = new HorizontalLayout();
+        HorizontalLayout topLine = new HorizontalLayout();
 
-		graphSelect = new NativeSelect("Graph:");
-		graphSelect.setImmediate(true);
+        graphSelect = new NativeSelect("Graph:");
+        graphSelect.setImmediate(true);
 //		graphSelect.addItem("Input Graph");
 //		graphSelect.addItem("Output Graph");
-		topLine.addComponent(graphSelect);
+        topLine.addComponent(graphSelect);
 
-		Button queryButton = new Button("Query");
-		queryButton.addClickListener(new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				Map<String, List<String>> data = query();
-				IndexedContainer container = buildDataSource(data);
-				resultTable.setContainerDataSource(container);
-			}
-		});
-		topLine.addComponent(queryButton);
-		topLine.setComponentAlignment(queryButton, Alignment.BOTTOM_RIGHT);
-		topLine.setSpacing(true);
-		mainLayout.addComponent(topLine);
+        Button queryButton = new Button("Query");
+        queryButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                Map<String, List<String>> data = query();
+                IndexedContainer container = buildDataSource(data);
+                resultTable.setContainerDataSource(container);
+            }
+        });
+        topLine.addComponent(queryButton);
+        topLine.setComponentAlignment(queryButton, Alignment.BOTTOM_RIGHT);
+        topLine.setSpacing(true);
+        mainLayout.addComponent(topLine);
 
-		queryText = new TextArea("SPARQL Query:");
-		queryText.setWidth("100%");
-		queryText.setHeight("30%");
-		mainLayout.addComponent(queryText);
+        queryText = new TextArea("SPARQL Query:");
+        queryText.setWidth("100%");
+        queryText.setHeight("30%");
+        mainLayout.addComponent(queryText);
 
-		//TODO: Change to table - resolve container issue
-		resultTable = new IntlibPagedTable();
-		resultTable.setWidth("100%");
-		resultTable.setHeight("60%");
-		mainLayout.addComponent(resultTable);
-		mainLayout.addComponent(resultTable.createControls());
-
-
-		mainLayout.setSizeFull();
-		setCompositionRoot(mainLayout);
-	}
-
-	private Map<String, List<String>> query() {
-		boolean onInputGraph = graphSelect.getValue().equals("Input Graph");
-		String query = queryText.getValue();
-		String repoPath = parent.getRepositoryPath(onInputGraph);
-		File repoDir = parent.getRepositoryDirectory(onInputGraph);
-
-		if(repoPath == null || repoDir == null) {
-			return new HashMap<>();
-		}
-
-		Logger logger = LoggerFactory.getLogger(QueryView.class);
-
-		// FileName is from backend LocalRdf.dumpName = "dump_dat.ttl"; .. store somewhere else ?
-		logger.debug("Create LocalRDFRepo in directory={} dumpDirname={}", repoDir.toString(), repoPath);
-
-		LocalRDFRepo repository = new LocalRDFRepo(repoDir.getAbsolutePath(), repoPath);
-		File dumpFile = new File(repoDir, "dump_dat.ttl");
-		repository.load(dumpFile);
-
-		Map<String, List<String>> data = repository.makeQueryOverRepository(query);
-		repository.shutDown();
-		return data;
-	}
-
-	private IndexedContainer buildDataSource(Map<String, List<String>> data) {
-		IndexedContainer result = new IndexedContainer();
-		if(data.isEmpty()) {
-			return result;
-		}
-
-		Set<String> columns = data.keySet();
-
-		result.addContainerProperty("#", Integer.class, "");
-		for (String column : columns) {
-			//		if (p.equals("exeid")==false)
-			result.addContainerProperty(column, String.class, "");
-		}
+        //TODO: Change to table - resolve container issue
+        resultTable = new IntlibPagedTable();
+        resultTable.setWidth("100%");
+        resultTable.setHeight("60%");
+        mainLayout.addComponent(resultTable);
+        mainLayout.addComponent(resultTable.createControls());
 
 
-		int count = data.get(columns.iterator().next()).size();
+        mainLayout.setSizeFull();
+        setCompositionRoot(mainLayout);
+    }
 
-		for (int i = 0; i < count; i++) {
-			Object num = result.addItem();
-			result.getContainerProperty(num, "#").setValue(i);
-			for (String column : columns) {
-				String value = data.get(column).get(i);
-				result.getContainerProperty(num, column).setValue(value);
-			}
-		}
+    private Map<String, List<String>> query() {
+        boolean onInputGraph = graphSelect.getValue().equals("Input Graph");
+        String query = queryText.getValue();
+        String repoPath = parent.getRepositoryPath(onInputGraph);
+        File repoDir = parent.getRepositoryDirectory(onInputGraph);
 
-		return result;
-	}
+        if (repoPath == null || repoDir == null) {
+            return new HashMap<>();
+        }
 
-	public void setGraphs(DpuType type) {
-		graphSelect.removeAllItems();
-		if(type != DpuType.EXTRACTOR) {
-			graphSelect.addItem("Input Graph");
-		}
-		if(type != DpuType.LOADER) {
-			graphSelect.addItem("Output Graph");
-		}
-	}
+        Logger logger = LoggerFactory.getLogger(QueryView.class);
+
+        // FileName is from backend LocalRdf.dumpName = "dump_dat.ttl"; .. store somewhere else ?
+        logger.debug("Create LocalRDFRepo in directory={} dumpDirname={}", repoDir.toString(), repoPath);
+
+        LocalRDFRepo repository = new LocalRDFRepo(repoDir.getAbsolutePath(), repoPath);
+        File dumpFile = new File(repoDir, "dump_dat.ttl");
+
+        try {
+            repository.load(dumpFile);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        
+        Map<String, List<String>> data = repository.makeQueryOverRepository(query);
+        repository.shutDown();
+        return data;
+    }
+
+    private IndexedContainer buildDataSource(Map<String, List<String>> data) {
+        IndexedContainer result = new IndexedContainer();
+        if (data.isEmpty()) {
+            return result;
+        }
+
+        Set<String> columns = data.keySet();
+
+        result.addContainerProperty("#", Integer.class, "");
+        for (String column : columns) {
+            //		if (p.equals("exeid")==false)
+            result.addContainerProperty(column, String.class, "");
+        }
+
+
+        int count = data.get(columns.iterator().next()).size();
+
+        for (int i = 0; i < count; i++) {
+            Object num = result.addItem();
+            result.getContainerProperty(num, "#").setValue(i);
+            for (String column : columns) {
+                String value = data.get(column).get(i);
+                result.getContainerProperty(num, column).setValue(value);
+            }
+        }
+
+        return result;
+    }
+
+    public void setGraphs(DpuType type) {
+        graphSelect.removeAllItems();
+        if (type != DpuType.EXTRACTOR) {
+            graphSelect.addItem("Input Graph");
+        }
+        if (type != DpuType.LOADER) {
+            graphSelect.addItem("Output Graph");
+        }
+    }
 }
