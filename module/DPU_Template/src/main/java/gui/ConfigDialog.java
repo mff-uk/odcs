3e7182e2e314324e72e2f2121652879624a87d5f
Petr Škoda <p.skoda@centrum.cz>
2013-05-30 10:54:44 +0200

    DPU_Template update

--- module/DPU_Template/src/main/java/gui/ConfigDialog.java
+++ module/DPU_Template/src/main/java/gui/ConfigDialog.java
@@ -1,29 +1,23 @@
 package gui;
 
-import com.vaadin.ui.CustomComponent;
-
 import cz.cuni.xrg.intlib.commons.configuration.Configuration;
 import cz.cuni.xrg.intlib.commons.configuration.ConfigurationException;
+import cz.cuni.xrg.intlib.commons.module.gui.AbstractConfigDialog;
 
-public class ConfigDialog extends CustomComponent {
+/**
+ * DPU's configuration dialog. User can use this dialog to configure DPU configuration.
+ * 
+ * @author Petyr
+ *
+ */
+public class ConfigDialog extends AbstractConfigDialog {
 
-    /**
-     * Return current configuration from dialog. In case of invalid 
-     * configuration should throw.
-     *
-     * @throws ConfigurationException
-     * @return current configuration or null
-     */
+	@Override
     public void getConfiguration(Configuration config) throws ConfigurationException {
     	// TODO: save current dialog configuration into config, in case of invalid configuration throw ConfigurationException
     }
 
-    /**
-     * Load values from configuration into dialog.
-     *
-     * @throws ConfigurationException
-     * @param conf
-     */
+	@Override
     public void setConfiguration(Configuration conf) throws ConfigurationException {
         // TODO: load configuration from config into dialog, in case of invalid configuration throw ConfigurationException
     }
--- module/DPU_Template/src/main/java/module/DPU_Template.java
+++ module/DPU_Template/src/main/java/module/DPU_Template.java
@@ -2,75 +2,25 @@ package module;
 
 import gui.ConfigDialog;
 
-import com.vaadin.ui.CustomComponent;
-
-import cz.cuni.xrg.intlib.commons.DpuType;
 import cz.cuni.xrg.intlib.commons.configuration.Configuration;
-import cz.cuni.xrg.intlib.commons.configuration.ConfigurationException;
-import cz.cuni.xrg.intlib.commons.extractor.ExtractContext;
-import cz.cuni.xrg.intlib.commons.extractor.ExtractException;
-import cz.cuni.xrg.intlib.commons.web.*;
+import cz.cuni.xrg.intlib.commons.module.dpu.AbstractTransformer;
+import cz.cuni.xrg.intlib.commons.module.gui.AbstractConfigDialog;
 
-public class DPU_Template implements GraphicalExtractor { // TODO 1: Select super interface here
-
-    /**
-     * Configuration component.
-     */
-    private gui.ConfigDialog configDialog = null;
-	
-    /**
-     * DPU configuration.
-     */
-    private Configuration config = null;
-        
-	@Override
-	public DpuType getType() {
-		// TODO 2:
-		return DpuType.EXTRACTOR;
-	}
+public class DPU_Template extends AbstractTransformer { // TODO 1: Select Abstract class to Exnted
 
 	@Override
 	public void saveConfigurationDefault(Configuration configuration) {
-		// TODO 3: Store default configuration into configuration	
+		// TODO 2: Store default configuration into configuration	
 		configuration.setValue("myNumer", 3);
 		configuration.setValue("myString", "Configuration ...");
 	}
 
 	@Override
-	public void saveConfiguration(Configuration configuration)
-			throws ConfigurationException {
-		// TODO 4:
-        this.config = configuration;
-        if (this.configDialog == null) {
-        } else {
-            // also set configuration for dialog
-            this.configDialog.getConfiguration(this.config);
-        }
+	public AbstractConfigDialog createConfigurationDialog() {
+		// TODO 3: Change to your configuration dialog. If you use gui.ConfigDialog this can be leave unchanged
+		return new ConfigDialog();
 	}
 
-	@Override
-	public void loadConfiguration(Configuration configuration)
-			throws ConfigurationException {
-		// TODO 5:
-        if (this.configDialog == null) {
-        } else {
-            // get configuration from dialog
-            this.configDialog.setConfiguration(configuration);
-        }   		
-	}
-
-	@Override
-	public CustomComponent getConfigurationComponent(Configuration configuration) {
-		// TODO 6:
-        // does dialog exist?
-        if (this.configDialog == null) {
-            // create it
-            this.configDialog = new ConfigDialog();
-            this.configDialog.setConfiguration(configuration);
-        }
-        return this.configDialog;
-	} 
-
-	// TODO 7. implement missing method based on implemented interface
+	// TODO 4: Implement extract/transform/load method based on super class selected in 1:
 	
 }
