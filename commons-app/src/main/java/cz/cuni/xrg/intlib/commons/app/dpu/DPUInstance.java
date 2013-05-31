a3c30af0736963f9cf824402876c34598c2e9af4
Jan Vojt <jan@vojt.net>
2013-05-31 23:30:40 +0200

    Removed commented out code from DPUInstance.

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/dpu/DPUInstance.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/dpu/DPUInstance.java
@@ -25,19 +25,7 @@ public class DPUInstance {
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "dpu_id", nullable = false)
 	private DPU dpu;
-
-	/**
-	 * All edges go to the component on the canvas.
-	 */
-	//private RDFGraph inputGraph;
-	/**
-	 * All edges go from the component on the canvas.
-	 */
-	//private RDFGraph outputGraph;
-	/**
-	 * Connection to knowledge base
-	 */
-	//private RDFGraph knowledgeGraph;
+	
 	/**
 	 * Configuration setting for this component.
 	 */
@@ -59,13 +47,7 @@ public class DPUInstance {
 		this.name = dpu.getName();
 		this.description = dpu.getDescription();
 	}
-
-	/*
-	 public DPUInstance(RDFGraph inputGraph, RDFGraph outputGraph, InstanceConfiguration instanceConfig) {
-	 this.inputGraph = inputGraph;
-	 this.outputGraph = outputGraph;
-	 this.instanceConfig = instanceConfig;
-	 }*/
+	
 	/**
 	 * @return the id
 	 */
@@ -86,45 +68,7 @@ public class DPUInstance {
 	public void setDpu(DPU dpu) {
 		this.dpu = dpu;
 	}
-
-	/**
-	 * @return the inputGraph
-	 */
-	/*
-	 public RDFGraph getInputGraph() {
-	 return inputGraph;
-	 }*/
-	/**
-	 * @param inputGraph the inputGraph to set
-	 */
-	/*
-	 public void setInputGraph(RDFGraph inputGraph) {
-	 this.inputGraph = inputGraph;
-	 }*/
-	/**
-	 * @return the outputGraph
-	 */
-	/*public RDFGraph getOutputGraph() {
-	 return outputGraph;
-	 }*/
-	/**
-	 * @param outputGraph the outputGraph to set
-	 */
-	/*public void setOutputGraph(RDFGraph outputGraph) {
-	 this.outputGraph = outputGraph;
-	 }*/
-	/**
-	 * @return the knowledgeGraph
-	 */
-	/*public RDFGraph getKnowledgeGraph() {
-	 return knowledgeGraph;
-	 }*/
-	/**
-	 * @param knowledgeGraph the knowledgeGraph to set
-	 */
-	/*public void setKnowledgeGraph(RDFGraph knowledgeGraph) {
-	 this.knowledgeGraph = knowledgeGraph;
-	 }*/
+	
 	/**
 	 * @return the instanceConfig
 	 */
