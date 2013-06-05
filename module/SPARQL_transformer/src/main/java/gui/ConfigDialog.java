80cbbe73c691534d0390a7c725d2a4b9615cd54c
Jiri Tomes <Tooomy@seznam.cz>
2013-06-06 00:32:19 +0200

    Add Query validator to SPARQL_transformer.

--- module/SPARQL_transformer/src/main/java/gui/ConfigDialog.java
+++ module/SPARQL_transformer/src/main/java/gui/ConfigDialog.java
@@ -6,6 +6,8 @@ import module.Config;
 import com.vaadin.ui.*;
 
 import cz.cuni.xrg.intlib.commons.configuration.*;
+import cz.cuni.xrg.intlib.rdf.impl.SPARQLValidator;
+import cz.cuni.xrg.intlib.rdf.interfaces.Validator;
 
 /**
  * Configuration dialog.
@@ -15,84 +17,92 @@ import cz.cuni.xrg.intlib.commons.configuration.*;
  */
 public class ConfigDialog extends CustomComponent {
 
-    private static final long serialVersionUID = 1L;
-    private GridLayout mainLayout;
-    private TextArea txtQuery;
-    private Label labelUpQuer;
-   
-    public ConfigDialog() {
-        buildMainLayout();
-        setCompositionRoot(mainLayout);
-    }
+	private static final long serialVersionUID = 1L;
+	private GridLayout mainLayout;
+	private TextArea txtQuery;
+	private Label labelUpQuer;
 
-    /**
-     * Return current configuration from dialog. Can return null, if current
-     * configuration is invalid.
-     *
-     */
-    public void getConfiguration(Configuration config) {
-        config.setValue(Config.SPARQL_Update_Query.name(), txtQuery.getValue());
-    }
+	public ConfigDialog() {
+		buildMainLayout();
+		setCompositionRoot(mainLayout);
+	}
 
-    /**
-     * Load values from configuration into dialog.
-     *
-     * @throws ConfigurationException
-     * @param conf
-     */
-    public void setConfiguration(Configuration conf) {
-        try {
-            txtQuery.setValue((String) conf.getValue(Config.SPARQL_Update_Query.name()));
+	/**
+	 * Return current configuration from dialog. Can return null, if current
+	 * configuration is invalid.
+	 *
+	 */
+	public void getConfiguration(Configuration config) {
+		config.setValue(Config.SPARQL_Update_Query.name(), txtQuery.getValue());
+	}
 
-        } catch (Exception ex) {
-            // throw setting exception
-            throw new ConfigurationException();
-        }
-    }
+	/**
+	 * Load values from configuration into dialog.
+	 *
+	 * @throws ConfigurationException
+	 * @param conf
+	 */
+	public void setConfiguration(Configuration conf) {
+		try {
+			txtQuery.setValue((String) conf.getValue(Config.SPARQL_Update_Query.name()));
 
-    private GridLayout buildMainLayout() {
-        // common part: create layout
-        mainLayout = new GridLayout(2, 1);
-        mainLayout.setImmediate(false);
-        mainLayout.setWidth("100%");
-        mainLayout.setHeight("100%");
-        mainLayout.setMargin(false);
-        //mainLayout.setSpacing(true);
+		} catch (Exception ex) {
+			// throw setting exception
+			throw new ConfigurationException();
+		}
+	}
 
-        // top-level component properties
-        setWidth("100%");
-        setHeight("100%");
+	private GridLayout buildMainLayout() {
+		// common part: create layout
+		mainLayout = new GridLayout(2, 1);
+		mainLayout.setImmediate(false);
+		mainLayout.setWidth("100%");
+		mainLayout.setHeight("100%");
+		mainLayout.setMargin(false);
+		//mainLayout.setSpacing(true);
 
-        // labelUpQuer
-        labelUpQuer = new Label();
-        labelUpQuer.setImmediate(false);
-        labelUpQuer.setWidth("68px");
-        labelUpQuer.setHeight("-1px");
-        labelUpQuer.setValue("SPARQL  Update Query");
-        mainLayout.addComponent(labelUpQuer, 0, 0);
+		// top-level component properties
+		setWidth("100%");
+		setHeight("100%");
 
-        // textAreaUpQuer
-        txtQuery = new TextArea();
-        
-        txtQuery.addValueChangeListener(new Property.ValueChangeListener() {
+		// labelUpQuer
+		labelUpQuer = new Label();
+		labelUpQuer.setImmediate(false);
+		labelUpQuer.setWidth("68px");
+		labelUpQuer.setHeight("-1px");
+		labelUpQuer.setValue("SPARQL  Update Query");
+		mainLayout.addComponent(labelUpQuer, 0, 0);
 
-            @Override
-            public void valueChange(Property.ValueChangeEvent event) {
-                final String query=txtQuery.getValue();
-                
-                
-            }
-        });
-        
-        txtQuery.setNullRepresentation("");
-        txtQuery.setImmediate(false);
-        txtQuery.setWidth("100%");
-        txtQuery.setHeight("211px");
-        txtQuery.setInputPrompt("PREFIX br:<http://purl.org/business-register#>\nMODIFY\nDELETE { ?s pc:contact ?o}\nINSERT { ?s br:contact ?o}\nWHERE {\n\t     ?s a gr:BusinessEntity .\n\t      ?s pc:contact ?o\n}");
-        mainLayout.addComponent(txtQuery, 1, 0);
-        mainLayout.setColumnExpandRatio(0, 0.00001f);
-        mainLayout.setColumnExpandRatio(1, 0.99999f);
+		// textAreaUpQuer
+		txtQuery = new TextArea();
 
-        return mainLayout;
-    }
+		txtQuery.addValueChangeListener(new Property.ValueChangeListener() {
+			@Override
+			public void valueChange(Property.ValueChangeEvent event) {
+				final String query = txtQuery.getValue();
+
+				Validator validator = new SPARQLValidator(query);
+
+				if (!validator.isQueryValid()) {
+					
+					Notification.show("Query Validator",
+							"Query is not valid: "
+							+ validator.getErrorMessage(),
+							Notification.Type.ERROR_MESSAGE);
+				}
+
+			}
+		});
+
+		txtQuery.setNullRepresentation("");
+		txtQuery.setImmediate(false);
+		txtQuery.setWidth("100%");
+		txtQuery.setHeight("211px");
+		txtQuery.setInputPrompt("PREFIX br:<http://purl.org/business-register#>\nMODIFY\nDELETE { ?s pc:contact ?o}\nINSERT { ?s br:contact ?o}\nWHERE {\n\t     ?s a gr:BusinessEntity .\n\t      ?s pc:contact ?o\n}");
+		mainLayout.addComponent(txtQuery, 1, 0);
+		mainLayout.setColumnExpandRatio(0, 0.00001f);
+		mainLayout.setColumnExpandRatio(1, 0.99999f);
+
+		return mainLayout;
+	}
 }
