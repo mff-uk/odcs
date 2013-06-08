903e243d013aa3fdce71fcd1df7f71735688fb3f
Petr Škoda <p.skoda@centrum.cz>
2013-06-08 22:29:19 +0200

    Modules update

--- db/virtuoso/rdbms/data.sql
+++ db/virtuoso/rdbms/data.sql
@@ -1,10 +1,5 @@
-
-INSERT INTO DB.INTLIB.DPU_TEMPLATE(id,name,description,type,jar_path,visibility,jar_description) VALUES(2,'RDF File Extractor','Extracts RDF data from a file.',0,'FileExtractor-0.0.1.jar',1,'No description in manifest.');
-
--- INSERT INTO DB.INTLIB.DPU_TEMPLATE(id,name,description,type,jar_path,visibility,jar_description) VALUES(5,'RDF File Loader','Loads RDF data into file.',2,'File_loader-0.0.1.jar',1,'No description in manifest.');
--- old
--- INSERT INTO DB.INTLIB.DPU_MODEL(id,name,description,type,visibility,jar_path) VALUES(1,'SPARQL Extractor','Extracts RDF data.',0,1,'RDF_extractor-0.0.1.jar');
--- INSERT INTO DB.INTLIB.DPU_MODEL(id,name,description,type,visibility,jar_path) VALUES(2,'RDF File Extractor','Extracts RDF data from a file.',0,1,'File_extractor-0.0.1.jar');
--- INSERT INTO DB.INTLIB.DPU_MODEL(id,name,description,type,visibility,jar_path) VALUES(3,'SPARQL Transformer','SPARQL Transformer.',1,1,'SPARQL_transformer-0.0.1.jar');
--- INSERT INTO DB.INTLIB.DPU_MODEL(id,name,description,type,visibility,jar_path) VALUES(4,'SPARQL Loader','Loads RDF data.',2,1,'RDF_loader-0.0.1.jar');
--- INSERT INTO DB.INTLIB.DPU_MODEL(id,name,description,type,visibility,jar_path) VALUES(5,'RDF File Loader','Loads RDF data into file.',2,1,'File_loader-0.0.1.jar');
+INSERT INTO DB.INTLIB.DPU_TEMPLATE(id,name,description,type,jar_path,visibility,jar_description) VALUES(1,'SPARQL Extractor','Extracts RDF data.',0,'RDF_Extractor-0.0.1.jar',1,'No description in manifest.');
+INSERT INTO DB.INTLIB.DPU_TEMPLATE(id,name,description,type,jar_path,visibility,jar_description) VALUES(2,'RDF File Extractor','Extracts RDF data from a file.',0,'File_Extractor-0.0.1.jar',1,'No description in manifest.');
+-- INSERT INTO DB.INTLIB.DPU_TEMPLATE(id,name,description,type,jar_path,visibility,jar_description) VALUES(3,'SPARQL Transformer','SPARQL Transformer.',1,'SPARQL_Transformer-0.0.1.jar',1,'No description in manifest.');
+INSERT INTO DB.INTLIB.DPU_TEMPLATE(id,name,description,type,jar_path,visibility,jar_description) VALUES(4,'SPARQL Loader','Loads RDF data.',2,'RDF_Loader-0.0.1.jar',1,'No description in manifest.');
+INSERT INTO DB.INTLIB.DPU_TEMPLATE(id,name,description,type,jar_path,visibility,jar_description) VALUES(5,'RDF File Loader','Loads RDF data into file.',2,'File_loader-0.0.1.jar',1,'No description in manifest.');
