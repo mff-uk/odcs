17d32165496b56a6f978e751b5b443070ebe8a17
Petr Škoda <p.skoda@centrum.cz>
2013-06-08 23:01:53 +0200

    DPUTemplate update

---
+++ module/DPU_Template/src/main/java/com/example/DPUTemplateConfig.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import cz.cuni.xrg.intlib.commons.configuration.Config;
+
+/**
+ *
+ * Put your DPU's configuration here.
+ *
+ */
+public class DPUTemplateConfig implements Config {
+
+}
---
+++ module/DPU_Template/src/main/java/com/example/DPUTemplateDialog.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import cz.cuni.xrg.intlib.commons.configuration.ConfigException;
+import cz.cuni.xrg.intlib.commons.web.AbstractConfigDialog;
+
+/**
+ * DPU's configuration dialog. User can use this dialog to configure DPU configuration.
+ *
+ */
+public class DPUTemplateDialog extends AbstractConfigDialog<DPUTemplateConfig> {
+
+	@Override
+	public void setConfiguration(DPUTemplateConfig conf) throws ConfigException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DPUTemplateConfig getConfiguration() throws ConfigException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
