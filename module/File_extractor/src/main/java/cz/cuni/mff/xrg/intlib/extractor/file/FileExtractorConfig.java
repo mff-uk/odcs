2c4c513ab2f355f05cc11361561901dca21dceb0
Jiri Tomes <Tooomy@seznam.cz>
2013-07-04 00:30:51 +0200

    Add UseStatisticalHandler to FileExtractorConfig.

--- module/File_extractor/src/main/java/cz/cuni/mff/xrg/intlib/extractor/file/FileExtractorConfig.java
+++ module/File_extractor/src/main/java/cz/cuni/mff/xrg/intlib/extractor/file/FileExtractorConfig.java
@@ -4,17 +4,19 @@ import cz.cuni.xrg.intlib.commons.configuration.Config;
 
 /**
  * File extractor configuration.
- * 
+ *
  * @author Petyr
  *
  */
 public class FileExtractorConfig implements Config {
 
 	public String Path;
-	
+
 	public String FileSuffix;
-		
+
 	public String OnlyThisText;
-	
+
 	public boolean OnlyThisSuffix;
+
+	public boolean UseStatisticalHandler;
 }
