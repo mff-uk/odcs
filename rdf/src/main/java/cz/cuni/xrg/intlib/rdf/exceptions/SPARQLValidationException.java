9410cee2679c567350fe2df18fc3f1e1b1ba09c1
Jiri Tomes <Tooomy@seznam.cz>
2013-07-12 19:26:05 +0200

    Formating comment in SPARQLValidatorException.

--- rdf/src/main/java/cz/cuni/xrg/intlib/rdf/exceptions/SPARQLValidationException.java
+++ rdf/src/main/java/cz/cuni/xrg/intlib/rdf/exceptions/SPARQLValidationException.java
@@ -3,8 +3,9 @@ package cz.cuni.xrg.intlib.rdf.exceptions;
 import cz.cuni.xrg.intlib.commons.configuration.ConfigException;
 
 /**
- * Exception is thrown when given SPARQL data are not valid using SPARQL validator. 
- * 
+ * Exception is thrown when given SPARQL data are not valid using SPARQL
+ * validator.
+ *
  * @author Jiri Tomes
  */
 public class SPARQLValidationException extends ConfigException {
