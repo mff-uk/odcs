abe5d85b67e462660be1ef5f4b5ccf9cb769071a
Petr Škoda <p.skoda@centrum.cz>
2013-07-15 20:54:44 +0200

    Added empty interface for description DPU's IO

---
+++ commons/src/main/java/cz/cuni/xrg/intlib/commons/data/IODescription.java
@@ -0,0 +1,12 @@
+package cz.cuni.xrg.intlib.commons.data;
+
+/**
+ * Can be used to describe DPU's inputs and outputs
+ * in terms of named DataUnits.
+ * 
+ * @author Petyr
+ *
+ */
+public interface IODescription {
+	// TODO Petyr
+}
