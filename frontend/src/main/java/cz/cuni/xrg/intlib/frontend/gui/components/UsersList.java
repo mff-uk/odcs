acf10247db4585af0184ab72670cf2bbf5172123
Maria Kukhar <mari.kukhar@gmail.com>
2013-08-09 12:33:16 +0200

    Administrator GUI, list of all users.

---
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/components/UsersList.java
@@ -0,0 +1,238 @@
+package cz.cuni.xrg.intlib.frontend.gui.components;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import com.vaadin.data.Property;
+import com.vaadin.data.util.IndexedContainer;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.server.Sizeable.Unit;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomTable;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Window.CloseEvent;
+import com.vaadin.ui.Window.CloseListener;
+
+import cz.cuni.xrg.intlib.commons.app.pipeline.PipelineExecution;
+import cz.cuni.xrg.intlib.commons.app.pipeline.PipelineExecutionStatus;
+import cz.cuni.xrg.intlib.commons.app.scheduling.Schedule;
+import cz.cuni.xrg.intlib.frontend.auxiliaries.App;
+import cz.cuni.xrg.intlib.frontend.auxiliaries.IntlibHelper;
+import cz.cuni.xrg.intlib.frontend.gui.views.GenerateActionColumnMonitor;
+
+
+
+public class UsersList {
+	
+	 private IntlibPagedTable usersTable;
+	 private VerticalLayout usersListLayout;
+	 static String[] visibleCols = new String[]{"id", "user", "role",
+	        "total_pipelines", "actions"};
+	 static String[] headers = new String[]{"Id", "User Name", "Role(s)",
+	        "Total Pipelines", "Actions"};
+	 private IndexedContainer tableData;
+	 private Long userId;
+
+
+	public VerticalLayout buildUsersListLayout(){
+		
+		
+		usersListLayout = new VerticalLayout();
+		usersListLayout.setMargin(true);
+		usersListLayout.setSpacing(true);
+		usersListLayout.setWidth("100%");
+		usersListLayout.setHeight("100%");
+		usersListLayout.setImmediate(true);
+		
+		
+		//Layout for buttons Add new user and Clear Filters on the top.
+		HorizontalLayout topLine = new HorizontalLayout();
+		topLine.setSpacing(true);
+		topLine.setWidth(100, Unit.PERCENTAGE);
+
+		Button addUserButton = new Button();
+		addUserButton.setCaption("Create new user");
+/*		addUserButton
+				.addClickListener(new com.vaadin.ui.Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// open scheduler dialog
+				SchedulePipeline sch = new SchedulePipeline();
+				App.getApp().addWindow(sch);
+				sch.addCloseListener(new CloseListener() {
+					@Override
+					public void windowClose(CloseEvent e) {
+						refreshData();
+					}
+				});
+			}
+		});*/
+		topLine.addComponent(addUserButton);
+		topLine.setComponentAlignment(addUserButton, Alignment.MIDDLE_RIGHT);
+
+		Button buttonDeleteFilters = new Button();
+		buttonDeleteFilters.setCaption("Clear Filters");
+		buttonDeleteFilters.setHeight("25px");
+		buttonDeleteFilters.setWidth("110px");
+		buttonDeleteFilters
+				.addClickListener(new com.vaadin.ui.Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				usersTable.resetFilters();
+				usersTable.setFilterFieldVisible("actions", false);
+			}
+		});
+		topLine.addComponent(buttonDeleteFilters);
+		topLine.setComponentAlignment(buttonDeleteFilters, Alignment.MIDDLE_RIGHT);
+
+		Label topLineFiller = new Label();
+		topLine.addComponentAsFirst(topLineFiller);
+		topLine.setExpandRatio(topLineFiller, 1.0f);
+		usersListLayout.addComponent(topLine);
+
+		
+		
+        tableData = getTableData();
+
+        //table with pipeline execution records
+        usersTable = new IntlibPagedTable();
+        usersTable.setSelectable(true);
+        usersTable.setContainerDataSource(tableData);
+        usersTable.setWidth("100%");
+        usersTable.setHeight("100%");
+        usersTable.setImmediate(true);
+        usersTable.setVisibleColumns(visibleCols); // Set visible columns
+        usersTable.setColumnHeaders(headers);
+
+        //Actions column. Contains actions buttons: Debug data, Show log, Stop.
+        usersTable.addGeneratedColumn("actions",
+                new actionColumnGenerator());
+
+        usersListLayout.addComponent(usersTable);
+        usersListLayout.addComponent(usersTable.createControls());
+        usersTable.setPageLength(10);
+        usersTable.setFilterBarVisible(true);
+        usersTable.setFilterFieldVisible("actions", false);
+
+		
+
+		
+		return usersListLayout;
+	}
+	
+	/**
+	 * Calls for refresh table {@link #schedulerTable}.
+	 */
+	private void refreshData() {
+		int page = usersTable.getCurrentPage();
+		tableData = getTableData();
+		usersTable.setContainerDataSource(tableData);
+		usersTable.setCurrentPage(page);
+		usersTable.setVisibleColumns(visibleCols);
+		usersTable.setFilterFieldVisible("actions", false);
+
+	}
+	
+    /**
+     * Container with data for {@link #usersTable}
+     *
+     * @param data. List of users
+     * @return result. IndexedContainer with data for users table
+     */
+	public static IndexedContainer getTableData() {
+
+/*		 static String[] visibleCols = new String[]{"id", "user", "role",
+		        "total_pipelines", "actions"};*/
+		
+		String[] id = { "1", "2", "3" };
+		String[] user = { "tomask", "mariak", "jirit" };
+		String[] role = { "administrator", "user", "user" };
+		String[] total_pipelines = { "7", "2", "4" };
+
+		IndexedContainer result = new IndexedContainer();
+
+		for (String p : visibleCols) {
+			result.addContainerProperty(p, String.class, "");
+		}
+
+		int max = getMinLength(id, user, role, total_pipelines);
+
+		for (int i = 0; i < max; i++) {
+			Object num = result.addItem();
+			result.getContainerProperty(num, "id").setValue(id[i]);
+			result.getContainerProperty(num, "user").setValue(user[i]);
+			result.getContainerProperty(num, "role").setValue(role[i]);
+			result.getContainerProperty(num, "total_pipelines").setValue(total_pipelines[i]);
+
+
+		}
+
+		return result;
+
+	}
+	
+	  private final static int UNDEFINED_LENGTH = -1;
+	
+	  public static int getMinLength(String[]... arraysLength) {
+	    int min = UNDEFINED_LENGTH;
+	    for (int i = 0; i < arraysLength.length; i++) {
+	      if (min == UNDEFINED_LENGTH) {
+	        min = arraysLength[i].length;
+	      } else {
+	        min = Math.min(min, arraysLength[i].length);
+	      }
+	    }
+	    return min;
+	
+	  } 
+    
+	/**
+	 * Generate column "actions" in the table {@link #usersTable}.
+	 *
+	 * @author Maria Kukhar
+	 *
+	 */
+	class actionColumnGenerator implements CustomTable.ColumnGenerator {
+
+		private ClickListener clickListener = null;
+
+		@Override
+		public Object generateCell(final CustomTable source, final Object itemId,
+				Object columnId) {
+
+			HorizontalLayout layout = new HorizontalLayout();
+
+		
+			//Delete button. Delete user's record from Database.
+			Button deleteButton = new Button();
+			deleteButton.setCaption("Delete");
+/*			deleteButton.addClickListener(new ClickListener() {
+				@Override
+				public void buttonClick(ClickEvent event) {
+
+					userId = (Long) tableData.getContainerProperty(itemId, "id")
+							.getValue();
+					Schedule schedule = App.getApp().getSchedules().getSchedule(userId);
+					App.getApp().getSchedules().delete(schedule);
+					refreshData();
+				}
+			});*/
+			layout.addComponent(deleteButton);
+			
+			//Delete button. Delete user's record from Database.
+			Button changeButton = new Button();
+			changeButton.setCaption("Change settings");
+			
+			layout.addComponent(changeButton);
+
+			return layout;
+		}
+	}
+}
--- frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/views/Administrator.java
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/gui/views/Administrator.java
@@ -3,37 +3,166 @@ package cz.cuni.xrg.intlib.frontend.gui.views;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.NativeButton;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 
 import cz.cuni.xrg.intlib.frontend.gui.ViewComponent;
+import cz.cuni.xrg.intlib.frontend.gui.components.IntlibPagedTable;
+import cz.cuni.xrg.intlib.frontend.gui.components.UsersList;
 
 class Administrator extends ViewComponent {
 
-	private AbsoluteLayout mainLayout;
+	private HorizontalLayout mainLayout;
+	private VerticalLayout usersLayout;
+	private VerticalLayout recordsLayout;
+	private VerticalLayout pipelinesLayout;
+	
+	private VerticalLayout tabsLayout;
+	private Button usersButton;
+	private Button recordsButton;
+	private Button pipelinesButton;
+	private Button shownTab = null;
+	private UsersList usersList;
 
-	private Label label;
 
 	public Administrator() { }
 
-	private AbsoluteLayout buildMainLayout() {
+	private HorizontalLayout buildMainLayout() {
 		// common part: create layout
-		mainLayout = new AbsoluteLayout();
+		mainLayout = new HorizontalLayout();
 		mainLayout.setImmediate(false);
-		mainLayout.setWidth("600px");
-		mainLayout.setHeight("800px");
+		mainLayout.setWidth("100%");
+		mainLayout.setHeight("100%");
+
 		
 		// top-level component properties
-		setWidth("600px");
-		setHeight("800px");
+		setWidth("100%");
+		setHeight("100%");
 		
-		// label
-		label = new Label();
-		label.setImmediate(false);
-		label.setWidth("-1px");
-		label.setHeight("-1px");
-		label.setValue("<h1>Administrator</h>");
-		label.setContentMode(ContentMode.HTML);
-		mainLayout.addComponent(label, "top:100.0px;left:80.0px;");
+        tabsLayout = new VerticalLayout();
+
+        tabsLayout.setWidth("100%");
+        tabsLayout.setImmediate(true);
+        
+
+
+        
+        usersLayout = new VerticalLayout();
+        usersLayout.setImmediate(true);
+        usersLayout.setWidth("100%");
+        usersLayout.setHeight("100%");
+        usersLayout.addComponent(new Label("Users"));
+        
+        usersList = new UsersList();
+        usersLayout = usersList.buildUsersListLayout();
+        usersLayout.setStyleName("settings");
+        
+        
+        recordsLayout = new VerticalLayout();
+        recordsLayout.setMargin(true);
+        recordsLayout.setSpacing(true);
+        recordsLayout.setImmediate(true);
+        recordsLayout.setStyleName("settings");
+        recordsLayout.setWidth("100%");
+        recordsLayout.addComponent(new Label("Records"));
+        
+        
+        pipelinesLayout = new VerticalLayout();
+        pipelinesLayout.setMargin(true);
+        pipelinesLayout.setSpacing(true);
+        pipelinesLayout.setImmediate(true);
+        pipelinesLayout.setStyleName("settings");
+        pipelinesLayout.setWidth("100%");
+        pipelinesLayout.addComponent(new Label("Pipelines"));
+        
+        usersButton = new NativeButton("Users");
+        usersButton.setHeight("40px");
+        usersButton.setWidth("90px");
+        usersButton.setStyleName("selectedtab");
+        usersButton.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				shownTab=usersButton;
+				shownTab.setStyleName("selectedtab");
+				recordsButton.setStyleName("multiline");
+				pipelinesButton.setStyleName("multiline");
+				
+				mainLayout.removeComponent(usersLayout);
+				mainLayout.removeComponent(recordsLayout);
+				mainLayout.removeComponent(pipelinesLayout);
+				mainLayout.addComponent(usersLayout);
+				mainLayout.setExpandRatio(usersLayout, 0.85f);
+
+				
+			}
+		});
+        tabsLayout.addComponent(usersButton);
+        tabsLayout.setComponentAlignment(usersButton, Alignment.TOP_RIGHT);
+        
+        recordsButton = new NativeButton("Records");
+        recordsButton.setHeight("40px");
+        recordsButton.setWidth("90px");
+        recordsButton.setStyleName("multiline");
+        recordsButton.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				shownTab=recordsButton;
+				shownTab.setStyleName("selectedtab");
+				usersButton.setStyleName("multiline");
+				pipelinesButton.setStyleName("multiline");
+				
+				mainLayout.removeComponent(usersLayout);
+				mainLayout.removeComponent(recordsLayout);
+				mainLayout.removeComponent(pipelinesLayout);
+				mainLayout.addComponent(recordsLayout);
+				mainLayout.setExpandRatio(recordsLayout, 0.85f);
+
+				
+			}
+		});
+        tabsLayout.addComponent(recordsButton);
+        tabsLayout.setComponentAlignment(recordsButton, Alignment.TOP_RIGHT);
+        
+        pipelinesButton = new NativeButton("Pipelines");
+        pipelinesButton.setHeight("40px");
+        pipelinesButton.setWidth("90px");
+        pipelinesButton.setStyleName("multiline");
+        pipelinesButton.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				shownTab=pipelinesButton;
+				shownTab.setStyleName("selectedtab");
+				usersButton.setStyleName("multiline");
+				recordsButton.setStyleName("multiline");
+				
+				mainLayout.removeComponent(usersLayout);
+				mainLayout.removeComponent(recordsLayout);
+				mainLayout.removeComponent(pipelinesLayout);
+				mainLayout.addComponent(pipelinesLayout);
+				mainLayout.setExpandRatio(pipelinesLayout, 0.85f);
+
+				
+			}
+		});
+        tabsLayout.addComponent(pipelinesButton);
+        tabsLayout.setComponentAlignment(pipelinesButton, Alignment.TOP_RIGHT);
+
+
+		
+		
+		mainLayout.addComponent(tabsLayout);
+		mainLayout.addComponent(usersLayout);
+		mainLayout.setExpandRatio(tabsLayout, 0.15f);
+		mainLayout.setExpandRatio(usersLayout, 0.85f);
 		
 		return mainLayout;
 	}
