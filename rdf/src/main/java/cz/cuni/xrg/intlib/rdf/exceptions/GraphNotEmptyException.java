d71fd8dd3521676d847d10b9e9911b6b3983b5fd
Petr Škoda <p.skoda@centrum.cz>
2013-08-11 21:52:18 +0200

    RDF's exception use DataUnitException as base class

--- rdf/src/main/java/cz/cuni/xrg/intlib/rdf/exceptions/GraphNotEmptyException.java
+++ rdf/src/main/java/cz/cuni/xrg/intlib/rdf/exceptions/GraphNotEmptyException.java
@@ -1,10 +1,12 @@
 package cz.cuni.xrg.intlib.rdf.exceptions;
 
+import cz.cuni.xrg.intlib.commons.data.DataUnitException;
+
 /**
  *
  * @author Jiri Tomes
  */
-public class GraphNotEmptyException extends RuntimeException {
+public class GraphNotEmptyException extends DataUnitException {
 
 	private static String defaultMessage = "Target graph is not empty. Load to SPARQL endpoint fail.";
 
