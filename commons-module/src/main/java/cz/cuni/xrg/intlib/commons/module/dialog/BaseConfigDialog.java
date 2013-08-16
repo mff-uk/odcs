1b04408fcef1beff188ac48f55e48d355aab1073
Petr Škoda <p.skoda@centrum.cz>
2013-08-16 16:42:01 +0200

    Bug fix and close #235

--- commons-module/src/main/java/cz/cuni/xrg/intlib/commons/module/dialog/BaseConfigDialog.java
+++ commons-module/src/main/java/cz/cuni/xrg/intlib/commons/module/dialog/BaseConfigDialog.java
@@ -23,7 +23,11 @@ public abstract class BaseConfigDialog<C extends DPUConfigObject>
     
 	@Override
 	public void setConfig(byte[] conf) throws ConfigException {
-		C config = configWrap.deserialize(conf);		
+		C config = configWrap.deserialize(conf);
+		if (config == null) {
+			// null -> use default configuration
+			return;
+		}
 		if (config.isValid()) {
 			setConfiguration(config);
 		} else {
@@ -37,4 +41,18 @@ public abstract class BaseConfigDialog<C extends DPUConfigObject>
 		return configWrap.serialize(getConfiguration());
 	}
 	
+	/**
+	 * Set dialog interface according to passed configuration. If
+	 * the passed configuration is invalid ConfigException can be thrown.
+	 * @param conf Configuration object.
+	 * @throws ConfigException
+	 */
+	protected abstract void setConfiguration(C conf) throws ConfigException;
+	
+	/**
+	 * Get configuration from dialog. In case of presence invalid configuration in 
+	 * dialog throw ConfigException.
+	 * @return getConfiguration object.
+	 */
+	protected abstract C getConfiguration() throws ConfigException;	
 }
