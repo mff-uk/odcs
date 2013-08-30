413f52253059f86826b971936ab0829bfebb5782
Tomas Knap <toknap@gmail.com>
2013-08-30 10:57:25 +0200

    DPU template doc adjusted

--- module/DPU_Template/src/main/java/com/example/DPUTemplateConfig.java
+++ module/DPU_Template/src/main/java/com/example/DPUTemplateConfig.java
@@ -7,9 +7,30 @@ import cz.cuni.xrg.intlib.commons.configuration.DPUConfigObject;
  */
 public class DPUTemplateConfig implements DPUConfigObject {
 
+        DPUTemplateConfig() {
+            width = 100;
+            height = 100;
+        }
+        
+        DPUTemplateConfig(int w, int h) {
+            width = w;
+            height = h;
+        }
+        
+        private int width;
+        private int height;
+        
+        public int getWidth() {
+            return width;    
+        }
+        
+         public int getHeight() {
+            return height;    
+        }
+
 	@Override
 	public boolean isValid() {
-		// TODO : Return true in case that the configuration is valid
+		// TODO : Return true in case the current configuration is valid
 		return true;
 	}
 
