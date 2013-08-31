a73b88fcd2a5a0e48b8fcc5ec0d46743a0822e1e
Jiri Tomes <Tooomy@seznam.cz>
2013-08-31 20:32:33 +0200

    Add TripleCountHandler class - need if no triples from SPARQL endpoint were extracted.

---
+++ rdf/src/main/java/cz/cuni/xrg/intlib/rdf/impl/TripleCountHandler.java
@@ -0,0 +1,32 @@
+package cz.cuni.xrg.intlib.rdf.impl;
+
+import org.openrdf.model.Statement;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.helpers.RDFHandlerBase;
+
+/**
+ * Class for counting of extracted triples from SPARQL endpoint. Need for using
+ * in cause throwing exception, when no triples were extracted from SPARQL
+ * endpoint.
+ *
+ *
+ * @author Jiri Tomes
+ */
+public class TripleCountHandler extends RDFHandlerBase {
+
+	private long count = 0;
+
+	@Override
+	public void handleStatement(Statement st) throws RDFHandlerException {
+		super.handleStatement(st);
+		count++;
+	}
+
+	public long getLoadedCount() {
+		return count;
+	}
+
+	public void resetTripleCount() {
+		count = 0;
+	}
+}
