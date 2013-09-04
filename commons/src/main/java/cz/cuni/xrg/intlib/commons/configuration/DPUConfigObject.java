83405fd3147b72a65211d6f55208332d97f48dd6
Petr Škoda <p.skoda@centrum.cz>
2013-09-04 13:22:18 +0200

    org.openrdf packages added to frontend/backend system export list, #367

--- commons/src/main/java/cz/cuni/xrg/intlib/commons/configuration/DPUConfigObject.java
+++ commons/src/main/java/cz/cuni/xrg/intlib/commons/configuration/DPUConfigObject.java
@@ -3,19 +3,37 @@ package cz.cuni.xrg.intlib.commons.configuration;
 import java.io.Serializable;
 
 /**
- * Base interface for dpu's configuration. 
- * <b>All the configuration object
- * must provide public parameter less constructor!</b>
- *
+ * Base interface for dpu's configuration. <b>All the configuration object must
+ * provide public parameter less constructor!</b>
+ * 
  * @author Petyr
  */
 public interface DPUConfigObject extends Serializable {
-	
+
 	/**
-	 * Check if configuration is valid ie. can be used to configure
-	 * DPU or configuration dialog.
+	 * Check if configuration is valid ie. can be used to configure DPU or
+	 * configuration dialog.
+	 * 
 	 * @return
 	 */
 	boolean isValid();
-	
+
+	/**
+	 * Return text that should be used as a DPU tool tip. The text should
+	 * contains information about configuration. If the configuration is not
+	 * valid, or this functionality is not supported can return null.
+	 * 
+	 * @return Can be null.
+	 */
+	String getToolTip();
+
+	/**
+	 * Return configuration summary that can be used as DPU description. The
+	 * summary should be short and as much informative as possible. Return null
+	 * in case of invalid configuration or it this functionality is not
+	 * supported
+	 * 
+	 * @return Can be null.
+	 */
+	String getDescription();
 }
