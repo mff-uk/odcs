35706c35bb4d3f30c3d89cac25768c2fe206c740
Petr Škoda <p.skoda@centrum.cz>
2013-09-04 17:09:09 +0200

    ConfigDialog interface changed, should now provide methods to generate short description as well as toolTips instead of DPUConfigObject

--- commons-web/src/main/java/cz/cuni/xrg/intlib/commons/web/AbstractConfigDialog.java
+++ commons-web/src/main/java/cz/cuni/xrg/intlib/commons/web/AbstractConfigDialog.java
@@ -30,4 +30,23 @@ public abstract class AbstractConfigDialog<C extends DPUConfigObject>
 	 */
 	public abstract byte[] getConfig() throws ConfigException;
 
+	/**
+	 * Return text that should be used as a DPU tool tip. The text should
+	 * contains information about configuration. If the configuration is not
+	 * valid, or this functionality is not supported can return null.
+	 * 
+	 * @return Can be null.
+	 */	
+	public abstract String getToolTip();
+	
+	/**
+	 * Return configuration summary that can be used as DPU description. The
+	 * summary should be short and as much informative as possible. Return null
+	 * in case of invalid configuration or it this functionality is not
+	 * supported
+	 * 
+	 * @return Can be null.
+	 */	
+	public abstract String getDescription();
+	
 }
--- commons/src/main/java/cz/cuni/xrg/intlib/commons/configuration/DPUConfigObject.java
+++ commons/src/main/java/cz/cuni/xrg/intlib/commons/configuration/DPUConfigObject.java
@@ -17,23 +17,5 @@ public interface DPUConfigObject extends Serializable {
 	 * @return
 	 */
 	boolean isValid();
-
-	/**
-	 * Return text that should be used as a DPU tool tip. The text should
-	 * contains information about configuration. If the configuration is not
-	 * valid, or this functionality is not supported can return null.
-	 * 
-	 * @return Can be null.
-	 */
-	// String getToolTip();
-
-	/**
-	 * Return configuration summary that can be used as DPU description. The
-	 * summary should be short and as much informative as possible. Return null
-	 * in case of invalid configuration or it this functionality is not
-	 * supported
-	 * 
-	 * @return Can be null.
-	 */
-	// String getDescription();
+	 
 }
