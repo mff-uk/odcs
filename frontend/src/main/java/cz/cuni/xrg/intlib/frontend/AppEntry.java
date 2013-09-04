824932001ee6415062c567fd1507fa5f7a2af57d
Jan Vojt <jan@vojt.net>
2013-09-04 21:44:07 +0200

    Enabled authentication.

--- frontend/src/main/java/cz/cuni/xrg/intlib/frontend/AppEntry.java
+++ frontend/src/main/java/cz/cuni/xrg/intlib/frontend/AppEntry.java
@@ -166,12 +166,12 @@ public class AppEntry extends com.vaadin.ui.UI {
 		this.getNavigator().addViewChangeListener(new ViewChangeListener() {
 			@Override
 			public boolean beforeViewChange(ViewChangeListener.ViewChangeEvent event) {
-//				if (!event.getViewName().equals(ViewNames.LOGIN.getUrl()) && !checkAuthentication()) {
-//					getNavigator().navigateTo(ViewNames.LOGIN.getUrl());
-//					getMain().refreshUserBar();
-//					return false;
-//				}
-//				setActive();
+				if (!event.getViewName().equals(ViewNames.LOGIN.getUrl()) && !checkAuthentication()) {
+					getNavigator().navigateTo(ViewNames.LOGIN.getUrl());
+					getMain().refreshUserBar();
+					return false;
+				}
+				setActive();
 				if(refreshThread == null) {
 					setupRefreshThread();
 				}
