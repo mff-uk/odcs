df014747688fe57822f5ac804f021d12dd4dc561
Petr Škoda <p.skoda@centrum.cz>
2013-09-18 19:27:18 +0200

    ModuleRefresher moved to commons-app

--- commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/module/ModuleRefresehr.java
+++ commons-app/src/main/java/cz/cuni/xrg/intlib/commons/app/module/ModuleRefresehr.java
@@ -1,11 +1,7 @@
-package cz.cuni.xrg.intlib.backend.module;
+package cz.cuni.xrg.intlib.commons.app.module;
 
-import org.osgi.framework.BundleException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-
 
 import cz.cuni.xrg.intlib.commons.app.module.ModuleFacade;
 import cz.cuni.xrg.intlib.commons.app.module.event.DPURefreshEvent;
@@ -19,20 +15,15 @@ import cz.cuni.xrg.intlib.commons.app.module.event.DPURefreshEvent;
  */
 class ModuleRefresher implements ApplicationListener<DPURefreshEvent> {
 
-	private final static Logger LOG = LoggerFactory.getLogger(ModuleRefresher.class);
-	
 	@Autowired
 	private ModuleFacade moduleFacade;
 	
 	@Override
 	public void onApplicationEvent(DPURefreshEvent event) {
-		final String relativePath = event.getRelativePath();
-		// just call update on ModuleFacade
-		try {			
-			moduleFacade.update(relativePath);
-		} catch (BundleException e) {
-			LOG.error("Faield to update bundle: {}", relativePath, e);
-		}
+		final String relativeDir = event.getDirectoryName();
+		// just uninstall the current version
+		// the new version will be loaded on demand
+		moduleFacade.uninstallDir(relativeDir);
 	}
 		
 }
