b45d5b7e60703dccb2c91007d53bed46cd13e98e
Petr Škoda <p.skoda@centrum.cz>
2013-09-26 18:01:02 +0200

    EmailCommunicator now send "instant" email report for schedules based on user settings

--- backend/src/main/java/cz/cuni/xrg/intlib/backend/communication/EmailCommunicator.java
+++ backend/src/main/java/cz/cuni/xrg/intlib/backend/communication/EmailCommunicator.java
@@ -13,87 +13,86 @@ import org.springframework.context.ApplicationListener;
 import cz.cuni.xrg.intlib.backend.pipeline.event.PipelineFinished;
 import cz.cuni.xrg.intlib.commons.app.communication.EmailSender;
 import cz.cuni.xrg.intlib.commons.app.pipeline.PipelineExecution;
-import cz.cuni.xrg.intlib.commons.app.pipeline.PipelineExecutionStatus;
 import cz.cuni.xrg.intlib.commons.app.scheduling.EmailAddress;
-import cz.cuni.xrg.intlib.commons.app.scheduling.NotificationRecordType;
+import cz.cuni.xrg.intlib.commons.app.scheduling.NotificationRecord;
 import cz.cuni.xrg.intlib.commons.app.scheduling.Schedule;
 import cz.cuni.xrg.intlib.commons.app.scheduling.ScheduleNotificationRecord;
 
 /**
- * Listen to the application event and if need send email about the event 
- * to the user.
+ * Listen to the application event and if need send email about the event to the
+ * user.
  * 
  * @author Petyr
- *
+ * 
  */
-public class EmailCommunicator implements ApplicationListener<ApplicationEvent>{
+public class EmailCommunicator implements ApplicationListener<ApplicationEvent> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(EmailCommunicator.class);
-	
+	private static final Logger LOG = LoggerFactory
+			.getLogger(EmailCommunicator.class);
+
 	/**
 	 * Provide functionality to send email.
 	 */
 	@Autowired
 	private EmailSender emailSender;
-	
+
 	/**
-	 * Add email based on settings from {@link Schedule}. 
-	 * @param emails
-	 *  @param execution
-	 * @param schedule
-	 */
-	private void add(Set<String> emails, PipelineExecution execution, Schedule schedule) {
-		ScheduleNotificationRecord notification = schedule.getNotification();
-		
-		if (notification == null) {
-			return;
-		}
-		
-		if (execution.getStatus() == PipelineExecutionStatus.FAILED) {
-			if (notification.getTypeError() == NotificationRecordType.INSTANT) {
-				// continue -> send 
-			} else {
-				// do not send
-				return;
-			}
-		} else {
-			if (notification.getTypeSuccess() == NotificationRecordType.INSTANT) {
-				// continue -> send 
-			} else {
-				// do not send
-				return;
-			}
-		}
-		
-		// add all 
-		for (EmailAddress adress : notification.getEmails()) {
-			emails.add(adress.toString());
-		}
-	}
-	
-	/**
-	 * Can add author of the execution based on the author email settings.
+	 * Add email based on {@link ScheduleNotificationRecord}.
+	 * 
 	 * @param emails
 	 * @param execution
+	 * @param notification Notification settings.
+	 * @param emailsToAdd
 	 */
-	private void add(Set<String> emails, PipelineExecution execution) {
-		if (execution.getStatus() == PipelineExecutionStatus.FAILED) {
-			// we send only for failed state 
-			// and if user has certain settings
-			
-			// TODO Petyr: Send to the author of the execution
+	private void add(Set<String> emails,
+			PipelineExecution execution,
+			NotificationRecord notification,
+			Set<EmailAddress> emailsToAdd) {
+		switch (execution.getStatus()) {
+		case FINISHED_SUCCESS:
+		case FINISHED_WARNING:
+			switch (notification.getTypeSuccess()) {
+			case INSTANT: // add email
+				for (EmailAddress email : emailsToAdd) {
+					emails.add(email.toString());
+				}
+				return;
+			case DAILY:
+				// we do not send now ... will be send in daily report
+			case NO_REPORT:
+				return;
+			}
+			break;
+		case FAILED:
+		case CANCELLED:
+			switch (notification.getTypeError()) {
+			case INSTANT: // add email
+				for (EmailAddress email : emailsToAdd) {
+					emails.add(email.toString());
+				}
+				return;
+			case DAILY:
+				// we do not send now ... will be send in daily report
+			case NO_REPORT:
+				return;
+			}
+			break;
+		default:
+			LOG.warn("Unexpected execution status '{}' ", execution.getStatus());
+			return;
 		}
 	}
-	
+
 	/**
 	 * Prepare body for email that inform about single execution.
+	 * 
 	 * @param execution
 	 * @param schedule
 	 * @return
 	 */
 	private String bodyInstant(PipelineExecution execution, Schedule schedule) {
 		StringBuilder body = new StringBuilder();
-		
+
 		body.append("Report for pipeline: ");
 		body.append(execution.getPipeline().getName());
 		body.append("<br/>");
@@ -106,27 +105,28 @@ public class EmailCommunicator implements ApplicationListener<ApplicationEvent>{
 		body.append("Execution result: ");
 		body.append(execution.getStatus());
 		body.append("<br/>");
-		
+
 		return body.toString();
 	}
-	
+
 	/**
 	 * Prepare subject for email that inform about single execution.
+	 * 
 	 * @param execution
 	 * @param schedule
 	 * @return
 	 */
 	private String subjectInstant(PipelineExecution execution, Schedule schedule) {
 		StringBuilder subject = new StringBuilder();
-		
+
 		subject.append("Execution report for: ");
 		subject.append(execution.getPipeline().getName());
 		subject.append(" ");
 		subject.append(execution.getStart().toString());
-		
+
 		return subject.toString();
 	}
-	
+
 	@Override
 	public void onApplicationEvent(ApplicationEvent event) {
 		if (event instanceof PipelineFinished) {
@@ -134,13 +134,33 @@ public class EmailCommunicator implements ApplicationListener<ApplicationEvent>{
 			PipelineExecution execution = pipelineFinishedEvent.getExecution();
 			Schedule schedule = execution.getSchedule();
 			// pipeline finished has been scheduled?
-			if(schedule == null) {
+			if (schedule == null) {
 				// we ignore non scheduled executions
 			} else {
 				// create list with recipients
 				Set<String> emails = new HashSet<>();
-				add(emails, execution);
-				add(emails, execution, schedule);
+
+				// use Schedule notification if available
+				// otherwise use setting from user
+				if (schedule.getNotification() == null) {
+					add(emails, execution, schedule.getOwner()
+							.getNotification(), schedule.getOwner()
+							.getNotification().getEmails());
+				} else {
+					add(emails, execution, schedule.getNotification(), schedule
+							.getNotification().getEmails());
+				}
+
+				if (emails.isEmpty()) {
+					// no one to send the email
+					LOG.info(
+							"There is no addres to which send email for schedule {}",
+							schedule.getName());
+					return;
+				}
+
+				LOG.debug("Sending email for schedule {}", schedule.getName());
+								
 				final String subject = subjectInstant(execution, schedule);
 				final String body = bodyInstant(execution, schedule);
 				// create list of recipients
