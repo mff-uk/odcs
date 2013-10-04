72ff80cbee028c9b0c9285a2866ea66b8a8f2330
Jiri Tomes <Tooomy@seznam.cz>
2013-10-04 18:05:07 +0200

    Add test using construct query in tranformer - issue #567

---
+++ rdf/src/test/java/cz/cuni/xrg/intlib/rdf/ValidationTest.java
@@ -0,0 +1,41 @@
+package cz.cuni.xrg.intlib.rdf;
+
+import cz.cuni.mff.xrg.odcs.rdf.enums.SPARQLQueryType;
+import cz.cuni.mff.xrg.odcs.rdf.impl.SPARQLQueryValidator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author Jiri Tomes
+ */
+public class ValidationTest {
+
+	@Test
+	public void testContructQueryInTransformer() {
+		String query = "prefix lex: <http://purl.org/lex#> \n"
+				+ "prefix frbr: <http://purl.org/vocab/frbr/core#> \n"
+				+ "prefix dcterms: <http://purl.org/dc/terms/> \n"
+				+ "prefix fn: <http://www.w3.org/2005/xpath-functions/#>  \n"
+				+ "\n"
+				+ "construct {\n"
+				+ " ?eoz a frbr:Expression. \n"
+				+ " ?eoz frbr:realizationOf ?oz . \n"
+				+ " ?eoz dcterms:title ?oztitle . \n"
+				+ " ?eoz dcterms:valid ?ucinnost . \n"
+				+ " ?eoz dcterms:language \"cs\" . \n"
+				+ " ?y lex:definesChange [ a lex:Update ; lex:changeResult ?eoz ] . \n"
+				+ "} \n"
+				+ "WHERE { "
+				+ "{ SELECT (iri(fn:concat(str(?oz),'/version/cz/',str(?ucinnost))) as ?eoz) "
+				+ "?oz ?y ?oztitle ?ucinnost where { ?y lex:aktivni-novelizuje ?oz . "
+				+ "?oz dcterms:title ?oztitle . ?y dcterms:valid ?ucinnost . } } }";
+
+		SPARQLQueryValidator validator = new SPARQLQueryValidator(query,
+				SPARQLQueryType.CONSTRUCT);
+
+		boolean isValid = validator.isQueryValid();
+		assertTrue(isValid);
+	}
+}
