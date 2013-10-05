e6c41c6b0eb06e1283cd7096ad9c85cd2071b7d5
Petr Škoda <p.skoda@centrum.cz>
2013-10-05 09:14:23 +0200

    ConfigurableBase can now be configured directly with configuration instance

--- commons-module/src/main/java/cz/cuni/mff/xrg/odcs/commons/module/dpu/ConfigurableBase.java
+++ commons-module/src/main/java/cz/cuni/mff/xrg/odcs/commons/module/dpu/ConfigurableBase.java
@@ -14,7 +14,7 @@ import cz.cuni.mff.xrg.odcs.commons.module.config.ConfigWrap;
  * @author Tomas Knap
  */
 public abstract class ConfigurableBase<C extends DPUConfigObject>
-		implements Configurable<C>, DPU  {
+		implements Configurable<C>, DPU {
 
 	/**
 	 * Object configuration.
@@ -26,18 +26,19 @@ public abstract class ConfigurableBase<C extends DPUConfigObject>
 	 */
 	private ConfigWrap<C> configWrap;
 
-	public ConfigurableBase(Class<C> configClass) {		
+	public ConfigurableBase(Class<C> configClass) {
 		this.configWrap = new ConfigWrap<C>(configClass);
 		this.config = this.configWrap.createInstance();
-	}	
-	
+	}
+
 	@Override
 	public void configure(byte[] c) throws ConfigException {
 		// set configuration for configWrap
 		C newConfig = configWrap.deserialize(c);
-		if (newConfig == null ) {
+		if (newConfig == null) {
 			return;
 		}
+
 		if (newConfig.isValid()) {
 			// use configuration from configWrap
 			config = newConfig;
@@ -50,8 +51,24 @@ public abstract class ConfigurableBase<C extends DPUConfigObject>
 	public byte[] getConf() throws ConfigException {
 		return configWrap.serialize(config);
 	}
-	
+
+	/**
+	 * Validate given configuration and if it's valid then configure the DPU.
+	 * Can be used to set null configuration too.
+	 * 
+	 * @param c
+	 * @throws ConfigException In case of invalid configuration.
+	 */
+	public void configure(C newConfig) throws ConfigException {
+		if (newConfig != null && newConfig.isValid()) {
+			config = newConfig;
+		} else {
+			throw new ConfigException("Invalid configuration.");
+		}
+	}
+
 	@Override
-	public void cleanUp() { }	
+	public void cleanUp() {
+	}
 
 }
