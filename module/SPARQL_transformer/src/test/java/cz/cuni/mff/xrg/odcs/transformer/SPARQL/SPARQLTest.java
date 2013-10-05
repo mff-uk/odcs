f028ea5c93f73e4333b2bc9ca1a7d6af34149b2d
Petr Škoda <p.skoda@centrum.cz>
2013-10-05 10:47:57 +0200

    Added 'construct all' test for SPARQL transformer

---
+++ module/SPARQL_transformer/src/test/java/cz/cuni/mff/xrg/odcs/transformer/SPARQL/SPARQLTest.java
@@ -0,0 +1,43 @@
+package cz.cuni.mff.xrg.odcs.transformer.SPARQL;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openrdf.rio.RDFFormat;
+
+import cz.cuni.mff.xrg.odcs.dpu.test.TestEnvironment;
+import cz.cuni.mff.xrg.odcs.rdf.interfaces.RDFDataUnit;
+
+public class SPARQLTest {
+
+	@Test
+	public void constructAllTest() throws Exception {
+		// prepare dpu
+		SPARQLTransformer trans = new SPARQLTransformer();
+		SPARQLTransformerConfig config = new SPARQLTransformerConfig();
+		config.isConstructType = true;
+		config.SPARQL_Update_Query = "CONSTRUCT {?s ?p ?o} where {?s ?p ?o }";
+		trans.configureDirectly(config);
+
+		// prepare test environment
+		TestEnvironment env = TestEnvironment.create();
+		// prepare data units
+		RDFDataUnit input = env.createRdfInputFromResource("input",
+				"dbpedia.ttl", RDFFormat.TURTLE);
+		RDFDataUnit output = env.createRdfOutput("output");
+		
+		// some triples has been loaded 
+		assertTrue(input.getTripleCount() > 0);
+		// run
+		try {
+			env.run(trans);
+
+			// verify result
+			assertTrue(input.getTripleCount() == output.getTripleCount());
+		} finally {
+			// release resources
+			env.release();
+		}
+	}
+
+}
