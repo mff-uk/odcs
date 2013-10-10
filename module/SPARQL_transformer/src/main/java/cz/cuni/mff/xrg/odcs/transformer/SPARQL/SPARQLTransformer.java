04ff6a031dc28c8382468ca2ac2b4d99a6b309b3
Jiri Tomes <Tooomy@seznam.cz>
2013-10-10 16:43:33 +0200

    Update SPARQL_Transformer using PlaceHolder and replace. Prepare for more inputs creating by Peter - issue #608.

--- module/SPARQL_transformer/src/main/java/cz/cuni/mff/xrg/odcs/transformer/SPARQL/SPARQLTransformer.java
+++ module/SPARQL_transformer/src/main/java/cz/cuni/mff/xrg/odcs/transformer/SPARQL/SPARQLTransformer.java
@@ -6,11 +6,17 @@ import cz.cuni.mff.xrg.odcs.commons.dpu.DPUException;
 import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.AsTransformer;
 import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.InputDataUnit;
 import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.OutputDataUnit;
+import cz.cuni.mff.xrg.odcs.commons.message.MessageType;
 import cz.cuni.mff.xrg.odcs.commons.module.dpu.ConfigurableBase;
 import cz.cuni.mff.xrg.odcs.commons.web.AbstractConfigDialog;
 import cz.cuni.mff.xrg.odcs.commons.web.ConfigDialogProvider;
 import cz.cuni.mff.xrg.odcs.rdf.exceptions.RDFDataUnitException;
+import cz.cuni.mff.xrg.odcs.rdf.impl.PlaceHolder;
 import cz.cuni.mff.xrg.odcs.rdf.interfaces.RDFDataUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.openrdf.model.Graph;
 
 /**
@@ -34,6 +40,85 @@ public class SPARQLTransformer
 		super(SPARQLTransformerConfig.class);
 	}
 
+	private List<PlaceHolder> getPlaceHolders(String constructQuery) {
+
+		String regex = "graph\\s+\\?[gG_][\\w-_]+";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(constructQuery);
+
+		boolean hasResult = matcher.find();
+
+		List<PlaceHolder> placeholders = new ArrayList<>();
+
+		while (hasResult) {
+
+			int start = matcher.start();
+			int end = matcher.end();
+
+			int partIndex = constructQuery.substring(start, end).indexOf("_") + 1;
+
+			start += partIndex;
+
+			String DPUName = constructQuery.substring(start, end);
+
+			PlaceHolder placeHolder = new PlaceHolder(DPUName);
+			placeholders.add(placeHolder);
+
+			hasResult = matcher.find();
+		}
+
+		return placeholders;
+	}
+
+	private void replaceAllPlaceHolders(List<RDFDataUnit> inputs,
+			List<PlaceHolder> placeHolders, DPUContext context) {
+
+		for (PlaceHolder next : placeHolders) {
+			boolean isReplased = false;
+
+			for (RDFDataUnit input : inputs) {
+				if (input.getDataUnitName().equals(next.getDPUName())) {
+
+					//set RIGHT data graph for DPU
+					next.setGraphName(input.getDataGraph().toString());
+					isReplased = true;
+					break;
+				}
+			}
+
+			if (!isReplased) {
+				String DPUName = next.getDPUName();
+				final String message = "Graph for DPU name " + DPUName + " was not replased";
+
+				context.sendMessage(MessageType.WARNING, message);
+			}
+
+		}
+
+	}
+
+	private String getContructQuery(String originalConstructQuery,
+			List<RDFDataUnit> inputs, DPUContext context) {
+
+		String result = originalConstructQuery;
+
+		List<PlaceHolder> placeHolders = getPlaceHolders(originalConstructQuery);
+
+		if (!placeHolders.isEmpty()) {
+			replaceAllPlaceHolders(inputs, placeHolders, context);
+		}
+
+		for (PlaceHolder next : placeHolders) {
+
+			String graphName = "<" + next.getGraphName() + ">";
+
+			result = result.replaceAll("\\?[g|G]_" + next
+					.getDPUName(), graphName);
+		}
+
+		return result;
+	}
+
 	@Override
 	public void execute(DPUContext context)
 			throws DPUException, DataUnitException {
@@ -43,8 +128,20 @@ public class SPARQLTransformer
 
 		try {
 			if (isConstructQuery) {
-				Graph graph = intputDataUnit.executeConstructQuery(updateQuery);
+
+				//TODO - update for more inputs
+				List<RDFDataUnit> inputs = new ArrayList<>();
+				inputs.add(intputDataUnit);
+
+				//creating newConstruct replaced query
+				String constructQuery = getContructQuery(updateQuery, inputs,
+						context);
+
+				//execute given construct query
+				Graph graph = intputDataUnit.executeConstructQuery(
+						constructQuery);
 				outputDataUnit.addTriplesFromGraph(graph);
+
 			} else {
 				outputDataUnit.merge(intputDataUnit);
 				outputDataUnit.executeSPARQLUpdateQuery(updateQuery);
