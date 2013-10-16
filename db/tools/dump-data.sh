7cd2dae28663d18cf4aad91987a9348338fadc80
Jan Vojt <jan@vojt.net>
2013-10-16 11:00:20 +0200

    Updated DB tools.

--- db/tools/dump-data.sh
+++ db/tools/dump-data.sh
@@ -7,13 +7,13 @@ dumpdir="${basedir}/../virtuoso/rdbms"
 config="${HOME}/.odcs/config.properties"
 
 # parse out login info from configuration
-dbuser=`grep "^virtuoso.user" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
-dbpass=`grep "^virtuoso.password" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
-dbhost=`grep "^virtuoso.hostname" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
-dbport=`grep "^virtuoso.port" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbuser=`grep "^virtuoso.rdbms.user" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbpass=`grep "^virtuoso.rdbms.password" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbhost=`grep "^virtuoso.rdbms.hostname" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbport=`grep "^virtuoso.rdbms.port" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
 
 # dump data in relational database for intlib
 echo "fk_check_input_values(0);" > "${dumpdir}/data.sql"
-dbdump "${dbhost}:${dbport}" "$dbuser" "$dbpass" tablename=db.intlib.% -c \
+dbdump "${dbhost}:${dbport}" "$dbuser" "$dbpass" tablename=db.odcs.% -c \
 	| sed 's/^EXIT;$//' >> "${dumpdir}/data.sql"
 echo "fk_check_input_values(1);" >> "${dumpdir}/data.sql"
