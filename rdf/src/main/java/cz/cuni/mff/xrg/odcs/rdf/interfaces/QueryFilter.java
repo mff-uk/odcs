59ad7f405f9c696af48fe3770663bd76223ce73a
Jiri Tomes <Tooomy@seznam.cz>
2013-10-21 19:45:11 +0200

    Add QueryFilter interface for filtering queries.

---
+++ rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/interfaces/QueryFilter.java
@@ -0,0 +1,27 @@
+package cz.cuni.mff.xrg.odcs.rdf.interfaces;
+
+import cz.cuni.mff.xrg.odcs.rdf.impl.QueryFilterManager;
+
+/**
+ * Interface responsible for filtering queries. Methods are using to managing
+ * filters in class {@link QueryFilterManager}.
+ *
+ * @author Jiri Tomes
+ */
+public interface QueryFilter {
+
+	/**
+	 * Return string representation for name of filter.
+	 *
+	 * @return name of filter.
+	 */
+	public String getFilterName();
+
+	/**
+	 * Return string representation of query transformed by filter.
+	 *
+	 * @param originalQuery query as input to filter
+	 * @return transformed query using filter.
+	 */
+	public String applyFilterToQuery(String originalQuery);
+}
