6ad122d82514dd8eedbbdc60eb4f7aeb514e0af7
Jiri Tomes <Tooomy@seznam.cz>
2013-10-25 14:40:40 +0200

    Add calling context.sendMessage to SPARQL_Transformer.

--- module/SPARQL_transformer/src/main/java/cz/cuni/mff/xrg/odcs/transformer/SPARQL/SPARQLTransformer.java
+++ module/SPARQL_transformer/src/main/java/cz/cuni/mff/xrg/odcs/transformer/SPARQL/SPARQLTransformer.java
@@ -31,55 +31,55 @@ import org.slf4j.LoggerFactory;
 public class SPARQLTransformer
 		extends ConfigurableBase<SPARQLTransformerConfig>
 		implements ConfigDialogProvider<SPARQLTransformerConfig> {
-
+	
 	private final Logger LOG = LoggerFactory.getLogger(SPARQLTransformer.class);
 	
 	@InputDataUnit
 	public RDFDataUnit intputDataUnit;
-
+	
 	@OutputDataUnit
 	public RDFDataUnit outputDataUnit;
-
+	
 	public SPARQLTransformer() {
 		super(SPARQLTransformerConfig.class);
 	}
-
+	
 	private List<PlaceHolder> getPlaceHolders(String constructQuery) {
-
+		
 		String regex = "graph\\s+\\?[gG]_[\\w-_]+";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(constructQuery);
-
+		
 		boolean hasResult = matcher.find();
-
+		
 		List<PlaceHolder> placeholders = new ArrayList<>();
-
+		
 		while (hasResult) {
-
+			
 			int start = matcher.start();
 			int end = matcher.end();
-
+			
 			int partIndex = constructQuery.substring(start, end).indexOf("_") + 1;
-
+			
 			start += partIndex;
-
+			
 			String DPUName = constructQuery.substring(start, end);
-
+			
 			PlaceHolder placeHolder = new PlaceHolder(DPUName);
 			placeholders.add(placeHolder);
-
+			
 			hasResult = matcher.find();
 		}
-
+		
 		return placeholders;
 	}
-
+	
 	private void replaceAllPlaceHolders(List<RDFDataUnit> inputs,
 			List<PlaceHolder> placeHolders, DPUContext context) throws DPUException {
-
+		
 		for (PlaceHolder next : placeHolders) {
 			boolean isReplased = false;
-
+			
 			for (RDFDataUnit input : inputs) {
 				if (input.getDataUnitName().equals(next.getDPUName())) {
 
@@ -89,49 +89,49 @@ public class SPARQLTransformer
 					break;
 				}
 			}
-
+			
 			if (!isReplased) {
 				String DPUName = next.getDPUName();
 				final String message = "Graph for DPU name " + DPUName + " was not replased";
-
+				
 				context.sendMessage(MessageType.ERROR, message);
 				throw new DPUException(message);
 			}
-
+			
 		}
-
+		
 	}
-
+	
 	private String getContructQuery(String originalConstructQuery,
 			List<RDFDataUnit> inputs, DPUContext context) throws DPUException {
-
+		
 		String result = originalConstructQuery;
-
+		
 		List<PlaceHolder> placeHolders = getPlaceHolders(originalConstructQuery);
-
+		
 		if (!placeHolders.isEmpty()) {
 			replaceAllPlaceHolders(inputs, placeHolders, context);
 		}
-
+		
 		for (PlaceHolder next : placeHolders) {
-
+			
 			String graphName = "<" + next.getGraphName() + ">";
-
+			
 			result = result.replaceAll("\\?[g|G]_" + next
 					.getDPUName(), graphName);
 		}
-
+		
 		return result;
 	}
-
+	
 	@Override
 	public void execute(DPUContext context)
 			throws DPUException, DataUnitException {
-
+		
 		final String updateQuery = config.SPARQL_Update_Query;
 		final boolean isConstructQuery = config.isConstructType;
-
-				
+		
+		
 		try {
 			if (isConstructQuery) {
 
@@ -147,21 +147,23 @@ public class SPARQLTransformer
 				Graph graph = intputDataUnit.executeConstructQuery(
 						constructQuery);
 				outputDataUnit.addTriplesFromGraph(graph);
-
+				
 			} else {
 				outputDataUnit.merge(intputDataUnit);
 				outputDataUnit.executeSPARQLUpdateQuery(updateQuery);
 			}
-
+			
 		} catch (RDFDataUnitException ex) {
+			context.sendMessage(MessageType.ERROR, ex.getMessage());
 			throw new DPUException(ex.getMessage(), ex);
 		}
-
+		
 		final long beforeTriplesCount = intputDataUnit.getTripleCount();
 		final long afterTriplesCount = outputDataUnit.getTripleCount();
-		LOG.info("Transformed {} triples into {}", beforeTriplesCount, afterTriplesCount);
+		LOG.info("Transformed {} triples into {}", beforeTriplesCount,
+				afterTriplesCount);
 	}
-
+	
 	@Override
 	public AbstractConfigDialog<SPARQLTransformerConfig> getConfigurationDialog() {
 		return new SPARQLTransformerDialog();
