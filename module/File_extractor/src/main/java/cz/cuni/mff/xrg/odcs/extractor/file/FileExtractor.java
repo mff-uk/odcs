33f92afbbbfa16ec15e4e98ac916cca1e500ea2f
Jiri Tomes <Tooomy@seznam.cz>
2013-10-25 14:42:59 +0200

    Add calling context.sendMessage, throw DPUException to File_extractor, update catch parsing in BaseRDF repo - issue #606.

--- module/File_extractor/src/main/java/cz/cuni/mff/xrg/odcs/extractor/file/FileExtractor.java
+++ module/File_extractor/src/main/java/cz/cuni/mff/xrg/odcs/extractor/file/FileExtractor.java
@@ -2,13 +2,16 @@ package cz.cuni.mff.xrg.odcs.extractor.file;
 
 import cz.cuni.mff.xrg.odcs.commons.data.DataUnitException;
 import cz.cuni.mff.xrg.odcs.commons.dpu.DPUContext;
+import cz.cuni.mff.xrg.odcs.commons.dpu.DPUException;
 import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.AsExtractor;
 import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.OutputDataUnit;
+import cz.cuni.mff.xrg.odcs.commons.message.MessageType;
 import cz.cuni.mff.xrg.odcs.commons.module.dpu.ConfigurableBase;
 import cz.cuni.mff.xrg.odcs.commons.web.AbstractConfigDialog;
 import cz.cuni.mff.xrg.odcs.commons.web.ConfigDialogProvider;
 import cz.cuni.mff.xrg.odcs.rdf.enums.FileExtractType;
 import cz.cuni.mff.xrg.odcs.rdf.enums.RDFFormatType;
+import cz.cuni.mff.xrg.odcs.rdf.exceptions.RDFException;
 import cz.cuni.mff.xrg.odcs.rdf.interfaces.RDFDataUnit;
 
 import org.openrdf.rio.RDFFormat;
@@ -16,7 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 
+ *
  * @author Jiri Tomes
  * @author Petyr
  */
@@ -25,7 +28,7 @@ public class FileExtractor extends ConfigurableBase<FileExtractorConfig>
 		implements ConfigDialogProvider<FileExtractorConfig> {
 
 	private final Logger LOG = LoggerFactory.getLogger(FileExtractor.class);
-	
+
 	@OutputDataUnit
 	public RDFDataUnit rdfDataUnit;
 
@@ -34,7 +37,7 @@ public class FileExtractor extends ConfigurableBase<FileExtractorConfig>
 	}
 
 	@Override
-	public void execute(DPUContext context) throws DataUnitException {
+	public void execute(DPUContext context) throws DataUnitException, DPUException {
 
 		final String baseURI = "";
 		final FileExtractType extractType = config.fileExtractType;
@@ -53,10 +56,14 @@ public class FileExtractor extends ConfigurableBase<FileExtractorConfig>
 		LOG.debug("baseURI: {}", baseURI);
 		LOG.debug("onlyThisSuffix: {}", onlyThisSuffix);
 		LOG.debug("useStatisticHandler: {}", useStatisticHandler);
-		
-		rdfDataUnit.extractFromFile(extractType, format, path, fileSuffix,
-				baseURI, onlyThisSuffix, useStatisticHandler);
-		
+
+		try {
+			rdfDataUnit.extractFromFile(extractType, format, path, fileSuffix,
+					baseURI, onlyThisSuffix, useStatisticHandler);
+		} catch (RDFException e) {
+			context.sendMessage(MessageType.ERROR, e.getMessage());
+			throw new DPUException(e.getMessage(), e);
+		}
 		final long triplesCount = rdfDataUnit.getTripleCount();
 		LOG.info("Extracted {} triples", triplesCount);
 	}
