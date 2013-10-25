9b1ee3fe4805cf97471ca32c84e28cb083a83b56
Jiri Tomes <Tooomy@seznam.cz>
2013-10-25 14:40:11 +0200

    Add calling context.sendMessage to File_loader.

--- module/File_loader/src/main/java/cz/cuni/mff/xrg/odcs/loader/file/FileLoader.java
+++ module/File_loader/src/main/java/cz/cuni/mff/xrg/odcs/loader/file/FileLoader.java
@@ -7,6 +7,7 @@ import cz.cuni.mff.xrg.odcs.commons.dpu.DPUContext;
 import cz.cuni.mff.xrg.odcs.commons.dpu.DPUException;
 import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.AsLoader;
 import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.InputDataUnit;
+import cz.cuni.mff.xrg.odcs.commons.message.MessageType;
 import cz.cuni.mff.xrg.odcs.commons.module.dpu.ConfigurableBase;
 import cz.cuni.mff.xrg.odcs.commons.web.*;
 import cz.cuni.mff.xrg.odcs.rdf.enums.RDFFormatType;
@@ -21,24 +22,24 @@ import cz.cuni.mff.xrg.odcs.rdf.interfaces.RDFDataUnit;
 @AsLoader
 public class FileLoader extends ConfigurableBase<FileLoaderConfig>
 		implements ConfigDialogProvider<FileLoaderConfig> {
-
+	
 	private final Logger LOG = LoggerFactory.getLogger(FileLoader.class);
 	
 	@InputDataUnit
 	public RDFDataUnit rdfDataUnit;
-
+	
 	public FileLoader() {
 		super(FileLoaderConfig.class);
 	}
-
+	
 	@Override
 	public void execute(DPUContext context) throws DPUException {
-
+		
 		final String filePath = config.FilePath;
 		final RDFFormatType formatType = config.RDFFileFormat;
 		final boolean isNameUnique = config.DiffName;
 		final boolean canFileOverwritte = true;
-
+		
 		final long triplesCount = rdfDataUnit.getTripleCount();
 		LOG.info("Loading {} triples", triplesCount);
 		
@@ -46,13 +47,13 @@ public class FileLoader extends ConfigurableBase<FileLoaderConfig>
 			rdfDataUnit.loadToFile(filePath, formatType,
 					canFileOverwritte, isNameUnique);
 		} catch (RDFException | CannotOverwriteFileException ex) {
+			context.sendMessage(MessageType.ERROR, ex.getMessage());
 			throw new DPUException(ex);
 		}
 	}
-
+	
 	@Override
 	public AbstractConfigDialog<FileLoaderConfig> getConfigurationDialog() {
 		return new FileLoaderDialog();
 	}
-	
 }
