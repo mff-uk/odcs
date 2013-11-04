6440d4afd927d792eface12c30a8b36c8a3162b5
Jiri Tomes <Tooomy@seznam.cz>
2013-11-04 17:32:33 +0100

    Update TripleCountHandler - used as default handler for parsing.

--- rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/handlers/TripleCountHandler.java
+++ rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/handlers/TripleCountHandler.java
@@ -1,17 +1,17 @@
 package cz.cuni.mff.xrg.odcs.rdf.handlers;
 
 import cz.cuni.mff.xrg.odcs.rdf.interfaces.TripleCounter;
+import org.openrdf.model.Resource;
 
 import org.openrdf.model.Statement;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.util.RDFInserter;
 import org.openrdf.rio.RDFHandlerException;
 
-
 /**
- * Class for counting of extracted triples from SPARQL endpoint. Need for using
- * in cause throwing exception, when no triples were extracted from SPARQL
- * endpoint.
+ * Class for counting of extracted triples from SPARQL endpoint or given file.
+ * Need very oftern in cause throwing exception, when no triples were extracted
+ * from SPARQL endpoint.
  *
  *
  * @author Jiri Tomes
@@ -20,29 +20,39 @@ public class TripleCountHandler extends RDFInserter implements TripleCounter {
 
 	public TripleCountHandler(RepositoryConnection connection) {
 		super(connection);
-	}	
-	private long count = 0;
+	}
+
+	private long tripleCount = 0;
 
 	@Override
 	public void handleStatement(Statement st) throws RDFHandlerException {
 		super.handleStatement(st);
-		count++;
+		tripleCount++;
 	}
 
 	@Override
 	public long getTripleCount() {
-		return count;
+		return tripleCount;
 	}
 
 	@Override
 	public void reset() {
-		count = 0;
+		tripleCount = 0;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return count == 0;
+		return tripleCount == 0;
 	}
-	
-	
+
+	/**
+	 * Set graphs where are insert data thanks using this handler.
+	 *
+	 * @param graphs Collection of graph to insert data from handler.
+	 */
+	public void setGraphContext(Resource... graphs) {
+		if (graphs != null) {
+			super.enforceContext(graphs);
+		}
+	}
 }
