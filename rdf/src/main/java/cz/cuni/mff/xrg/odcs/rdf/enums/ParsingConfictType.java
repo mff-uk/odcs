ee1604c565389e1b1f83d8f497d9a555a4cd8e55
Jiri Tomes <Tooomy@seznam.cz>
2013-11-04 23:06:58 +0100

    Create ParsingConfictType for loggin problems in data parsing using RDF handler.

---
+++ rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/enums/ParsingConfictType.java
@@ -0,0 +1,14 @@
+package cz.cuni.mff.xrg.odcs.rdf.enums;
+
+/**
+ *
+ * Type of problems we want to find out in case of parsing RDF data triples
+ * using RDF handler.
+ *
+ * @author Jiri Tomes
+ */
+public enum ParsingConfictType {
+
+	WARNING,
+	ERROR;
+}
