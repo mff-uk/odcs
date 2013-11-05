1132ed9fe221dcd97eb8271cd3bc40c21232402d
Tomas Knap <toknap@gmail.com>
2013-11-05 15:56:04 +0100

    to hold common info for the views

---
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/views/Utils.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cuni.mff.xrg.odcs.frontend.gui.views;
+
+/**
+ *
+ * @author tomasknap
+ */
+public class Utils {
+    public static final int PAGE_LENGTH = 20;
+}
