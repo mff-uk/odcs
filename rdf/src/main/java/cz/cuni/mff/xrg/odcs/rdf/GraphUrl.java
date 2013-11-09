71de6da713acc82c5a210627b4121f8542a6a2ef
Jiri Tomes <Tooomy@seznam.cz>
2013-11-09 02:20:59 +0100

    Add method getGraphPrefix to GraphUrl - need for deleting application graphs.

--- rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/GraphUrl.java
+++ rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/GraphUrl.java
@@ -2,22 +2,29 @@ package cz.cuni.mff.xrg.odcs.rdf;
 
 /**
  * Help class for creating graph's url.
- * 
+ *
  * @author Petyr
+ * @author Jiri Tomes
  *
  */
 public class GraphUrl {
 
 	private static final String prefix = "http://linked.opendata.cz/resource/odcs/internal/pipeline/";
-	
-	private GraphUrl() {}
-	
+
+	private GraphUrl() {
+	}
+
 	/**
 	 * Translate the dataUnit id in to graph url format.
+	 *
 	 * @param dataUnitId
 	 * @return
 	 */
 	public static String translateDataUnitId(String dataUnitId) {
 		return prefix + dataUnitId.replace('_', '/');
+	}
+
+	public static String getGraphPrefix() {
+		return prefix;
 	}
 }
