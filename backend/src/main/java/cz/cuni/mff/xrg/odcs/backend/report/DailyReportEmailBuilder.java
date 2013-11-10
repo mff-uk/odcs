a9680b2a63545b634e074073b8554a62541af865
skodape <p.skoda@centrum.cz>
2013-11-10 21:35:51 +0100

    Added support for sending daily reports #740

---
+++ backend/src/main/java/cz/cuni/mff/xrg/odcs/backend/report/DailyReportEmailBuilder.java
@@ -0,0 +1,69 @@
+package cz.cuni.mff.xrg.odcs.backend.report;
+
+import cz.cuni.mff.xrg.odcs.commons.app.pipeline.PipelineExecution;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+/**
+ * Create email with daily report.
+ * 
+ * @author Petyr
+ */
+@Component
+public class DailyReportEmailBuilder {
+	
+	public String build(List<PipelineExecution> executions) {
+		StringBuilder body = new StringBuilder();
+		
+		body.append("<table border=2 cellpadding=2 >");
+		body.append("<tr bgcolor=\"#C0C0C0\">");
+		body.append("<th>pipeline</th><th>start</th><th>end</th><th>result</th>");
+		body.append("</tr>");
+		
+		for (PipelineExecution exec : executions) {
+			// pipeline
+			body.append("<td>");
+			body.append(exec.getPipeline().getName());
+			body.append("</td>");
+			// start
+			body.append("<td>");
+			body.append(exec.getStart().toString());
+			body.append("</td>");
+			// end
+			body.append("<td>");
+			body.append(exec.getEnd().toString());
+			body.append("</td>");
+			// result
+			body.append("<td>");
+			switch(exec.getStatus()) {
+				case CANCELLED:
+					body.append("cancelled");
+					break;
+				case CANCELLING:
+					body.append("canceling");
+					break;
+				case FAILED:
+					body.append("failed");
+					break;
+				case FINISHED_SUCCESS:
+					body.append("finished");
+					break;
+				case FINISHED_WARNING:
+					body.append("finished with warning");
+					break;
+				case QUEUED:
+					body.append("queued");
+					break;
+				case RUNNING:
+					body.append("running");
+					break;
+			}
+			body.append("</td>");
+		}
+		
+		body.append("</table>");
+		
+		return body.toString();
+	}
+	
+}
---
+++ backend/src/main/java/cz/cuni/mff/xrg/odcs/backend/report/InstantReportEmailBuilder.java
@@ -0,0 +1,89 @@
+package cz.cuni.mff.xrg.odcs.backend.report;
+
+import cz.cuni.mff.xrg.odcs.commons.app.dpu.DPUFacade;
+import cz.cuni.mff.xrg.odcs.commons.app.execution.message.MessageRecord;
+import cz.cuni.mff.xrg.odcs.commons.app.pipeline.PipelineExecution;
+import cz.cuni.mff.xrg.odcs.commons.app.scheduling.Schedule;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Create email with instant report.
+ * 
+ * @author Petyr
+ */
+@Component
+public class InstantReportEmailBuilder {
+	
+	@Autowired
+	private DPUFacade dpuFacade;
+	
+	public String build(PipelineExecution execution, Schedule schedule) {
+		StringBuilder body = new StringBuilder();
+
+		body.append("<b>Report for pipeline: </b>");
+		body.append(execution.getPipeline().getName());
+		body.append("<br/>");
+		body.append("<b>Execution starts at: </b>");
+		body.append(execution.getStart().toString());
+		body.append("<br/>");
+		body.append("<b>Execution ends at: </b>");
+		body.append(execution.getEnd().toString());
+		body.append("<br/>");
+		body.append("<b>Execution result: </b>");
+		body.append(execution.getStatus());
+		body.append("<br/><br/>");
+		// append messages
+		final List<MessageRecord> messages = dpuFacade.getAllDPURecords(execution);
+		body.append("<b>Published messages:</b> <br/>");
+		body.append("<table border=2 cellpadding=2 >");
+		body.append("<tr bgcolor=\"#C0C0C0\">");
+		body.append("<th>dpu</th><th>time</th><th>type</th><th>short message</th>");
+		body.append("</tr>");
+		
+		for (MessageRecord message : messages) {
+			// set color based on type
+			switch (message.getType()) {
+			case DPU_LOG:
+			case DPU_DEBUG:
+				body.append("<tr>");
+				break;
+			case DPU_ERROR:
+			case PIPELINE_ERROR:
+				body.append("<tr bgcolor=\"#FFE0E0\">");
+				break;
+			case DPU_INFO:
+			case PIPELINE_INFO:
+				body.append("<tr bgcolor=\"#E0E0FF\">");
+				break;			
+			case DPU_WARNING:
+				body.append("<tr bgcolor=\"#FFFFA0\">");
+				break;
+			}
+			// name
+			body.append("<td>");
+			body.append(message.getDpuInstance().getName());
+			body.append("</td>");
+			// time
+			body.append("<td>");
+			body.append(message.getTime());
+			body.append("</td>");
+			// type
+			body.append("<td>");
+			body.append(message.getType().toString());
+			body.append("</td>");
+			// short message			
+			body.append("<td>");
+			body.append(message.getShortMessage());
+			body.append("</td>");
+			// ...
+			body.append("</tr>");
+		}
+		
+		body.append("</table>");
+		
+		return body.toString();
+	}
+	
+}
