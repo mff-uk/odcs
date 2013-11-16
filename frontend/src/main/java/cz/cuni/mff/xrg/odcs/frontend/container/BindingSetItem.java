e248ec60399996246184fdf7dfbc20694eaee5c9
Jiri Tomes <Tooomy@seznam.cz>
2013-11-16 19:41:47 +0100

    Add headers to BindingsSetItem for right variables orded - issue #732.

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/container/BindingSetItem.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/container/BindingSetItem.java
@@ -4,19 +4,25 @@ import com.vaadin.data.Item;
 import com.vaadin.data.Property;
 import com.vaadin.data.util.ObjectProperty;
 import java.util.Collection;
+import java.util.List;
 import org.openrdf.query.BindingSet;
 
 /**
- * Implementation of {@link Item} interface for underlying {@link BindingSet} object.
+ * Implementation of {@link Item} interface for underlying {@link BindingSet}
+ * object.
  *
  * @author Bogo
  */
 public class BindingSetItem implements Item {
 
 	private BindingSet binding;
+
+	private List<String> headers;
+
 	private int id;
 
-	public BindingSetItem(BindingSet binding, int id) {
+	public BindingSetItem(List<String> headers, BindingSet binding, int id) {
+		this.headers = headers;
 		this.binding = binding;
 		this.id = id;
 	}
@@ -38,16 +44,18 @@ public class BindingSetItem implements Item {
 
 	@Override
 	public Collection<?> getItemPropertyIds() {
-		return binding.getBindingNames();
+		return headers;
 	}
 
 	@Override
 	public boolean addItemProperty(Object id, Property property) throws UnsupportedOperationException {
-		throw new UnsupportedOperationException("Not supported for BindingSetItem.");
+		throw new UnsupportedOperationException(
+				"Not supported for BindingSetItem.");
 	}
 
 	@Override
 	public boolean removeItemProperty(Object id) throws UnsupportedOperationException {
-		throw new UnsupportedOperationException("Not supported for BindingSetItem.");
+		throw new UnsupportedOperationException(
+				"Not supported for BindingSetItem.");
 	}
 }
