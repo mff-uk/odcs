9f2ddf7d6b1da02a6c9f4cd7861d5fb4f7c9456e
Jiri Tomes <Tooomy@seznam.cz>
2013-11-25 09:43:26 +0100

    Add overriding method toString in ParsingConflictType - need for creating validator report.

--- rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/enums/ParsingConfictType.java
+++ rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/enums/ParsingConfictType.java
@@ -11,4 +11,19 @@ public enum ParsingConfictType {
 
 	WARNING,
 	ERROR;
+
+	/**
+	 *
+	 * @return Name of {@link ParsingConfictType} starts with capital first
+	 *         letter (others are small).
+	 */
+	@Override
+	public String toString() {
+		String typeName = this.name().toLowerCase();
+
+		String firstLetter = typeName.substring(0, 1).toUpperCase();
+		String restWord = typeName.substring(1);
+
+		return firstLetter + restWord;
+	}
 }
