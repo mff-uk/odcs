5665ab912d44f5d4b7ed13d5a92ff4f470809b0f
skodape <p.skoda@centrum.cz>
2013-11-26 22:56:55 +0100

    Added prune method to the DbLogRead

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/log/DbLogRead.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/log/DbLogRead.java
@@ -1,11 +1,18 @@
 package cz.cuni.mff.xrg.odcs.commons.app.execution.log;
 
 import cz.cuni.mff.xrg.odcs.commons.app.dao.db.DbAccessRead;
+import java.util.Date;
 
 /**
  *
  * @author Petyr
  */
 public interface DbLogRead extends DbAccessRead<Log> {
+	
+	/**
+	 * Delete all logs that are older then given date.
+	 * @param date 
+	 */
+	void prune(Date date);
 	
 }
--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/log/DbLogReadImpl.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/log/DbLogReadImpl.java
@@ -1,6 +1,9 @@
 package cz.cuni.mff.xrg.odcs.commons.app.execution.log;
 
 import cz.cuni.mff.xrg.odcs.commons.app.dao.db.DbAccessReadBase;
+import java.util.Date;
+import javax.persistence.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -10,6 +13,14 @@ public class DbLogReadImpl extends DbAccessReadBase<Log> implements DbLogRead {
 
 	public DbLogReadImpl() {
 		super(Log.class);
+	}
+	
+	@Transactional
+	@Override	
+	public void prune(Date date) {
+		Query query = em.createQuery(
+				"DELETE FROM Log l WHERE l.timestamp < :time");
+		query.setParameter("time", date.getTime()).executeUpdate();
 	}
 	
 }
