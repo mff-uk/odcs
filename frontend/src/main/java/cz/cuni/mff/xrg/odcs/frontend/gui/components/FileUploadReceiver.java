fd845612db08011e0597bd91036bf0fa1dc92213
Jiri Tomes <Tooomy@seznam.cz>
2013-12-01 18:44:17 +0100

    Refactoring Maria´s FileUploadReceiver class in frontend - use FileOutputStream as return value - remove unused variables - make private variables + add get/set method.

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/components/FileUploadReceiver.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/components/FileUploadReceiver.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import com.vaadin.server.Page;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Upload.Receiver;
+import org.apache.log4j.Logger;
 
 /**
  * Upload selected file to template directory
@@ -20,11 +21,11 @@ import com.vaadin.ui.Upload.Receiver;
  */
 public class FileUploadReceiver implements Receiver {
 
-	private static final long serialVersionUID = 5099459605355200117L;
+	private Logger logger = Logger.getLogger(FileUploadReceiver.class);
+
 	private File file;
-	private FileOutputStream fstream = null;
+
 	private Path path;
-	private String fName;
 
 	/**
 	 * return an OutputStream
@@ -32,60 +33,32 @@ public class FileUploadReceiver implements Receiver {
 	@Override
 	public OutputStream receiveUpload(final String filename,
 			final String MIMEType) {
-		fName = filename;
-
-		OutputStream fos;
 
 		try {
 			//create template directory
 			path = Files.createTempDirectory("jarDPU");
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		} catch (IOException ex) {
+			logger.debug(ex);
 		}
 
 		try {
 			file = new File("/" + path + "/" + filename);
-			fstream = new FileOutputStream(file);
-			fos = new OutputStream() {
-				private static final int searchedByte = '\n';
+			FileOutputStream fstream = new FileOutputStream(file);
 
-				@Override
-				public void write(final int b) throws IOException {
-					fstream.write(b);
-				}
-
-				@Override
-				public void write(byte b[], int off, int len) throws IOException {
-					if (b == null) {
-						throw new NullPointerException();
-					} else if ((off < 0) || (off > b.length) || (len < 0)
-							|| ((off + len) > b.length) || ((off + len) < 0)) {
-						throw new IndexOutOfBoundsException();
-					} else if (len == 0) {
-						return;
-					}
-					fstream.write(b, off, len);
-				}
-
-				@Override
-				public void close() throws IOException {
-					fstream.close();
-					super.close();
-				}
-			};
+			return fstream;
 
 		} catch (FileNotFoundException e) {
 			new Notification("Could not open file<br/>", e.getMessage(),
 					Notification.Type.ERROR_MESSAGE).show(Page.getCurrent());
 			return null;
 		}
-		return fos;
+
 	}
-	
+
 	public File getFile() {
 		return file;
 	}
-	
+
 	public Path getPath() {
 		return path;
 	}
