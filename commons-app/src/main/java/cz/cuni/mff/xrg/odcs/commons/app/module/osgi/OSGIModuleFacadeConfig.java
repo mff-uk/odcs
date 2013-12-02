e51c5a2c94ca0b044f9050b4d02754563a3f3fb9
skodape <p.skoda@centrum.cz>
2013-12-02 19:46:09 +0100

    Added rdf.impl package into export list

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/module/osgi/OSGIModuleFacadeConfig.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/module/osgi/OSGIModuleFacadeConfig.java
@@ -65,6 +65,7 @@ class OSGIModuleFacadeConfig {
 			+ "cz.cuni.mff.xrg.odcs.rdf.enums;version=\"1.0.0\","
 			+ "cz.cuni.mff.xrg.odcs.rdf.exceptions;version=\"1.0.0\","
 			+ "cz.cuni.mff.xrg.odcs.rdf.interfaces;version=\"1.0.0\","
+			+ "cz.cuni.mff.xrg.odcs.rdf.impl;version=\"1.0.0\","
 			+ "cz.cuni.mff.xrg.odcs.rdf.repositories;version=\"1.0.0\","
 			+ "cz.cuni.mff.xrg.odcs.rdf.validators;version=\"1.0.0\","
 			+ "cz.cuni.mff.xrg.odcs.rdf.handlers;version=\"1.0.0\","
