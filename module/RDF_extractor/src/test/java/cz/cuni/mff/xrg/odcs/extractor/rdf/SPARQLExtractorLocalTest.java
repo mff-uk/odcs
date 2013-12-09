04ee29b65900149d2fc1d731264a24ec60c9239b
Jiri Tomes <Tooomy@seznam.cz>
2013-12-09 22:03:29 +0100

    Add SPARQLExtractorLocalTest class - add orginal tests for local RDF storage

---
+++ module/RDF_extractor/src/test/java/cz/cuni/mff/xrg/odcs/extractor/rdf/SPARQLExtractorLocalTest.java
@@ -0,0 +1,142 @@
+package cz.cuni.mff.xrg.odcs.extractor.rdf;
+
+import cz.cuni.mff.xrg.odcs.commons.IntegrationTest;
+import cz.cuni.mff.xrg.odcs.rdf.data.RDFDataUnitFactory;
+import cz.cuni.mff.xrg.odcs.rdf.exceptions.RDFException;
+import cz.cuni.mff.xrg.odcs.rdf.interfaces.RDFDataUnit;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.*;
+import org.junit.experimental.categories.Category;
+import org.openrdf.rio.RDFFormat;
+import static org.junit.Assert.*;
+
+/**
+ * Test funcionality extaction for Local RDF storage from SPARQL endpoint.
+ *
+ * @author Jiri Tomes
+ */
+@Category(IntegrationTest.class)
+public class SPARQLExtractorLocalTest {
+
+	private final Logger logger = LoggerFactory.getLogger(
+			SPARQLExtractorLocalTest.class);
+
+	private static RDFDataUnit repository;
+
+	private static final String USER = "dba";
+
+	private static final String PASSWORD = "dba";
+
+	private static final String UPDATE_ENDPOINT = "http://localhost:8890/sparql-auth";
+
+	@BeforeClass
+	public static void setRDFDataUnit() {
+
+		repository = RDFDataUnitFactory.createLocalRDFRepo("Local");
+
+	}
+
+	@Before
+	public void cleanRepository() {
+		repository.cleanAllData();
+	}
+
+	@AfterClass
+	public static void deleteRDFDataUnit() {
+		repository.delete();
+	}
+
+	@Test
+	public void extractBigDataFromEndpoint() {
+
+		try {
+			URL endpointURL = new URL("http://internal.opendata.cz:8890/sparql");
+			String defaultGraphUri = "http://linked.opendata.cz/resource/dataset/seznam.gov.cz/ovm/list/notransform";
+			String query = "CONSTRUCT {?s ?p ?o} where {?s ?p ?o}";
+
+			long sizeBefore = repository.getTripleCount();
+
+			try {
+				SPARQLExtractor extractor = new SPARQLExtractor(repository);
+				extractor
+						.extractFromSPARQLEndpoint(endpointURL, defaultGraphUri,
+						query);
+
+			} catch (RDFException e) {
+				fail(e.getMessage());
+			}
+
+			long sizeAfter = repository.getTripleCount();
+
+			assertTrue(sizeBefore < sizeAfter);
+
+		} catch (MalformedURLException ex) {
+			logger.error("Bad URL for SPARQL endpoint: " + ex.getMessage());
+		}
+	}
+
+	@Test
+	public void extractDataFromSPARQLEndpointTest() {
+
+		try {
+			URL endpointURL = new URL("http://dbpedia.org/sparql");
+			String defaultGraphUri = "http://dbpedia.org";
+			String query = "construct {?s ?o ?p} where {?s ?o ?p} LIMIT 50";
+
+			long sizeBefore = repository.getTripleCount();
+
+			try {
+				SPARQLExtractor extractor = new SPARQLExtractor(repository);
+
+				extractor
+						.extractFromSPARQLEndpoint(endpointURL, defaultGraphUri,
+						query);
+			} catch (RDFException e) {
+				fail(e.getMessage());
+			}
+
+			long sizeAfter = repository.getTripleCount();
+
+			assertTrue(sizeBefore < sizeAfter);
+
+		} catch (MalformedURLException ex) {
+			logger.error("Bad URL for SPARQL endpoint: " + ex.getMessage());
+		}
+	}
+
+	/**
+	 * This is not unit test, as it depends on remote server -> commented out
+	 * for build, use only when debugging
+	 */
+	@Test
+	public void extractDataFromSPARQLEndpointNamePasswordTest() {
+		try {
+			URL endpoint = new URL(UPDATE_ENDPOINT.toString());
+			String defaultGraphUri = "";
+			String query = "construct {?s ?o ?p} where {?s ?o ?p} LIMIT 10";
+
+			RDFFormat format = RDFFormat.N3;
+
+			long sizeBefore = repository.getTripleCount();
+
+			try {
+				SPARQLExtractor extractor = new SPARQLExtractor(repository);
+				extractor.extractFromSPARQLEndpoint(
+						endpoint, defaultGraphUri, query, USER, PASSWORD,
+						format);
+			} catch (RDFException e) {
+				fail(e.getMessage());
+			}
+
+			long sizeAfter = repository.getTripleCount();
+
+			assertTrue(sizeBefore < sizeAfter);
+
+		} catch (MalformedURLException ex) {
+			logger.error("Bad URL for SPARQL endpoint: " + ex.getMessage());
+		}
+	}
+}
