e033031cd91b0c557148f323b1c35d602288427a
Maria Kukhar <mari.kukhar@gmail.com>
2013-12-09 21:01:13 +0100

    Pipeline Conflict dialog. #491

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/views/PipelineEdit.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/views/PipelineEdit.java
@@ -34,6 +34,7 @@ import cz.cuni.mff.xrg.odcs.frontend.auxiliaries.RefreshManager;
 import cz.cuni.mff.xrg.odcs.frontend.gui.ViewComponent;
 import cz.cuni.mff.xrg.odcs.frontend.gui.components.DPUTree;
 import cz.cuni.mff.xrg.odcs.frontend.gui.components.DebuggingView;
+import cz.cuni.mff.xrg.odcs.frontend.gui.components.PipelineConflicts;
 import cz.cuni.mff.xrg.odcs.frontend.gui.components.pipelinecanvas.DetailClosedEvent;
 import cz.cuni.mff.xrg.odcs.frontend.gui.components.pipelinecanvas.PipelineCanvas;
 import cz.cuni.mff.xrg.odcs.frontend.gui.components.pipelinecanvas.ShowDebugEvent;
@@ -82,6 +83,7 @@ public class PipelineEdit extends ViewComponent {
 	Button buttonSave;
 	Button buttonSaveAndClose;
 	Button buttonCancel;
+	Button buttonConflicts;
 	private Button btnMinimize;
 	private Button btnExpand;
 	private boolean isExpanded = true;
@@ -95,6 +97,8 @@ public class PipelineEdit extends ViewComponent {
 	private RefreshManager refreshManager;
 	@Autowired
 	private PipelineHelper pipelineHelper;
+	@Autowired
+	private PipelineConflicts conflictDialog;
 
 	/**
 	 * Empty constructor.
@@ -422,6 +426,20 @@ public class PipelineEdit extends ViewComponent {
 		});
 		buttonBar.addComponent(buttonCommit);
 		
+		buttonConflicts = new Button("Conflicts");
+		buttonConflicts.setHeight("25px");
+		buttonConflicts.setWidth("150px");
+		buttonConflicts.setImmediate(true);
+		buttonConflicts.addClickListener(new com.vaadin.ui.Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+
+				showConflictPipeline();
+
+			}
+		});
+		buttonBar.addComponent(buttonConflicts);
+		
 		buttonSave = new Button("Save");
 		buttonSave.setHeight("25px");
 		buttonSave.setWidth("150px");
@@ -468,6 +486,18 @@ public class PipelineEdit extends ViewComponent {
 		calculateCanvasDimensions(bounds);
 		
 		return mainLayout;
+	}
+	
+	private void showConflictPipeline() {
+
+		// open scheduler dialog
+		if (!conflictDialog.isInitialized()) {
+			conflictDialog.init();
+					}
+
+		if (!UI.getCurrent().getWindows().contains(conflictDialog)) {
+			UI.getCurrent().addWindow(conflictDialog);
+		}
 	}
 	
 	private void setDetailState(boolean expand) {
