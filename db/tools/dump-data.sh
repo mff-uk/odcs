c47a770de246e184769bb9fb042d3960a96b579e
Jan Vojt <jan@vojt.net>
2013-12-16 22:57:18 +0100

    Fixed generation of sequence queries in DB dumping tool.

--- db/tools/dump-data.sh
+++ db/tools/dump-data.sh
@@ -22,5 +22,5 @@ echo "fk_check_input_values(1);" >> "${dumpdir}/data.sql"
 cat "${dumpdir}/schema.sql" | egrep "^(CREATE TABLE|sequence_set)" \
         | awk  '/^sequence_set/ { seq1=$1; seq2=$3;}
                 /^CREATE TABLE/ { if(seq1) print seq1, "(SELECT COALESCE(MAX(id)+1,1) FROM "$3"),", seq2; seq1="";}' \
-        >> "${dumpdir}/sequences.sql"
+        > "${dumpdir}/sequences.sql"
 
