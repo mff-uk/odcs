1d276c121783125feeec3719441797a94e818a2a
Jiri Tomes <Tooomy@seznam.cz>
2013-12-27 19:41:25 +0100

    Add comments to CannotOverwriteFileException.

--- rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/exceptions/CannotOverwriteFileException.java
+++ rdf/src/main/java/cz/cuni/mff/xrg/odcs/rdf/exceptions/CannotOverwriteFileException.java
@@ -3,6 +3,8 @@ package cz.cuni.mff.xrg.odcs.rdf.exceptions;
 import cz.cuni.mff.xrg.odcs.commons.data.DataUnitException;
 
 /**
+ * Exception is thrown when file where we load RDF data is protected for
+ * overwritting.
  *
  * @author Jiri Tomes
  */
@@ -10,18 +12,41 @@ public class CannotOverwriteFileException extends DataUnitException {
 
 	private static final String MESSAGE = "This file cannot be overwritten";
 
+	/**
+	 * Create a new instance of {@link CannotOverwriteFileException} with
+	 * default {@link #MESSAGE}.
+	 */
 	public CannotOverwriteFileException() {
 		super(MESSAGE);
 	}
 
+	/**
+	 * Create new instance of {@link CannotOverwriteFileException} with specific
+	 * message.
+	 *
+	 * @param message String value of described message
+	 */
 	public CannotOverwriteFileException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Create new instance of {@link CannotOverwriteFileException} with cause of
+	 * throwing this exception.
+	 *
+	 * @param cause Cause of throwing exception
+	 */
 	public CannotOverwriteFileException(Throwable cause) {
 		super(cause);
 	}
 
+	/**
+	 * Create new instance of {@link CannotOverwriteFileException} with a
+	 * specifi message and cause of throwing this exception.
+	 *
+	 * @param message String value of described message
+	 * @param cause   Cause of throwing exception
+	 */
 	public CannotOverwriteFileException(String message, Throwable cause) {
 		super(message, cause);
 	}
