37e0c0869734753938883961fd7c8192fdea7f86
Jan Vojt <jan@vojt.net>
2013-12-28 02:27:57 +0100

    Fixed update scripts so they work with new renamed config properties.

--- db/tools/update-rdbms.sh
+++ db/tools/update-rdbms.sh
@@ -7,10 +7,10 @@ dumpdir="${basedir}/../virtuoso/rdbms"
 config="${HOME}/.odcs/config.properties"
 
 # parse out login info from configuration
-dbuser=`grep "^virtuoso.rdbms.user" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
-dbpass=`grep "^virtuoso.rdbms.password" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
-dbhost=`grep "^virtuoso.rdbms.hostname" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
-dbport=`grep "^virtuoso.rdbms.port" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbuser=`grep "^database.sql.user" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbpass=`grep "^database.sql.password" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbhost=`grep "^database.sql.hostname" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
+dbport=`grep "^database.sql.port" $config | head -n 1 | sed -r "s/^[^=]*=\\s?//"`
 
 # recreate schema and import data
 cat "${dumpdir}/schema.sql" "${dumpdir}/data.sql" "${dumpdir}/sequences.sql" | \
