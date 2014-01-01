f0ee8e58407880610cd53ea788db96c6c6b2f367
skodape <p.skoda@centrum.cz>
2014-01-01 19:46:03 +0100

    Class rename due the back compatibility

--- Ontology/src/main/java/cz/cuni/mff/xrg/odcs/ontology/OdcsTerms.java
+++ Ontology/src/main/java/cz/cuni/mff/xrg/odcs/ontology/OdcsTerms.java
@@ -4,7 +4,7 @@ package cz.cuni.mff.xrg.odcs.ontology;
  * Representation of the odcs ontology
  * @author tomasknap
  */
-public class DataUnitTerms {
+public class OdcsTerms {
         
      public static final String DATA_UNIT_TEXT_VALUE_PREDICATE = "http://linked.opendata.cz/ontology/odcs/textValue";
 	 
