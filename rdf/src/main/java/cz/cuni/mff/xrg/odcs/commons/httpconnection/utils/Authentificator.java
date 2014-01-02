d9413a67c9e8ea72843abe6f0ca2ee8667d48ed3
Jiri Tomes <Tooomy@seznam.cz>
2014-01-02 12:54:14 +0100

    Add/update comments in Authentificator class

--- rdf/src/main/java/cz/cuni/mff/xrg/odcs/commons/httpconnection/utils/Authentificator.java
+++ rdf/src/main/java/cz/cuni/mff/xrg/odcs/commons/httpconnection/utils/Authentificator.java
@@ -4,12 +4,18 @@ import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 
 /**
- * Class responsible for autentification to access to SPARQL endpoint.
+ * Class responsible for authentification to access to SPARQL endpoint.
  *
  * @author Jiri Tomes
  */
 public class Authentificator {
 
+	/**
+	 * Make authentification based pair - host name and password.
+	 *
+	 * @param hostName string value of hostName
+	 * @param password string value of password
+	 */
 	public static void authenticate(String hostName, String password) {
 
 		boolean usePassword = !(hostName.isEmpty() && password.isEmpty());
