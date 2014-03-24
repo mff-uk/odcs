e64a0779de84ed8807f0012aae26c986ec5b74a4
Helmut Nagy <h.nagy@semantic-web.at>
2014-03-24 12:37:47 +0100

    Changed logo -> text (UnifiedViews), changed header Login

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/views/Login.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/views/Login.java
@@ -79,7 +79,7 @@ public class Login extends ViewComponent {
         layout.setMargin(true);
         layout.setSpacing(true);
         Label logo = new Label();
-        logo.setValue("<h1>UnifiedViews</h1>");
+        logo.setValue("<h1>Login</h1>");
         logo.setContentMode(ContentMode.HTML);
         layout.addComponent(logo);
 		
@@ -88,7 +88,7 @@ public class Login extends ViewComponent {
 		error.setVisible(false);
 		layout.addComponent(error);
 
-        login = new TextField("Login:");
+        login = new TextField("User:");
 		login.focus();
         layout.addComponent(login);
 
