b3f79e073a5f49ca7528445c1c1e6d7096c9156e
Jan Marcek <jan.marcek@eea.sk>
2014-06-05 16:20:36 +0200

    167 - previous way how to "wire" platform was replaced by bean method.

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/dao/db/datasource/DataSourceFactory.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/dao/db/datasource/DataSourceFactory.java
@@ -17,38 +17,9 @@ import cz.cuni.mff.xrg.odcs.commons.app.conf.ConfigProperty;
  */
 public class DataSourceFactory {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DataSourceFactory.class);
-
     public static final String MYSQL_VALUE = "mysql";
 
     public static final String VIRTUOSO_VALUE = "virtuoso";
 
-    @Autowired
-    private AppConfig configuration;
-
-    private DataSource dataSource;
-
-    public DataSource getDataSource() {
-
-        // initialize data source only once (singleton)
-        if (dataSource == null) {
-            AppConfig rdbmsConf = configuration.getSubConfiguration(ConfigProperty.RDBMS);
-
-            String platform = rdbmsConf.getString(ConfigProperty.DATABASE_PLATFORM);
-            switch (platform) {
-                case VIRTUOSO_VALUE:
-                    dataSource = new VirtuosoDataSource(rdbmsConf);
-                    break;
-                case MYSQL_VALUE:
-                    dataSource = new MySQLDataSource(rdbmsConf);
-                    break;
-                default:
-                    throw new RuntimeException("Unexpected value for database platform found: '"
-                            + platform + "'.");
-            }
-        }
-
-        return dataSource;
-    }
 
 }
