65593d0ac081edad0c117c085ac0a3ac14fab610
Jan Marcek <jan.marcek@eea.sk>
2014-06-06 15:15:19 +0200

    167 - clean

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/dao/db/datasource/DataSourceFactory.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/dao/db/datasource/DataSourceFactory.java
@@ -1,14 +1,5 @@
 package cz.cuni.mff.xrg.odcs.commons.app.dao.db.datasource;
 
-import javax.sql.DataSource;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import cz.cuni.mff.xrg.odcs.commons.app.conf.AppConfig;
-import cz.cuni.mff.xrg.odcs.commons.app.conf.ConfigProperty;
-
 /**
  * Factory bean choosing correct data source specified by application
  * configuration.
