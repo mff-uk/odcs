e94ff7d1387d1efc3597eb667336888cc0404b80
Sveto Krchnavy <krchnavy@cngroup.dk>
2014-07-07 09:45:21 +0200

    RDFToRDFMerger2Transformer dependent on dpu-api

--- module/RDFToRDFMerger2Transformer/src/main/java/cz/cuni/mff/xrg/odcs/dpu/rdftordfmerger2transformer/RDFToRDFMerger2Transformer.java
+++ module/RDFToRDFMerger2Transformer/src/main/java/cz/cuni/mff/xrg/odcs/dpu/rdftordfmerger2transformer/RDFToRDFMerger2Transformer.java
@@ -3,35 +3,32 @@ package cz.cuni.mff.xrg.odcs.dpu.rdftordfmerger2transformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import cz.cuni.mff.xrg.odcs.commons.data.DataUnitException;
+import eu.unifiedviews.dataunit.DataUnit;
+import eu.unifiedviews.dpu.DPU;
+import eu.unifiedviews.dpu.DPUContext;
+import eu.unifiedviews.dpu.DPUException;
 import cz.cuni.mff.xrg.odcs.commons.data.ManagableDataUnit;
-import cz.cuni.mff.xrg.odcs.commons.dpu.DPUContext;
-import cz.cuni.mff.xrg.odcs.commons.dpu.DPUException;
-import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.AsTransformer;
-import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.InputDataUnit;
-import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.OutputDataUnit;
-import cz.cuni.mff.xrg.odcs.commons.message.MessageType;
 import cz.cuni.mff.xrg.odcs.commons.module.dpu.NonConfigurableBase;
 import cz.cuni.mff.xrg.odcs.rdf.RDFDataUnit;
 import cz.cuni.mff.xrg.odcs.rdf.WritableRDFDataUnit;
 
-@AsTransformer
+@DPU.AsTransformer
 public class RDFToRDFMerger2Transformer extends NonConfigurableBase {
     private static final Logger LOG = LoggerFactory.getLogger(RDFToRDFMerger2Transformer.class);
 
-    @InputDataUnit(name = "rdfInput")
+    @DataUnit.AsInput(name = "rdfInput")
     public RDFDataUnit rdfInput;
 
-    @OutputDataUnit(name = "rdfOutput")
+    @DataUnit.AsOutput(name = "rdfOutput")
     public WritableRDFDataUnit rdfOutput;
 
     public RDFToRDFMerger2Transformer() {
     }
 
     @Override
-    public void execute(DPUContext dpuContext) throws DPUException, DataUnitException, InterruptedException {
+    public void execute(DPUContext dpuContext) throws DPUException, InterruptedException {
         String shortMessage = this.getClass().getSimpleName() + " starting.";
-        dpuContext.sendMessage(MessageType.INFO, shortMessage);
+        dpuContext.sendMessage(DPUContext.MessageType.INFO, shortMessage);
         LOG.info(shortMessage);
         
         ((ManagableDataUnit) rdfOutput).merge(rdfInput);
