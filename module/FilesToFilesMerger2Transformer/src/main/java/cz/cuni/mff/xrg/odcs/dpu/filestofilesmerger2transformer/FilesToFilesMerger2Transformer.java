db88e1f035df8139a8fd7f60cd97dbafe3f17760
Sveto Krchnavy <krchnavy@cngroup.dk>
2014-07-07 08:51:23 +0200

    FilesToFilesMerger2Transformer dependent on dpu-api

--- module/FilesToFilesMerger2Transformer/src/main/java/cz/cuni/mff/xrg/odcs/dpu/filestofilesmerger2transformer/FilesToFilesMerger2Transformer.java
+++ module/FilesToFilesMerger2Transformer/src/main/java/cz/cuni/mff/xrg/odcs/dpu/filestofilesmerger2transformer/FilesToFilesMerger2Transformer.java
@@ -1,33 +1,30 @@
 package cz.cuni.mff.xrg.odcs.dpu.filestofilesmerger2transformer;
 
-import cz.cuni.mff.xrg.odcs.commons.data.DataUnitException;
+import eu.unifiedviews.dataunit.DataUnit;
+import eu.unifiedviews.dpu.DPU;
+import eu.unifiedviews.dpu.DPUContext;
+import eu.unifiedviews.dpu.DPUException;
 import cz.cuni.mff.xrg.odcs.commons.data.ManagableDataUnit;
-import cz.cuni.mff.xrg.odcs.commons.dpu.DPUContext;
-import cz.cuni.mff.xrg.odcs.commons.dpu.DPUException;
-import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.AsTransformer;
-import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.InputDataUnit;
-import cz.cuni.mff.xrg.odcs.commons.dpu.annotation.OutputDataUnit;
-import cz.cuni.mff.xrg.odcs.commons.message.MessageType;
 import cz.cuni.mff.xrg.odcs.commons.module.dpu.NonConfigurableBase;
 import cz.cuni.mff.xrg.odcs.files.FilesDataUnit;
 import cz.cuni.mff.xrg.odcs.files.WritableFilesDataUnit;
 
-@AsTransformer
+@DPU.AsTransformer
 public class FilesToFilesMerger2Transformer extends NonConfigurableBase {
 
-    @InputDataUnit(name = "filesInput")
+    @DataUnit.AsInput(name = "filesInput")
     public FilesDataUnit filesInput;
 
-    @OutputDataUnit(name = "filesOutput")
+    @DataUnit.AsOutput(name = "filesOutput")
     public WritableFilesDataUnit filesOutput;
 
     public FilesToFilesMerger2Transformer() {
     }
 
     @Override
-    public void execute(DPUContext dpuContext) throws DPUException, DataUnitException, InterruptedException {
+    public void execute(DPUContext dpuContext) throws DPUException, InterruptedException {
         String shortMessage = this.getClass().getSimpleName() + " starting.";
-        dpuContext.sendMessage(MessageType.INFO, shortMessage);
+        dpuContext.sendMessage(DPUContext.MessageType.INFO, shortMessage);
         
         ((ManagableDataUnit) filesOutput).merge(filesInput);
     }
