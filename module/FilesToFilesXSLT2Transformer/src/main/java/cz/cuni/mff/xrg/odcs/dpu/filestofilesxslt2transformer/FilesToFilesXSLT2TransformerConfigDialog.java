a918416b38ee096b26e714756c53c491d8a453b3
Michal Klempa <mkl@eea.sk>
2014-07-21 08:51:21 +0200

    Trying to make one DPU independent

--- module/FilesToFilesXSLT2Transformer/src/main/java/cz/cuni/mff/xrg/odcs/dpu/filestofilesxslt2transformer/FilesToFilesXSLT2TransformerConfigDialog.java
+++ module/FilesToFilesXSLT2Transformer/src/main/java/cz/cuni/mff/xrg/odcs/dpu/filestofilesxslt2transformer/FilesToFilesXSLT2TransformerConfigDialog.java
@@ -11,18 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.data.util.ObjectProperty;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.ProgressIndicator;
-import com.vaadin.ui.TextArea;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.Upload;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
+import com.vaadin.ui.*;
 
 import eu.unifiedviews.dpu.config.DPUConfigException;
 import eu.unifiedviews.helpers.dpu.config.BaseConfigDialog;
