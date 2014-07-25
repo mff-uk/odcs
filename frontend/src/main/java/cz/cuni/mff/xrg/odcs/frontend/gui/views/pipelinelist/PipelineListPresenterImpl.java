6c92f6c707df4b4187666a9798e6d6f5d9bbe688
Martin Virág <martin.virag@eea.sk>
2014-07-25 13:58:12 +0200

    108 force refresh after changing view

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/views/pipelinelist/PipelineListPresenterImpl.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/views/pipelinelist/PipelineListPresenterImpl.java
@@ -150,6 +150,7 @@ public class PipelineListPresenterImpl implements PipelineListPresenter, PostLog
                 }
             }
         });
+        refreshManager.triggerRefresh();
 	}
 
 	@Override
