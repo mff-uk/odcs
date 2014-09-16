6f10ff1b5179e7013dbcc04966df1ec1eadcacbf
Jan Marcek <jan.marcek@eea.sk>
2014-09-16 15:09:53 +0200

    447 - new jobType was added

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/JobsTypes.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/JobsTypes.java
@@ -1,11 +1,10 @@
 package cz.cuni.mff.xrg.odcs.commons.app;
 
 public class JobsTypes {
-    // -1 - running
-    // 0 - normal
-    // 1 - unlimited aka realtime
-    public static final long UNLIMITED = -1;
 
+    public static final long RUNNING = -1;
+
+    // run from frontend - unscheduled pipeline
     public static final long UNLIMITED = 1;
 
     public static final long MAX_PRIORITY = 0;
