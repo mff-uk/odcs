7b44006606a3cd3f92573750e9d1be154335bafc
Jan Marcek <jan.marcek@eea.sk>
2014-09-16 15:09:20 +0200

    447 - new jobType was added, database scripts was updated

--- db/mysql/rdbms/updates/20140916-limitation_of_running_pipelines.sql
+++ db/mysql/rdbms/updates/20140916-limitation_of_running_pipelines.sql
@@ -1,4 +1,4 @@
-ALTER TABLE exec_pipeline ADD order_number BIGINT  not null DEFAULT -1;
+ALTER TABLE exec_pipeline ADD order_number BIGINT  not null;
 
 ALTER TABLE exec_pipeline ADD created_epoch BIGINT not null;
 
