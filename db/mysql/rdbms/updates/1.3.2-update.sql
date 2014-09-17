6f7b23b0efe1e7225464e521db82046964ddcba0
Jan Marcek <jan.marcek@eea.sk>
2014-09-17 13:27:54 +0200

    447 - updated - db script

--- db/mysql/rdbms/updates/1.3.2-update.sql
+++ db/mysql/rdbms/updates/1.3.2-update.sql
@@ -1,5 +1,7 @@
 ALTER TABLE exec_pipeline ADD order_number BIGINT  not null;
-
 ALTER TABLE exec_pipeline ADD created_epoch BIGINT not null;
-
 ALTER TABLE exec_schedule ADD priority BIGINT not null;
+
+UPDATE exec_pipeline SET order_number = 1 ;
+UPDATE exec_pipeline SET created_epoch = -1 ;
+UPDATE exec_schedule SET priority = 1 ;
