f3fc82ebbfd476b46bbcb659e12618c9e18057c4
Jan Marcek <jan.marcek@eea.sk>
2014-09-17 13:33:33 +0200

    447 - updated - db script - created_epoch - set default value from actual time in epoch

--- db/mysql/rdbms/updates/1.3.2-update.sql
+++ db/mysql/rdbms/updates/1.3.2-update.sql
@@ -3,5 +3,5 @@ ALTER TABLE exec_pipeline ADD created_epoch BIGINT not null;
 ALTER TABLE exec_schedule ADD priority BIGINT not null;
 
 UPDATE exec_pipeline SET order_number = 1 ;
-UPDATE exec_pipeline SET created_epoch = -1 ;
+UPDATE exec_pipeline SET created_epoch = UNIX_TIMESTAMP(NOW());
 UPDATE exec_schedule SET priority = 1 ;
