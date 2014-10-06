15ad63926d14c8d52adecb3942897b7f50dec51f
Martin Virág <martin.virag@eea.sk>
2014-10-06 10:22:30 +0200

    632 ComboBox for priority runtime property

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/ScheduledJobsPriority.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/ScheduledJobsPriority.java
@@ -13,4 +13,14 @@ public enum ScheduledJobsPriority {
         return priority;
     }
 
+    public static ScheduledJobsPriority getForValue(final long value) {
+        if (value <= 0) {
+            return IGNORE;
+        } else if (value == MEDIUM.getValue()) {
+            return MEDIUM;
+        } else if (value == LOWEST.getValue()) {
+            return LOWEST;
+        }
+        return HIGHEST; // value >= HIGHEST
+    }
 }
