272b010f49af13a470dc6f1e9bbf4c1760aa52a6
Peter Golian <pgo@eea.sk>
2014-10-27 16:54:37 +0100

    getSchedules for pipeline

--- master/src/main/java/eu/unifiedviews/master/api/PipelineResource.java
+++ master/src/main/java/eu/unifiedviews/master/api/PipelineResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,16 +13,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import cz.cuni.mff.xrg.odcs.commons.app.facade.PipelineFacade;
+import cz.cuni.mff.xrg.odcs.commons.app.facade.ScheduleFacade;
 import cz.cuni.mff.xrg.odcs.commons.app.pipeline.Pipeline;
+import cz.cuni.mff.xrg.odcs.commons.app.scheduling.Schedule;
 import eu.unifiedviews.master.converter.PipelineToDTOConverter;
+import eu.unifiedviews.master.converter.ScheduleToDTOConverter;
 import eu.unifiedviews.master.model.ApiException;
 import eu.unifiedviews.master.model.PipelineDTO;
+import eu.unifiedviews.master.model.PipelineScheduleDTO;
 
 @Component
 @Path("/pipelines")
 public class PipelineResource {
     @Autowired
     private PipelineFacade pipelineFacade;
+
+    @Autowired
+    private ScheduleFacade scheduleFacade;
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
@@ -33,15 +41,19 @@ public class PipelineResource {
         return PipelineToDTOConverter.convert(pipelines);
     }
 
-//    @GET
-//    @Path("/{pipelineid}")
-//    @Produces({ MediaType.APPLICATION_JSON })
-//    public PipelineDTO getPipeline(@PathParam("pipelineid") Long id) {
-//        Pipeline pip = pipelineFacade.getPipeline(id.longValue());
-//        if (pip == null) {
-//            throw new ApiException(Response.Status.BAD_REQUEST, String.format("Pipeline with id=%d doesn't exist!", id));
-//        }
-//        return PipelineToDTOConverter.convert(pip);
-//    }
+    @GET
+    @Path("/{pipelineid}/schedules")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public List<PipelineScheduleDTO> getPipelineSchedules(@PathParam("pipelineid") Long id) {
+        Pipeline pipeline = pipelineFacade.getPipeline(id.longValue());
+        if (pipeline == null) {
+            throw new ApiException(Response.Status.BAD_REQUEST, String.format("Pipeline with id=%d doesn't exist!", id));
+        }
+        List<Schedule> schedules = scheduleFacade.getSchedulesFor(pipeline);
+        if (schedules == null) {
+            throw new ApiException(Response.Status.INTERNAL_SERVER_ERROR, "ScheduleFacade returned null!");
+        }
+        return ScheduleToDTOConverter.convert(schedules);
+    }
 
 }
---
+++ master/src/main/java/eu/unifiedviews/master/converter/ScheduleToDTOConverter.java
@@ -0,0 +1,65 @@
+package eu.unifiedviews.master.converter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import cz.cuni.mff.xrg.odcs.commons.app.pipeline.Pipeline;
+import cz.cuni.mff.xrg.odcs.commons.app.scheduling.Schedule;
+import eu.unifiedviews.master.model.PipelineScheduleDTO;
+
+public class ScheduleToDTOConverter {
+    private static final String DATE_FORMAT = "yyyyMMdd'T'HH:mm:ss.SSSZ";
+
+    private static final DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+
+    public static PipelineScheduleDTO convert(Schedule schedule) {
+        PipelineScheduleDTO dto = null;
+        if (schedule != null) {
+            dto = new PipelineScheduleDTO();
+            dto.setId(schedule.getId());
+            dto.setDescription(schedule.getDescription());
+            dto.setEnabled(schedule.isEnabled());
+            if (schedule.getFirstExecution() != null) {
+                dto.setFirstExecution(df.format(schedule.getFirstExecution()));
+            } else {
+                dto.setFirstExecution("");
+            }
+            dto.setJustOnce(schedule.isJustOnce());
+            if (schedule.getLastExecution() != null) {
+                dto.setLastExecution(df.format(schedule.getLastExecution()));
+            } else {
+                dto.setLastExecution("");
+            }
+            dto.setPeriod(schedule.getPeriod());
+            dto.setPeriodUnit(schedule.getPeriodUnit());
+            dto.setScheduleType(schedule.getType());
+            Set<Pipeline> pipelines = schedule.getAfterPipelines();
+            List<Long> pipelineIds = new ArrayList<Long>();
+            if (pipelines != null && pipelines.size() > 0) {
+                for (Pipeline pipeline : pipelines) {
+                    pipelineIds.add(pipeline.getId());
+                }
+            }
+            dto.setAfterPipelines(pipelineIds);
+        }
+        return dto;
+    }
+
+    public static List<PipelineScheduleDTO> convert(List<Schedule> schedules) {
+        List<PipelineScheduleDTO> dtos = null;
+        if (schedules != null) {
+            dtos = new ArrayList<PipelineScheduleDTO>();
+            for (Schedule schedule : schedules) {
+                PipelineScheduleDTO dto = convert(schedule);
+                if (dto != null) {
+                    dtos.add(dto);
+                }
+
+            }
+        }
+        return dtos;
+    }
+}
