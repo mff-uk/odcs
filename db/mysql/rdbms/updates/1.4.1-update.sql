b120c62de3563bddce38f04bc5a4780093ec7170
skodape <p.skoda@centrum.cz>
2014-11-13 10:49:52 +0100

    sql update script update

--- db/mysql/rdbms/updates/1.4.1-update.sql
+++ db/mysql/rdbms/updates/1.4.1-update.sql
@@ -1,6 +1,3 @@
---
-ALTER TABLE `dpu_instance` ADD COLUMN `use_template_config` SMALLINT NOT NULL DEFAULT 0;
-
 -- Update version.
 UPDATE `properties` SET `value` = '001.004.001' WHERE `key` = 'UV.Core.version';
 UPDATE `properties` SET `value` = '001.000.001' WHERE `key` = 'UV.Plugin-DevEnv.version';
