d1fb04b52e790d8c06c2cb6dd6aa268e09289743
skodape <p.skoda@centrum.cz>
2014-12-09 09:57:19 +0100

    SQL update script fixed

--- db/mysql/rdbms/updates/1.4.1-update.sql
+++ db/mysql/rdbms/updates/1.4.1-update.sql
@@ -8,20 +8,20 @@ ALTER TABLE `dpu_instance` ADD COLUMN `use_template_config` SMALLINT NOT NULL DE
 -- Views.
 
 # Last execution for each pipeline
-CREATE VIEW `uv`.`exec_last_view` AS
+CREATE VIEW `exec_last_view` AS
 SELECT id, pipeline_id, t_end, t_start, status
-FROM `uv`.`exec_pipeline` AS exec
-WHERE t_end = (SELECT max(t_end) FROM `uv`.`exec_pipeline` AS lastExec WHERE exec.pipeline_id = lastExec.pipeline_id)
+FROM `exec_pipeline` AS exec
+WHERE t_end = (SELECT max(t_end) FROM `exec_pipeline` AS lastExec WHERE exec.pipeline_id = lastExec.pipeline_id);
 
 # Pipeline list
-CREATE VIEW `uv`.`pipeline_view` AS
+CREATE VIEW `pipeline_view` AS
 SELECT ppl.id AS id, ppl.name AS name, exec.t_start AS t_start, exec.t_end AS t_end, exec.status AS status
-FROM `uv`.`ppl_model` AS ppl
-LEFT JOIN `uv`.`exec_last_view` AS exec ON exec.pipeline_id = ppl.id
+FROM `ppl_model` AS ppl
+LEFT JOIN `exec_last_view` AS exec ON exec.pipeline_id = ppl.id;
 
 # Execution list.
-CREATE VIEW `uv`.`exec_view` AS
+CREATE VIEW `exec_view` AS
 SELECT exec.id AS id, exec.status AS status, ppl.id AS pipeline_id, ppl.name AS pipeline_name, exec.debug_mode AS debug_mode, exec.t_start AS t_start, exec.t_end AS t_end, exec.schedule_id AS schedule_id, owner.username AS owner_name, exec.stop AS stop, exec.t_last_change AS t_last_change
-FROM `uv`.`exec_pipeline` AS exec
-JOIN `uv`.`ppl_model` AS ppl ON ppl.id = exec.pipeline_id
-JOIN `uv`.`usr_user` AS owner ON owner.id = exec.owner_id
+FROM `exec_pipeline` AS exec
+JOIN `ppl_model` AS ppl ON ppl.id = exec.pipeline_id
+JOIN `usr_user` AS owner ON owner.id = exec.owner_id;
