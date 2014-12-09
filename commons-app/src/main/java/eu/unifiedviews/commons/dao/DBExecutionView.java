36ee196db6faa22193eb3ea16f9b96957557171f
skodape <p.skoda@centrum.cz>
2014-12-09 09:48:32 +0100

    Added views for Pipeline and Execution into commons-app (they should be moved into separated project )

---
+++ commons-app/src/main/java/eu/unifiedviews/commons/dao/DBExecutionView.java
@@ -0,0 +1,12 @@
+package eu.unifiedviews.commons.dao;
+
+import cz.cuni.mff.xrg.odcs.commons.app.dao.db.DbAccess;
+import eu.unifiedviews.commons.dao.view.ExecutionView;
+
+/**
+ *
+ * @author Škoda Petr
+ */
+public interface DBExecutionView extends DbAccess<ExecutionView> {
+
+}
