7c2f33444496f243bd2e53760dfdbfe16afe797e
skodape <p.skoda@centrum.cz>
2015-01-08 13:23:17 +0100

    Added implementation for interfaces from DataUnit, FaultTolerantImpl can be configured from configuration file

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/dataunit/DataUnitFactoryImpl.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/dataunit/DataUnitFactoryImpl.java
@@ -5,9 +5,13 @@ import java.io.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import cz.cuni.mff.xrg.odcs.commons.app.rdf.RepositoryManager;
 import eu.unifiedviews.commons.dataunit.ManagableDataUnit;
+import eu.unifiedviews.commons.dataunit.core.CoreServiceBus;
+import eu.unifiedviews.commons.dataunit.core.CoreServiceBusImpl;
+import eu.unifiedviews.commons.dataunit.core.FaultTolerantImpl;
 import eu.unifiedviews.commons.rdf.repository.ManagableRepository;
 import eu.unifiedviews.commons.rdf.repository.RDFException;
 import eu.unifiedviews.dataunit.DataUnitException;
@@ -26,6 +30,12 @@ class DataUnitFactoryImpl implements DataUnitFactory {
     @Autowired
     private RepositoryManager repositoryManager;
 
+    @Value("${dataunit.failure.wait:30}")
+    private int waitTime;
+
+    @Value("${dataunit.failure.attemps:6}")
+    private int numberOfAttemps;
+
     private final RDFDataUnitFactory rdfDataUnitFactory = new RDFDataUnitFactory();
 
     private final FilesDataUnitFactory filesDataUnitFactory = new FilesDataUnitFactory();
@@ -35,12 +45,18 @@ class DataUnitFactoryImpl implements DataUnitFactory {
         LOG.info("create({}, {}, {}, {}, {})", type, executionId, dataUnitUri, dataUnitName, dataUnitDirectory);
         // Get repository.
         final ManagableRepository repository = repositoryManager.get(executionId);
+        // Prepare core bus.
+        final CoreServiceBus serviceBus = new CoreServiceBusImpl(
+                repository.getConnectionSource(),
+                new FaultTolerantImpl(repository.getConnectionSource(), waitTime * 1000, numberOfAttemps));
         // Create DataUnit.
         switch (type) {
             case FILES:
-                return filesDataUnitFactory.create(dataUnitName, dataUnitUri, repository.getConnectionSource(), dataUnitDirectory);
+                return filesDataUnitFactory.create(dataUnitName, dataUnitUri, 
+                        dataUnitDirectory.toURI().toString(), serviceBus);
             case RDF:
-                return rdfDataUnitFactory.create(dataUnitName, dataUnitUri, repository.getConnectionSource(), dataUnitDirectory);
+                return rdfDataUnitFactory.create(dataUnitName, dataUnitUri,
+                        dataUnitDirectory.toURI().toString(), serviceBus);
             default:
                 throw new DataUnitException("Unknwon DataUnit type: " + type.toString());
         }
