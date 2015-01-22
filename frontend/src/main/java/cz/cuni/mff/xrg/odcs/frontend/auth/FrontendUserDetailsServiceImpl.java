3007191fa75926b205d968e946a9187997beee53
skodape <p.skoda@centrum.cz>
2015-01-22 13:33:34 +0100

    Minor code formating

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/auth/FrontendUserDetailsServiceImpl.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/auth/FrontendUserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package cz.cuni.mff.xrg.odcs.frontend.auth;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,30 +12,28 @@ import cz.cuni.mff.xrg.odcs.commons.app.user.User;
 
 public class FrontendUserDetailsServiceImpl implements UserDetailsService {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(PasswordAuthenticationProvider.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PasswordAuthenticationProvider.class);
 
-	private UserFacade userFacade;
+    private final UserFacade userFacade;
 
     /**
      * Constructor sets up dependencies.
-     * 
+     *
      * @param userFacade
      */
     public FrontendUserDetailsServiceImpl(UserFacade userFacade) {
         this.userFacade = userFacade;
     }
-	
-	@Override
-	public UserDetails loadUserByUsername(String username)
-			throws UsernameNotFoundException {
-		LOG.warn("loading user by username " + username);
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LOG.warn("loading user by username " + username);
         User user = userFacade.getUserByUsername(username);
         if (user == null) {
-        	LOG.error("user is null !");
+            LOG.error("user is null !");
             throw new UsernameNotFoundException(username);
         }
 
         return user;
-	}
+    }
 }
