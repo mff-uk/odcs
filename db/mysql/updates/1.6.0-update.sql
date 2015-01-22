c6b662cdf674e70838177e3bd9128ee6dbf303e0
skodape <p.skoda@centrum.cz>
2015-01-22 16:34:00 +0100

    Removed database named from sql-update script 1.6.0

--- db/mysql/updates/1.6.0-update.sql
+++ db/mysql/updates/1.6.0-update.sql
@@ -1,2 +1,2 @@
 # Update loggin table, rename column logLevel into log_level.
-ALTER TABLE `uv`.`logging` CHANGE COLUMN `logLevel` `log_level` INT(11) NOT NULL  ;
+ALTER TABLE `logging` CHANGE COLUMN `logLevel` `log_level` INT(11) NOT NULL ;
