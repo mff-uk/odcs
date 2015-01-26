2d08e93aa880e975c502d2ea73d23ec79c109df6
eea03 <tomas.caban@eea.sk>
2015-01-26 10:36:21 +0100

    Bugfix: missing database views for Postgres (for MySQL created in c1924d2b41ef896ab95e64c9ec0be2efe9f49f7d)

--- db/postgresql/schema.sql
+++ db/postgresql/schema.sql
@@ -603,3 +603,21 @@ CREATE TABLE "logging"
 CREATE INDEX "ix_LOGGING_dpu" ON "logging" ("dpu");
 CREATE INDEX "ix_LOGGIN_execution" ON "logging" ("execution");
 CREATE INDEX "ix_LOGGIN_relative_id" ON "logging" ("relative_id");
+
+-- Views.
+
+CREATE VIEW "exec_last_view" AS
+SELECT id, pipeline_id, t_end, t_start, status
+FROM "exec_pipeline" AS exec
+WHERE t_end = (SELECT max(t_end) FROM "exec_pipeline" AS lastExec WHERE exec.pipeline_id = lastExec.pipeline_id);
+
+CREATE VIEW "pipeline_view" AS
+SELECT ppl.id AS id, ppl.name AS name, exec.t_start AS t_start, exec.t_end AS t_end, exec.status AS status
+FROM "ppl_model" AS ppl
+LEFT JOIN "exec_last_view" AS exec ON exec.pipeline_id = ppl.id;
+
+CREATE VIEW "exec_view" AS
+SELECT exec.id AS id, exec.status AS status, ppl.id AS pipeline_id, ppl.name AS pipeline_name, exec.debug_mode AS debug_mode, exec.t_start AS t_start, exec.t_end AS t_end, exec.schedule_id AS schedule_id, owner.username AS owner_name, exec.stop AS stop, exec.t_last_change AS t_last_change
+FROM "exec_pipeline" AS exec
+JOIN "ppl_model" AS ppl ON ppl.id = exec.pipeline_id
+JOIN "usr_user" AS owner ON owner.id = exec.owner_id;
