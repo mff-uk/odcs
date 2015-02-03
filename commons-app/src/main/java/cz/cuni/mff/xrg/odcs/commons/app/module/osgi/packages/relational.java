0ca48a6c6081e14da9f6fc5482309316d91e4dd3
eea03 <tomas.caban@eea.sk>
2015-02-03 14:31:20 +0100

    OSGi exports for relational data unit moved to properties file

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/module/osgi/packages/relational.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/module/osgi/packages/relational.java
@@ -6,8 +6,6 @@ public class relational {
      * List of OSGI packages to export. Does not start nor end with separator.
      */
     public static final String PACKAGE_LIST =
-            "eu.unifiedviews.dataunit.relational;uses:=\"eu.unifiedviews.dataunit,java.sql\";version=\"${uv-dataunit-relational.version}\","
-                    + "eu.unifiedviews.helpers.dataunit.relationalhelper;uses:=\"eu.unifiedviews.dataunit.relational,eu.unifiedviews.dataunit\";version=\"${uv-dataunit-helpers.version}\","
-                    + "java.sql";
+            "java.sql";
 
 }
