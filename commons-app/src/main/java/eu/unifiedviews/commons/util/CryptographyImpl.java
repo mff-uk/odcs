88082b53b5407cbc06f47f8877eda1b6f4322415
Tomas Knap <toknap@gmail.com>
2015-03-10 16:07:40 +0100

    Revert "#320. Reverted cryptography."
    This reverts commit d882f7e94de1f6532269ef8fea1ea2478a9f419a.
    
    Reason: API for Cryptography reverted, this has to be reverted as well, please resubmit changes to proper feature branch.

--- commons-app/src/main/java/eu/unifiedviews/commons/util/CryptographyImpl.java
+++ commons-app/src/main/java/eu/unifiedviews/commons/util/CryptographyImpl.java
@@ -2,11 +2,17 @@ package eu.unifiedviews.commons.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -30,18 +36,11 @@ public class CryptographyImpl implements Cryptography {
 
     @Override
     public String encrypt(String plainText) {
-        return doFinal(Cipher.ENCRYPT_MODE, plainText);
-    }
+        String result = plainText;
 
-    private String doFinal(int opmode, String input) {
-        String result = input;
-
-        if (key != null && StringUtils.isNotBlank(input)) {
+        if (StringUtils.isNotBlank(plainText)) {
             try {
-                Cipher cipher = Cipher.getInstance("AES");
-                cipher.init(opmode, key);
-
-                result = new String(cipher.doFinal(input.getBytes("utf8")), "utf8");
+                result = Base64.encodeBase64String(doFinal(Cipher.ENCRYPT_MODE, plainText.getBytes("utf8")));
             } catch (Exception e) {
                 throw new RuntimeException(e.getMessage(), e);
             }
@@ -50,9 +49,32 @@ public class CryptographyImpl implements Cryptography {
         return result;
     }
 
+    private byte[] doFinal(int opmode, byte[] input) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] result = input;
+
+        if (key != null) {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(opmode, key);
+
+            result = cipher.doFinal(input);
+        }
+
+        return result;
+    }
+
     @Override
     public String decrypt(String cipherText) {
-        return doFinal(Cipher.DECRYPT_MODE, cipherText);
+        String result = cipherText;
+
+        if (StringUtils.isNotBlank(cipherText)) {
+            try {
+                result = new String(doFinal(Cipher.DECRYPT_MODE, Base64.decodeBase64(cipherText)), "utf8");
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+
+        return result;
     }
 
 }
