d882f7e94de1f6532269ef8fea1ea2478a9f419a
eea01 <uvdev@eea.sk>
2015-03-10 14:09:26 +0100

    #320. Reverted cryptography.

--- commons-app/src/main/java/eu/unifiedviews/commons/util/CryptographyImpl.java
+++ commons-app/src/main/java/eu/unifiedviews/commons/util/CryptographyImpl.java
@@ -2,17 +2,11 @@ package eu.unifiedviews.commons.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.security.InvalidKeyException;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -36,27 +30,21 @@ public class CryptographyImpl implements Cryptography {
 
     @Override
     public String encrypt(String plainText) {
-        String result = plainText;
+        return doFinal(Cipher.ENCRYPT_MODE, plainText);
+    }
 
-        if (StringUtils.isNotBlank(plainText)) {
+    private String doFinal(int opmode, String input) {
+        String result = input;
+
+        if (key != null && StringUtils.isNotBlank(input)) {
             try {
-                result = Base64.encodeBase64String(doFinal(Cipher.ENCRYPT_MODE, plainText.getBytes("utf8")));
+                Cipher cipher = Cipher.getInstance("AES");
+                cipher.init(opmode, key);
+
+                result = new String(cipher.doFinal(input.getBytes("utf8")), "utf8");
             } catch (Exception e) {
                 throw new RuntimeException(e.getMessage(), e);
             }
-        }
-
-        return result;
-    }
-
-    private byte[] doFinal(int opmode, byte[] input) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        byte[] result = input;
-
-        if (key != null) {
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(opmode, key);
-
-            result = cipher.doFinal(input);
         }
 
         return result;
@@ -64,17 +52,7 @@ public class CryptographyImpl implements Cryptography {
 
     @Override
     public String decrypt(String cipherText) {
-        String result = cipherText;
-
-        if (StringUtils.isNotBlank(cipherText)) {
-            try {
-                result = new String(doFinal(Cipher.DECRYPT_MODE, Base64.decodeBase64(cipherText)), "utf8");
-            } catch (Exception e) {
-                throw new RuntimeException(e.getMessage(), e);
-            }
-        }
-
-        return result;
+        return doFinal(Cipher.DECRYPT_MODE, cipherText);
     }
 
 }
