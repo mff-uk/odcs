33a2b7600b626b5d207f10ff3303110db63ee11c
Miroslav Valus <valus@eea.sk>
2015-03-24 08:20:12 +0100

    Added update script.

--- db/mysql/updates/1.6.0-update.sql
+++ db/mysql/updates/1.6.0-update.sql
@@ -2,3 +2,5 @@
 -- Update version.
 UPDATE `properties` SET `value` = '001.006.000' WHERE `key` = 'UV.Core.version';
 UPDATE `properties` SET `value` = '001.003.000' WHERE `key` = 'UV.Plugin-DevEnv.version';
+
+ALTER TABLE ppl_model ADD UNIQUE (name(255));
