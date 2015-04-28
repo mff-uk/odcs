3b849eb1f1d00c8142114b6168fac3057bc85aa3
Miroslav Valus <valus@eea.sk>
2015-04-28 14:45:23 +0200

    Bug fix: logback configuration missing in Master API.

--- master/src/main/java/eu/unifiedviews/master/model/ApiException.java
+++ master/src/main/java/eu/unifiedviews/master/model/ApiException.java
@@ -1,5 +1,8 @@
 package eu.unifiedviews.master.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -8,9 +11,11 @@ import javax.ws.rs.core.Response.Status;
 public class ApiException extends WebApplicationException {
     private static final long serialVersionUID = -5793273224388941908L;
 
+    private static final Logger LOG = LoggerFactory.getLogger(ApiException.class);
+
     public ApiException(Status status, String message) {
         super(Response.status(status)
                 .entity(new ErrorResponse(message)).type(MediaType.APPLICATION_JSON_TYPE).build());
+        LOG.debug("API exception raised: status: {}, message: {}", status, message);
     }
-
 }
