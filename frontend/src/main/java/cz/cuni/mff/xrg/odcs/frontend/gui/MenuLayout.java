0e52e2cb54d3b77600730345ed8b1f1a4e94ce7b
eea03 <tomas.caban@eea.sk>
2015-06-15 13:48:12 +0200

    External link icon displayed in external link menu item

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/MenuLayout.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/MenuLayout.java
@@ -303,6 +303,7 @@ public class MenuLayout extends CustomComponent {
                 }
             });
             item.setStyleName("menu-external-link");
+            item.setIcon(new ThemeResource("icons/external_link.png"));
             this.menuItems.put("External", item);
 
         } catch (MissingConfigPropertyException e) {
