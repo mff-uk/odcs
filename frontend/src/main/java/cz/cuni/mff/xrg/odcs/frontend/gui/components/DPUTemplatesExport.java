a75cd5d08c4026d79190cdd78996613bb75dd74e
Martin Virág <martin.virag@eea.sk>
2015-07-15 14:20:23 +0200

    #373 clean up temp files after exporting DPU templates

--- frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/components/DPUTemplatesExport.java
+++ frontend/src/main/java/cz/cuni/mff/xrg/odcs/frontend/gui/components/DPUTemplatesExport.java
@@ -1,7 +1,6 @@
 package cz.cuni.mff.xrg.odcs.frontend.gui.components;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -126,7 +125,7 @@ public class DPUTemplatesExport extends Window {
                     return null;
                 }
                 try {
-                    return new FileInputStream(fileToExport);
+                    return new DeletingFileInputStream(fileToExport);
                 } catch (FileNotFoundException e) {
                     LOG.error("Failed to export templates.", e);
                     Notification.show(Messages.getString("DPUTemplatesExport.export.fail.fileNotFound"), e.getMessage(), Notification.Type.ERROR_MESSAGE);
