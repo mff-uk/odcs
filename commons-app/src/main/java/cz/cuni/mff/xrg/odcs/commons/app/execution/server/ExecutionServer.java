b3e026bb5090018d9f7f6bd97b04a2488c364e33
cmader <mail@christianmader.net>
2015-11-06 17:28:42 +0100

    changed field definition, replaced swc repo for mssql jdbc driver with eea repo

--- commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/server/ExecutionServer.java
+++ commons-app/src/main/java/cz/cuni/mff/xrg/odcs/commons/app/execution/server/ExecutionServer.java
@@ -16,20 +16,11 @@
  */
 package cz.cuni.mff.xrg.odcs.commons.app.execution.server;
 
+import cz.cuni.mff.xrg.odcs.commons.app.dao.DataObject;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import cz.cuni.mff.xrg.odcs.commons.app.dao.DataObject;
 
 @Entity
 @Table(name = "backend_servers")
@@ -46,7 +37,7 @@ public class ExecutionServer implements Serializable, DataObject {
     private Long id;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "last_update")
+    @Column(name = "last_update", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Date lastUpdate;
 
     @Column(name = "backend_id")
